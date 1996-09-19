91c078a7aafd19e672d45ec11a0fdb87fab13019
Marc G. Fournier <scrappy@hub.org>
1996-09-19 20:01:40 +0000

    Just a small change to flesh out the func_ptr typedef declaration.
    Submitted by: darcy@druid.druid.com (D'Arcy J.M. Cain)

--- src/backend/include/postgres.h
+++ src/backend/include/postgres.h
@@ -61,7 +61,7 @@ typedef Oid regproc;
 typedef Oid RegProcedure;
 
 /* ptr to func returning (char *) */
-typedef char *	((*func_ptr)());	
+typedef char *	((*func_ptr)(int, ...));	
 
 
 #define RegProcedureIsValid(p)  OidIsValid(p)
