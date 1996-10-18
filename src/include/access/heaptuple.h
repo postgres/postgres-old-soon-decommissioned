21f1216284f5733de0a762185bac992efde2b23e
Marc G. Fournier <scrappy@hub.org>
1996-10-18 17:58:33 +0000

    Add prototype for heap_getattr() to quiet compiler

---
+++ src/include/access/heaptuple.h
@@ -0,0 +1,22 @@
+/*-------------------------------------------------------------------------
+ *
+ * heaptuple.h--
+ *    
+ *
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ * $Id$
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef	HEAPTUPLE_H
+#define HEAPTUPLE_H
+
+extern char *heap_getattr(HeapTuple tup,
+                          Buffer b,
+                          int attnum,
+                          TupleDesc tupleDesc,
+                          bool *isnull);
+
+#endif	/* HEAP_TUPLE_H */ 
