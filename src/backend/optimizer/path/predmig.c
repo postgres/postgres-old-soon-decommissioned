0d8a14158a9439d26d3e6ef5919bd0294be661c5
bryanh <bryanh>
1996-10-23 07:14:43 +0000

    Update "planner" directory to "optimizer" in include paths in this dead code.

--- src/backend/optimizer/path/predmig.c
+++ src/backend/optimizer/path/predmig.c
@@ -41,12 +41,12 @@
 #include "nodes/relation.h"
 #include "utils/palloc.h"
 #include "utils/elog.h"
-#include "planner/xfunc.h"
-#include "planner/pathnode.h"
-#include "planner/internal.h"
-#include "planner/cost.h"
-#include "planner/keys.h"
-#include "planner/tlist.h"
+#include "optimizer/xfunc.h"
+#include "optimizer/pathnode.h"
+#include "optimizer/internal.h"
+#include "optimizer/cost.h"
+#include "optimizer/keys.h"
+#include "optimizer/tlist.h"
 #include "lib/qsort.h"
 
 #define is_clause(node) (get_cinfo(node))  /* a stream node represents a
