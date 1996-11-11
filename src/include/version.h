1dbf076a4d26045513988e4461a4aecdce541dcd
bryanh <bryanh>
1996-11-11 14:24:38 +0000

    Goes with the recently added src/utils/version.c, will replace some stuff in miscadmin.h.

---
+++ src/include/version.h
@@ -0,0 +1,24 @@
+/*-------------------------------------------------------------------------
+ *
+ * version.h--
+ *    this file contains the interface to version.c.
+ *    Also some parameters.
+ *
+ * $Id$
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef VERSION_H
+#define VERSION_H
+
+void
+ValidatePgVersion(const char *path, char **reason_p);
+
+void
+SetPgVersion(const char *path, char **reason_p);
+
+#define	PG_RELEASE	6
+#define PG_VERSION	0
+#define	PG_VERFILE	"PG_VERSION"
+
+#endif
