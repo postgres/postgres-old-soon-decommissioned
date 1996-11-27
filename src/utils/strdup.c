3c3e47cdb7c0af918e466fa548ff77b5dbb90cea
bryanh <bryanh>
1996-11-27 01:46:52 +0000

    Forgot in yesterday's Ultrix compile work.

---
+++ src/utils/strdup.c
@@ -0,0 +1,24 @@
+/*-------------------------------------------------------------------------
+ *
+ * strdup.c--
+ *    copies a null-terminated string.
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *    $Header$
+ *
+ *-------------------------------------------------------------------------
+ */
+#include <string.h>
+#include "strdup.h"
+
+char *
+strdup(char *string)
+{
+    char *nstr;
+
+    nstr = strcpy((char *)palloc(strlen(string)+1), string);
+    return nstr;
+}
