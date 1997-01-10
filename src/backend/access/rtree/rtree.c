a3e3a4074b93049bfceb5a5f46418f754abbf312
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-01-10 09:47:28 +0000

    index_insert has now HeapRelation as last param (for unique index implementation).

--- src/backend/access/rtree/rtree.c
+++ src/backend/access/rtree/rtree.c
@@ -277,7 +277,7 @@ rtbuild(Relation heap,
  *    It doesn't do any work; just locks the relation and passes the buck.
  */
 InsertIndexResult
-rtinsert(Relation r, Datum *datum, char *nulls, ItemPointer ht_ctid, bool is_update)
+rtinsert(Relation r, Datum *datum, char *nulls, ItemPointer ht_ctid, Relation heapRel)
 {
     InsertIndexResult res;
     IndexTuple itup;
