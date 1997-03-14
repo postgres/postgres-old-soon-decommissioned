3bf9ef09d2a0c4d500187ec20b163f22864616fc
Marc G. Fournier <scrappy@hub.org>
1997-03-14 06:01:40 +0000

    Last of Dan's hidden tar balls :)

---
+++ src/backend/port/linuxalpha/port.c
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------------------
+ *
+ * port.c--
+ *    Linux-specific routines
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *    $Header$
+ *
+ *-------------------------------------------------------------------------
+ */
