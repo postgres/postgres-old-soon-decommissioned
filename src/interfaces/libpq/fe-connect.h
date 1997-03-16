88c551ed1d4497b7fb6f6c406bb0154a6f302ec6
Marc G. Fournier <scrappy@hub.org>
1997-03-16 19:06:04 +0000

    Don't forget the fe-connect.h include file

---
+++ src/interfaces/libpq/fe-connect.h
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------------------
+ *
+ * fe-connect.h
+ *
+ *    Definitions related to setting up a connection to the backend
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ * $Id$
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef FE_CONNECT_H
+#define        FE_CONNECT_H
+
+/*----------------------------------------------------------------
+ * Common routines and definitions
+ *----------------------------------------------------------------
+ */
+
+extern int packetSend(Port *port, PacketBuf *buf, PacketLen len, bool nonBlocking);
+
+#endif /* FE_CONNECT_H */
+#ifndef FE_CONNECT_H
+#define FE_CONNECT_H
+
+int packetSend(Port *port, PacketBuf *buf, PacketLen len, bool nonBlocking);
+
+#endif
