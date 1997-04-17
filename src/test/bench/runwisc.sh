1930901843e9324ce879204d32648927764865c2
Marc G. Fournier <scrappy@hub.org>
1997-04-17 13:48:52 +0000

    Some slight changes to the Wisconsin Benchmark tests since postgres requires a -D<datadir> option

--- src/test/bench/runwisc.sh
+++ src/test/bench/runwisc.sh
@@ -6,12 +6,17 @@
 #
 # time $POSTGRES -texecutor -tplanner -f hashjoin -Q bench
 #
+if [ ! -d $1 ]; then
+        echo " you must specify a valid data directory "
+        exit
+fi
+
 if [ -d ./obj ]; then
 	cd ./obj
 fi
 
 echo =============== vacuuming benchmark database... =================
-echo "vacuum" | postgres -Q bench > /dev/null
+echo "vacuum" | postgres -D${1} -Q bench > /dev/null
 
 echo =============== running benchmark... =================
-time postgres -texecutor -tplanner -Q bench < bench.sql
+time postgres -D${1} -texecutor -tplanner -Q bench < bench.sql
