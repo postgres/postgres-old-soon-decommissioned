0a9af702e569fd9e9fd523493fc3497b99c04ed4
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-04-23 05:52:32 +0000

    There were too many comments.

--- src/include/tcop/variable.h
+++ src/include/tcop/variable.h
@@ -1,29 +1,10 @@
 /*
- * Headers for handling of SET var TO statements
+ * Headers for handling of 'SET var TO', 'SHOW var' and 'RESET var' 
+ * statements
  *
  * $Id$
  *
- * $Log: variable.h,v $
- * Revision 1.2  1997/04/17 13:50:57  scrappy
- * From: "Martin J. Laubach" <mjl@CSlab.tuwien.ac.at>
- * Subject: [HACKERS] Patch: set date to euro/us postgres/iso/sql
- *
- *   Here a patch that implements a SET date for use by the datetime
- * stuff. The syntax is
- *
- *         SET date TO 'val[,val,...]'
- *
- *   where val is us (us dates), euro (european dates), postgres,
- * iso or sql.
- *
- *   Thomas is working on the integration in his datetime module.
- * I just needed to get the patch out before it went stale :)
- *
- * Revision 1.1  1997/04/10 16:53:30  mjl
- * Initial revision
- *
  */
-/*-----------------------------------------------------------------------*/
 
 enum DateFormat { Date_Postgres, Date_SQL, Date_ISO };
 
