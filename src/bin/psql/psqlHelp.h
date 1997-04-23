9aff9f538f7dbc4928e57c424f3b8cb5c90d6710
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-04-23 06:41:56 +0000

    Vacuum help changed (ANALYZE).

--- src/bin/psql/psqlHelp.h
+++ src/bin/psql/psqlHelp.h
@@ -166,6 +166,6 @@ static struct _helpStruct QL_HELP[] = {
       "update <class_name> set <attr1>=<expr1>,...<attrN>=<exprN> [from <from_clause>] [where <qual>];"},
   { "vacuum",
       "vacuum the database, i.e. cleans out deleted records, updates statistics",
-      "vacuum [table];"},
+      "vacuum [verbose] [analyze]\n\tor\nvacuum [verbose] table [analyze [(attr1, ... attrN)] ];"},
   { NULL, NULL, NULL}  /* important to keep a NULL terminator here! */
 };
