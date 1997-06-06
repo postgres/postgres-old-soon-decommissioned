67c8f70f0c242d41db30a69517523f800b8089ad
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-06-06 03:11:46 +0000

    Added check is new item successfuly inserted to a page or not.

--- src/backend/access/nbtree/nbtsort.c
+++ src/backend/access/nbtree/nbtsort.c
@@ -938,8 +938,9 @@ _bt_buildadd(Relation index, void *pstate, BTItem bti, int flags)
 	     o <= last_off;
 	     o = OffsetNumberNext(o), n = OffsetNumberNext(n)) {
 	    ii = PageGetItemId(opage, o);
-	    (void) PageAddItem(npage, PageGetItem(opage, ii),
-			       ii->lp_len, n, LP_USED);
+	    if ( PageAddItem(npage, PageGetItem(opage, ii),
+			       ii->lp_len, n, LP_USED) == InvalidOffsetNumber )
+		elog (FATAL, "btree: failed to add item to the page in _bt_sort (1)");
 #if 0
 #if defined(FASTBUILD_DEBUG) && defined(FASTBUILD_MERGE)
 	    {
@@ -1021,7 +1022,8 @@ _bt_buildadd(Relation index, void *pstate, BTItem bti, int flags)
      * new chain of duplicates.
      */
     off = OffsetNumberNext(last_off);
-    (void) PageAddItem(npage, (Item) bti, btisz, off, LP_USED);
+    if ( PageAddItem(npage, (Item) bti, btisz, off, LP_USED) == InvalidOffsetNumber )
+	elog (FATAL, "btree: failed to add item to the page in _bt_sort (2)");
 #if 0
 #if defined(FASTBUILD_DEBUG) && defined(FASTBUILD_MERGE)
     {
