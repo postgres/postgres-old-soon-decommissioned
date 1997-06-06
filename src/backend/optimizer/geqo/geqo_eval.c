fe9906ac731fc736a210d4febe64961a6f97ed0a
Marc G. Fournier <scrappy@hub.org>
1997-06-06 00:38:17 +0000

    From: "D'Arcy J.M. Cain" <darcy@druid.net> Subject: [HACKERS] src.original/./backend/lib/fstack.c
    Another change I suggested.  I bracket an unused function and add a
    return to quiet the compiler.  In addition I added an internal
    consistency check.

--- src/backend/optimizer/geqo/geqo_eval.c
+++ src/backend/optimizer/geqo/geqo_eval.c
@@ -415,6 +415,7 @@ new_joininfo_list(List *joininfo_list, List *join_relids)
     return(current_joininfo_list);
 }
 
+#ifdef	NOTUSED
 /*
  * add-new-joininfos--
  *    For each new join relation, create new joininfos that
@@ -636,6 +637,7 @@ nonoverlap_sets(List *s1, List *s2)
     }
     return(true);
 }
+#endif	/* NOTUSED */
 
 /*
  * geqo_joinrel_size--
@@ -682,4 +684,6 @@ geqo_nth(int stop, List *rels)
 		if (i == stop) return lfirst(r);
 		i++;
 		}
+	elog(WARN,"geqo_nth: Internal error - ran off end of list");
+	return NULL;	/* to keep compiler happy */
 }
