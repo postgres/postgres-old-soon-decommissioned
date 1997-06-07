eed00fd18e0ba56c7c44f11918fda60c85851bda
Bruce Momjian <bruce@momjian.us>
1997-06-07 17:34:35 +0000

    Cleanup on xinv# table names.

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -313,7 +313,7 @@ vc_getrels(NameData *VacRelP)
 	}
 
 	/* don't vacuum large objects for now - something breaks when we do */
-	if ( (strlen(rname) > 5) && rname[0] == 'x' &&
+	if ( (strlen(rname) >= 5) && rname[0] == 'x' &&
 		rname[1] == 'i' && rname[2] == 'n' &&
 		(rname[3] == 'v' || rname[3] == 'x') &&
 		rname[4] >= '0' && rname[4] <= '9')
