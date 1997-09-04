01e99ee5d28e5ad0d0ae8c6721b74ff6b0b32902
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-09-04 13:25:14 +0000

    pg_trigger changed

--- src/include/catalog/pg_class.h
+++ src/include/catalog/pg_class.h
@@ -139,7 +139,7 @@ DATA(insert OID = 1271 (  pg_time 100         PGUID 0 0 0 0 0 f t s n 1 0 0 0 f 
 DATA(insert OID = 1273 (  pg_hosts 101        PGUID 0 0 0 0 0 f t s n 3 0 0 0 f _null_ ));
 DATA(insert OID = 1215 (  pg_attrdef 109      PGUID 0 0 0 0 0 t t r n 4 0 0 0 f _null_ ));
 DATA(insert OID = 1216 (  pg_relcheck 110     PGUID 0 0 0 0 0 t t r n 4 0 0 0 f _null_ ));
-DATA(insert OID = 1219 (  pg_trigger 111      PGUID 0 0 0 0 0 t t r n 10 0 0 0 f _null_ ));
+DATA(insert OID = 1219 (  pg_trigger 111      PGUID 0 0 0 0 0 t t r n 7 0 0 0 f _null_ ));
 
 #define RelOid_pg_type		1247
 #define RelOid_pg_demon       	1251
