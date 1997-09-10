c925c6be2cab3d38397bf1d65057a4d6bba1d622
Bruce Momjian <bruce@momjian.us>
1997-09-10 23:57:49 +0000

    Cleanup for array fix patch.

--- src/backend/access/common/tupdesc.c
+++ src/backend/access/common/tupdesc.c
@@ -481,7 +481,6 @@ BuildDescForRelation(List *schema, char *relname)
 		arry = entry->typename->arrayBounds;
 		attisset = entry->typename->setof;
 
-		strNcpy(typename, entry->typename->name, NAMEDATALEN - 1);
 		if (arry != NIL)
 		{
 			/* array of XXX is _XXX */
@@ -489,7 +488,10 @@ BuildDescForRelation(List *schema, char *relname)
 			attdim = length(arry);
 		}
 		else
+		{
+			strNcpy(typename, entry->typename->name, NAMEDATALEN - 1);
 			attdim = 0;
+		}
 
 		if (!TupleDescInitEntry(desc, attnum, attname,
 								typename, attdim, attisset))
