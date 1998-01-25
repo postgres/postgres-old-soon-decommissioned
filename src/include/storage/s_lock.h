cd7d28a4d63ee3ea693e1f64ba98b9305be4ad1f
Marc G. Fournier <scrappy@hub.org>
1998-01-25 05:23:59 +0000

    From: Keith Parks <emkxp01@mtcc.demon.co.uk>
    Small patch for sparc solaris/tas()

--- src/include/storage/s_lock.h
+++ src/include/storage/s_lock.h
@@ -117,8 +117,7 @@
  * Solaris 2
  */
 
-#if (defined(__i386__) && defined(__sun__)) || \
-	defined(sparc_solaris)
+#if ((defined(__i386__) || defined(__sparc__)) && defined(__sun__)
 /* for xxxxx_solaris, this is defined in port/.../tas.s */
 
 static int	tas(slock_t *lock);
