e8fceaefdd3488e434c1c80edc51f7597723bc3a
Marc G. Fournier <scrappy@hub.org>
1998-01-26 02:48:36 +0000

    Try this to fix HP/UX port compile

---
+++ src/backend/port/dynloader/hpux.h
@@ -0,0 +1,22 @@
+/*-------------------------------------------------------------------------
+ *
+ * dynloader.h--
+ *	  dynamic loader for HP-UX using the shared library mechanism
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *	  $Header$
+ *
+ *	NOTES
+ *		all functions are defined here -- it's impossible to trace the
+ *		shl_* routines from the bundled HP-UX debugger.
+ *
+ *-------------------------------------------------------------------------
+ */
+/* System includes */
+void	   *pg_dlopen(char *filename);
+func_ptr	pg_dlsym(void *handle, char *funcname);
+void		pg_dlclose(void *handle);
+char	   *pg_dlerror();
