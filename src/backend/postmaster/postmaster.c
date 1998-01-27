2ffd9db43c8518ef196093c48902ec06248952c9
Bruce Momjian <bruce@momjian.us>
1998-01-27 03:11:46 +0000

    Protocol cleanup from Phil.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -1162,13 +1162,13 @@ DoExec(Port *port)
 	argbuf[(2 * ARGV_SIZE)] = '\0';
 	split_opts(av, &ac, argbuf);
 
-	StrNCpy(dbbuf, port->database, ARGV_SIZE);
-	av[ac++] = dbbuf;
-
 	/* Tell the backend what protocol the frontend is using. */
 
 	sprintf(protobuf, "-v %u", port->proto);
 	av[ac++] = protobuf;
+
+	StrNCpy(dbbuf, port->database, ARGV_SIZE);
+	av[ac++] = dbbuf;
 
 	av[ac] = (char *) NULL;
 
