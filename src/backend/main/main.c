678d2146f0cec08154cd65869957867fb7b31bba
Marc G. Fournier <scrappy@hub.org>
1998-02-05 04:21:56 +0000

    init_fixup_addr() function removed, so port-protos.h no longer required

--- src/backend/main/main.c
+++ src/backend/main/main.c
@@ -22,9 +22,6 @@
 #include "miscadmin.h"
 #include "bootstrap/bootstrap.h"/* for BootstrapMain() */
 #include "tcop/tcopprot.h"		/* for PostgresMain() */
-#if defined(NOFIXADE) || defined(NOPRINTADE)
-#  include "port-protos.h"		/* for init_address_fixup() */
-#endif
 
 #define NOROOTEXEC "\
 \n\"root\" execution of the PostgreSQL backend is not permitted\n\n\
