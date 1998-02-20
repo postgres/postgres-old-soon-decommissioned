a5ecff3674f435a73f7a9e0c6f33cda93f0be644
Marc G. Fournier <scrappy@hub.org>
1998-02-20 01:44:18 +0000

    Okay, revert back to non-REVOKED...happy now? :)

--- src/bin/initdb/initdb.sh
+++ src/bin/initdb/initdb.sh
@@ -351,18 +351,6 @@ echo "vacuuming template1"
 echo "vacuum" | postgres -F -Q -D$PGDATA template1 2>&1 > /dev/null |\
 	grep -v "^DEBUG:"
 
-echo "Altering pg_user acl"
-echo "REVOKE ALL ON pg_user FROM public" | \
-	 postgres -F -Q -D$PGDATA template1 2>&1 > /dev/null |\
-         grep -v "'DEBUG:"
-echo "GRANT SELECT ON pg_user TO public" | \
-	 postgres -F -Q -D$PGDATA template1 2>&1 > /dev/null |\
-         grep -v "'DEBUG:"
-echo "CREATE RULE pg_user_hide_pw as on SELECT to pg_user.passwd DO INSTEAD SELE
-CT '********' as passwd;" | \
-	 postgres -F -Q -D$PGDATA template1 2>&1 > /dev/null |\
-         grep -v "'DEBUG:"
-
 echo "COPY pg_user TO '$PGDATA/pg_pwd' USING DELIMITERS '\\t'" | postgres -F -Q -D$PGDATA template1 2>&1 > /dev/null |\
          grep -v "'DEBUG:"
 
