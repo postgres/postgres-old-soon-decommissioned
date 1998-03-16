b2143250dfcc1b910f805a225eb4bf0086d95cb5
Bruce Momjian <bruce@momjian.us>
1998-03-16 05:41:39 +0000

    Fix for pg_dump of large objects, from alvin@camberlo.demon.co.uk.

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -1856,7 +1856,7 @@ getIndices(int *numIndices)
 	 * find all the user-defined indices. We do not handle partial
 	 * indices.
 	 *
-	 * skip 'Xinx*' - indices on inversion objects
+	 * skip 'xinx*' - indices on inversion objects
 	 *
 	 * this is a 4-way join !!
 	 */
@@ -1877,7 +1877,7 @@ getIndices(int *numIndices)
 			"from pg_index i, pg_class t1, pg_class t2, pg_am a "
 			"where t1.oid = i.indexrelid and t2.oid = i.indrelid "
 			"and t1.relam = a.oid and i.indexrelid > '%d'::oid "
-			"and t2.relname !~ '^pg_' and t1.relname !~ '^Xinx'",
+			"and t2.relname !~ '^pg_' and t1.relname !~ '^xinx'",
 			g_last_builtin_oid);
 
 	res = PQexec(g_conn, query);
