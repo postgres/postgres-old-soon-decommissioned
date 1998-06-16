818db819a398f7a4b52f39a8d7bee6139e3f19a6
Bruce Momjian <bruce@momjian.us>
1998-06-16 06:42:02 +0000

    Hello!
       Attached to the mail is locale-patch.tar.gz. In the archive
       there are:
    
    file README.locale
       short description
    
    directory src/test/locale
       test suite; currently only koi8-r tests, but the suite can be
       easily extended
    
    file locale.patch
       the very patch; to apply: patch < locale.patch; should be applied
       to postgres-6.3.2 (at least I created it with 6.3.2 without any
    additional
       patches)
    
       Files touched by the patch:  src/include/utils/builtins.h
    src/backend/utils/adt/char.c src/backend/utils/adt/varchar.c
    src/backend/utils/adt/varlena.c
    
    Oleg

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -428,6 +428,8 @@ extern char *textout(text *vlena);
 extern text *textcat(text *arg1, text *arg2);
 extern bool texteq(text *arg1, text *arg2);
 extern bool textne(text *arg1, text *arg2);
+extern int varstr_cmp(unsigned char *arg1, int len1, unsigned char *arg2, int len2);
+extern int text_cmp(text *arg1, text *arg2);
 extern bool text_lt(text *arg1, text *arg2);
 extern bool text_le(text *arg1, text *arg2);
 extern bool text_gt(text *arg1, text *arg2);
