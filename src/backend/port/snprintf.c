0760db09fe8cf51f2fa813ec19f04e0ab7de499f
Bruce Momjian <bruce@momjian.us>
1998-12-12 21:31:20 +0000

    Fix prototype for 64-bit platforms.

--- src/backend/port/snprintf.c
+++ src/backend/port/snprintf.c
@@ -121,7 +121,13 @@ vsnprintf(char *str, size_t count, const char *fmt,...)
  */
 
 static void fmtstr __P((char *value, int ljust, int len, int zpad, int maxwidth));
+
+#ifndef HAVE_LONG_INT_64
 static void fmtnum __P((long value, int base, int dosign, int ljust, int len, int zpad));
+#else
+static void fmtnum __P((long_long value, int base, int dosign, int ljust, int len, int zpad));
+#endif
+
 static void dostr __P((char *, int));
 static char *output;
 static void dopr_outch __P((int c));
