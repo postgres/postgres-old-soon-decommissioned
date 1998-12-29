f2e02a5ed2913215cfec7956ef19c48c4ca47667
Bruce Momjian <bruce@momjian.us>
1998-12-29 18:29:18 +0000

    Fix for deadlock timer timeout.

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -449,7 +449,6 @@ ProcSleep(PROC_QUEUE *waitQueue,/* lock->waitProcs */
 	int			i;
 	bool		deadlock_checked = false;
 	PROC	   *proc;
-	struct timeval timeval;
 
 	/*
 	 * If the first entries in the waitQueue have a greater priority than
@@ -513,29 +512,24 @@ ProcSleep(PROC_QUEUE *waitQueue,/* lock->waitProcs */
 	SpinRelease(spinlock);
 
 	/* --------------
-	 * We set this so we can wake up periodically and check for a deadlock.
+	 * We set this so we can wake up after one second to check for a deadlock.
 	 * If a deadlock is detected, the handler releases the processes
 	 * semaphore and aborts the current transaction.
-	 *
-	 * Need to zero out struct to set the interval and the micro seconds fields
-	 * to 0.
 	 * --------------
 	 */
-	MemSet(&timeval, 0, sizeof(struct timeval));
-	timeval.tv_sec = \
-		(DeadlockCheckTimer ? DeadlockCheckTimer : DEADLOCK_CHECK_TIMER);
 
 	do
 	{
-		int expire;
+		int expired;
 		
 		MyProc->errType = NO_ERROR;		/* reset flag after deadlock check */
 
-		if ((expire = select(0, NULL, NULL, NULL,
-			(deadlock_checked == false) ? &timeval : NULL)) == -1)
-			elog(FATAL, "ProcSleep: Unable to set timer for process wakeup");
+		if (deadlock_checked == false)
+			expired = sleep(DeadlockCheckTimer ? DeadlockCheckTimer : DEADLOCK_CHECK_TIMER);
+		else
+			pause();
 
-		if (expire == 0 /* timeout reached */ && deadlock_checked == false)
+		if (expired == 0 && deadlock_checked == false)
 		{
 			HandleDeadLock();
 			deadlock_checked = true;
