2fc1eed94a69de54a98a74d608d2d5c44cadb541
Bruce Momjian <bruce@momjian.us>
1999-04-13 17:28:35 +0000

    array_in is defined in the system catalog as taking two arguments while it actually takes three. Please apply the following patch.
    Massimo

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -996,7 +996,7 @@ DATA(insert OID = 748 (  array_set		   PGUID 11 f t f 8 f 23 "0 23 0 0 23 23 23 
 DESCR("array");
 DATA(insert OID = 749 (  array_ref		   PGUID 11 f t f 7 f 23 "0 23 0 23 23 23 0" 100 0 0 100 array_ref -));
 DESCR("array");
-DATA(insert OID = 750 (  array_in		   PGUID 11 f t f 2 f 23 "0 0" 100 0 0 100	array_in - ));
+DATA(insert OID = 750 (  array_in		   PGUID 11 f t f 3 f 23 "0 0 23" 100 0 0 100	array_in - ));
 DESCR("array");
 DATA(insert OID = 751 (  array_out		   PGUID 11 f t f 2 f 23 "0 0" 100 0 0 100	array_out - ));
 DESCR("array");
