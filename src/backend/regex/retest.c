5ffbefc3fdd48838911d76e17459610525a7aeb7
Tatsuo Ishii <ishii@postgresql.org>
1999-05-21 06:27:54 +0000

    add retest, a regex testing program

---
+++ src/backend/regex/retest.c
@@ -0,0 +1,42 @@
+/*
+ * a simple regexp debug program
+ * 
+ * $Header$
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "postgres.h"
+#include <regex/regex.h>
+
+int main()
+{
+  int sts;
+  regex_t re;
+  char buf[1024];
+  char *p;
+
+  printf("type in regexp string: ");
+  if (!fgets(buf,sizeof(buf),stdin)) {
+    exit(0);
+  }
+  p = strchr(buf, '\n');
+  if (p) *p = '\0';
+
+  sts = pg95_regcomp(&re, buf, 1);
+  printf("regcomp: parses \"%s\" and returns %d\n",buf, sts);
+  for (;;) {
+    printf("type in target string: ");
+    if (!fgets(buf,sizeof(buf),stdin)) {
+      exit(0);
+    }
+    p = strchr(buf, '\n');
+    if (p) *p = '\0';
+
+    sts = pg95_regexec(&re, buf, 0, 0, 0);
+    printf("regexec: returns %d\n", sts);
+  }
+}
+
+void elog(int lev, const char *fmt,...)
+{}
