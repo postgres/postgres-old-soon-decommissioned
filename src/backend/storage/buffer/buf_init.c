880b185535b0e4845d7c24b3a19d8a3832d4edbd
Bruce Momjian <bruce@momjian.us>
1999-06-26 15:58:28 +0000

    Add var defines for no testandset

--- src/backend/storage/buffer/buf_init.c
+++ src/backend/storage/buffer/buf_init.c
@@ -235,6 +235,8 @@ InitBufferPool(IPCKey key)
 #ifndef HAS_TEST_AND_SET
 	{
 		int			status;
+		extern IpcSemaphoreId WaitIOSemId;
+		extern IpcSemaphoreId WaitCLSemId;
 
 		WaitIOSemId = IpcSemaphoreCreate(IPCKeyGetWaitIOSemaphoreKey(key),
 										 1, IPCProtection, 0, 1, &status);
