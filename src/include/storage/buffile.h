ce7469b7422b02493514e213f5ef60c31c5eb238
Tom Lane <tgl@sss.pgh.pa.us>
1999-10-16 19:49:28 +0000

    Second phase of psort reconstruction project: add bookkeeping logic to recycle storage within sort temp file on a block-by-block basis.  This reduces peak disk usage to essentially just the volume of data being sorted, whereas it had been about 4x the data volume before.

--- src/include/storage/buffile.h
+++ src/include/storage/buffile.h
@@ -37,11 +37,12 @@ typedef struct BufFile BufFile;
 
 extern BufFile *BufFileCreateTemp(void);
 extern BufFile *BufFileCreate(File file);
-extern BufFile *BufFileReaccess(BufFile *file);
 extern void BufFileClose(BufFile *file);
 extern size_t BufFileRead(BufFile *file, void *ptr, size_t size);
 extern size_t BufFileWrite(BufFile *file, void *ptr, size_t size);
 extern int BufFileSeek(BufFile *file, int fileno, long offset, int whence);
 extern void BufFileTell(BufFile *file, int *fileno, long *offset);
+extern int BufFileSeekBlock(BufFile *file, long blknum);
+extern long BufFileTellBlock(BufFile *file);
 
 #endif	 /* BUFFILE_H */
