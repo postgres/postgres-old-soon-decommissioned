5b17d1eff6c045bbf28a42cf95e01cec2e337996
Bruce Momjian <bruce@momjian.us>
2000-01-10 18:27:41 +0000

    More updates for function call interface > 8.

--- src/backend/utils/fmgr/fmgr.c
+++ src/backend/utils/fmgr/fmgr.c
@@ -465,31 +465,6 @@ fmgr_c(FmgrInfo *finfo,
 									  values->data[30], values->data[31]);
 			break;
 		default:
-
-			/*
-			 * XXX Note that functions with >FUNC_MAX_ARGS arguments can only be
-			 * called from inside the system, not from the user level,
-			 * since the catalogs only store FUNC_MAX_ARGS argument types for user
-			 * type-checking!
-			 */
-			returnValue = (*user_fn) (values->data[0], values->data[1],
-									  values->data[2], values->data[3],
-									  values->data[4], values->data[5],
-									  values->data[6], values->data[7],
-									  values->data[8], values->data[9],
-									  values->data[10], values->data[11],
-									  values->data[12], values->data[13],
-									  values->data[14], values->data[15],
-									  values->data[16], values->data[17],
-									  values->data[18], values->data[19],
-									  values->data[20], values->data[21],
-									  values->data[22], values->data[23],
-									  values->data[24], values->data[25],
-									  values->data[26], values->data[27],
-									  values->data[28], values->data[29],
-									  values->data[30], values->data[31]);
-			break;
-		default:
 			elog(ERROR, "fmgr_c: function %u: too many arguments (%d > %d)",
 				 finfo->fn_oid, n_arguments, FUNC_MAX_ARGS);
 			break;
