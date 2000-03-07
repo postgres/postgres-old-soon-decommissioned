844efdcb15dbd66258973bbb1976d2e7ca8b7758
Tom Lane <tgl@sss.pgh.pa.us>
2000-03-07 23:30:53 +0000

    Someone (probably me) forgot about handling of typecasts applied to parameters.

--- src/backend/parser/parse_expr.c
+++ src/backend/parser/parse_expr.c
@@ -78,19 +78,21 @@ transformExpr(ParseState *pstate, Node *expr, int precedence)
 				ParamNo    *pno = (ParamNo *) expr;
 				int			paramno = pno->number;
 				Oid			toid = param_type(paramno);
-				Param	   *param;
+				Param	   *param = makeNode(Param);
 
 				if (!OidIsValid(toid))
 					elog(ERROR, "Parameter '$%d' is out of range", paramno);
-				param = makeNode(Param);
 				param->paramkind = PARAM_NUM;
 				param->paramid = (AttrNumber) paramno;
 				param->paramname = "<unnamed>";
-				param->paramtype = (Oid) toid;
-				param->param_tlist = (List *) NULL;
+				param->paramtype = toid;
+				param->param_tlist = NIL;
 				result = transformIndirection(pstate, (Node *) param,
 											  pno->indirection);
-				/* XXX what about cast (typename) applied to Param ??? */
+				/* cope with typecast applied to param */
+				if (pno->typename != NULL)
+					result = parser_typecast_expression(pstate, result,
+														pno->typename);
 				break;
 			}
 		case T_TypeCast:
@@ -732,6 +734,7 @@ exprTypmod(Node *expr)
  * We assume that a two-argument function named for a datatype, whose
  * output and first argument types are that datatype, and whose second
  * input is an int32 constant, represents a forced length coercion.
+ *
  * XXX It'd be better if the parsetree retained some explicit indication
  * of the coercion, so we didn't need these heuristics.
  */
