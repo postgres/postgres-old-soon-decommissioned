49c84053edb4db30cf273a41917a0d51a3bb3ca7
Tom Lane <tgl@sss.pgh.pa.us>
2000-03-18 18:03:12 +0000

    Modify lexing of multi-char operators per pghackers discussion around 16-Mar-00: trailing + or - is not part of the operator unless the operator also contains characters not present in SQL92-defined operators.  This solves the 'X=-Y' problem without unduly constraining users' choice of operator names --- in particular, no existing Postgres operator names become invalid.
    Also, remove processing of // comments, as agreed in the same thread.

--- src/bin/psql/mainloop.c
+++ src/bin/psql/mainloop.c
@@ -318,8 +318,7 @@ MainLoop(FILE *source)
 			}
 
 			/* single-line comment? truncate line */
-			else if ((line[i] == '-' && line[i + thislen] == '-') ||
-					 (line[i] == '/' && line[i + thislen] == '/'))
+			else if (line[i] == '-' && line[i + thislen] == '-')
 			{
 				line[i] = '\0'; /* remove comment */
 				break;
