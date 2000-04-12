7607b3387ab8a9ec14bbd7064cd8bc6b4f8ec5fd
Bruce Momjian <bruce@momjian.us>
2000-04-12 05:29:10 +0000

    Reverse out macro fix for the time being.

--- src/include/c.h
+++ src/include/c.h
@@ -531,11 +531,9 @@ typedef struct Exception
  *
  */
 #define Trap(condition, exception) \
-		do { \
-			if ((assert_enabled) && (condition)) \
+		{ if ((assert_enabled) && (condition)) \
 				ExceptionalCondition(CppAsString(condition), &(exception), \
-						(char*)NULL, __FILE__, __LINE__); \
-		} while (0)
+						(char*)NULL, __FILE__, __LINE__); }
 
 /*
  *	TrapMacro is the same as Trap but it's intended for use in macros:
@@ -579,11 +577,9 @@ extern int	assert_enabled;
  *
  */
 #define LogTrap(condition, exception, printArgs) \
-		do { \
-			if ((assert_enabled) && (condition)) \
+		{ if ((assert_enabled) && (condition)) \
 				ExceptionalCondition(CppAsString(condition), &(exception), \
-						vararg_format printArgs, __FILE__, __LINE__); \
-		} while (0)
+						vararg_format printArgs, __FILE__, __LINE__); }
 
 /*
  *	LogTrapMacro is the same as LogTrap but it's intended for use in macros:
