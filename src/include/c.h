1d0c09b2f82b0684fa419ca547130214dfcf8d8f
Bruce Momjian <bruce@momjian.us>
2000-06-02 16:33:17 +0000

    Reverse PG_BINARY defines

--- src/include/c.h
+++ src/include/c.h
@@ -896,7 +896,7 @@ extern char *vararg_format(const char *fmt,...);
  * ----------------------------------------------------------------
  */
 
-#ifndef __CYGWIN32__
+#ifdef __CYGWIN32__
 #define	PG_BINARY	0
 #define	PG_BINARY_R	"rb"
 #define	PG_BINARY_W	"wb"
