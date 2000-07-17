63102c01a617588e57fa0194d62af597ded7f3bc
Tom Lane <tgl@sss.pgh.pa.us>
2000-07-17 04:35:55 +0000

    I concur with Andreas: unconditionally including  <sys/fcntl.h>  is not portable.  Seems to have been added for O_BINARY, which is only needed for __CYGWIN32__, so include it here only in that case.

--- src/include/c.h
+++ src/include/c.h
@@ -49,12 +49,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/fcntl.h>
 #include <stddef.h>
 #include <stdarg.h>
 
 #ifdef __CYGWIN32__
 #include <errno.h>
+#include <sys/fcntl.h>		/* ensure O_BINARY is available */
 #endif
 
 /* ----------------------------------------------------------------
