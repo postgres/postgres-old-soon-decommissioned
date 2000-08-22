8c6e9b76bd1851a3aa62e85ca65934a47554e709
Tatsuo Ishii <ishii@postgresql.org>
2000-08-22 12:59:04 +0000

    Make makeObjectName multibyte aware. Currently, it may produce incorrect multibyte sequence while truncating too long names.

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -30,6 +30,10 @@
 #include "utils/relcache.h"
 #include "utils/syscache.h"
 
+#ifdef MULTIBYTE
+#include "mb/pg_wchar.h"
+#endif
+
 void		CheckSelectForUpdate(Query *qry);	/* no points for style... */
 
 static Query *transformStmt(ParseState *pstate, Node *stmt);
@@ -549,6 +553,13 @@ makeObjectName(char *name1, char *name2, char *typename)
 		else
 			name2chars--;
 	}
+
+#ifdef MULTIBYTE
+	if (name1)
+		name1chars = pg_mbcliplen(name1, name1chars, name1chars);
+	if (name2)
+		name2chars = pg_mbcliplen(name2, name2chars, name2chars);
+#endif
 
 	/* Now construct the string using the chosen lengths */
 	name = palloc(name1chars + name2chars + overhead + 1);
