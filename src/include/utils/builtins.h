ffa344c2e510fd125ff8e80237d2b9183c5fab27
Thomas G. Lockhart <lockhart@alumni.caltech.edu>
2000-08-23 06:04:49 +0000

    Add functions to convert to and from text, and to truncate to MAC OUI. Remove hardcoded macaddr_manuf(), which had really old, obsolete info.  Replace this with some contrib/mac/ code to maniag OUI info from IEEE.

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -502,7 +502,9 @@ extern Datum macaddr_eq(PG_FUNCTION_ARGS);
 extern Datum macaddr_ge(PG_FUNCTION_ARGS);
 extern Datum macaddr_gt(PG_FUNCTION_ARGS);
 extern Datum macaddr_ne(PG_FUNCTION_ARGS);
-extern Datum macaddr_manuf(PG_FUNCTION_ARGS);
+extern Datum macaddr_trunc(PG_FUNCTION_ARGS);
+extern Datum macaddr_text(PG_FUNCTION_ARGS);
+extern Datum text_macaddr(PG_FUNCTION_ARGS);
 
 /* numeric.c */
 extern Datum numeric_in(PG_FUNCTION_ARGS);
