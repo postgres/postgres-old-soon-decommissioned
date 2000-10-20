00531d623d16f14b7c11e751bc9dce2384cefd89
Tom Lane <tgl@sss.pgh.pa.us>
2000-10-20 02:53:10 +0000

    Clean up gcc warnings.

--- src/backend/commands/rename.c
+++ src/backend/commands/rename.c
@@ -177,17 +177,19 @@ renameatt(char *relname,
 void
 renamerel(const char *oldrelname, const char *newrelname)
 {
-	int			i;
 	Relation	targetrelation;
 	Relation	relrelation;	/* for RELATION relation */
 	HeapTuple	oldreltup;
 	Oid			reloid;
 	char		relkind;
+	Relation	irelations[Num_pg_class_indices];
+#ifdef OLD_FILE_NAMING
+	int			i;
 	char		oldpath[MAXPGPATH],
 				newpath[MAXPGPATH],
 				toldpath[MAXPGPATH + 10],
 				tnewpath[MAXPGPATH + 10];
-	Relation	irelations[Num_pg_class_indices];
+#endif
 
 	if (!allowSystemTableMods && IsSystemRelationName(oldrelname))
 		elog(ERROR, "renamerel: system relation \"%s\" not renamed",
