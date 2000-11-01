9221b78d4d617279d1c53f4ccef8a951faf9cf0e
Vadim B. Mikheev <vadim4o@yahoo.com>
2000-11-01 20:39:58 +0000

    Forgot to check page LSN and unlock buffer in btree_xlog_delete - fixed. (Thanks to Tatsuo Ishii for finding bug)

--- src/backend/access/nbtree/nbtree.c
+++ src/backend/access/nbtree/nbtree.c
@@ -1018,7 +1018,17 @@ btree_xlog_delete(bool redo, XLogRecPtr lsn, XLogRecord *record)
 	if (PageIsNew((PageHeader) page))
 		elog(STOP, "btree_delete_redo: uninitialized page");
 
+	if (XLByteLE(lsn, PageGetLSN(page)))
+	{
+		UnlockAndReleaseBuffer(buffer);
+		return;
+	}
+
 	PageIndexTupleDelete(page, ItemPointerGetOffsetNumber(&(xlrec->target.tid)));
+
+	PageSetLSN(page, lsn);
+	PageSetSUI(page, ThisStartUpID);
+	UnlockAndWriteBuffer(buffer);
 
 	return;
 }
