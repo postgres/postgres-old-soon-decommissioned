d94316a00eb7df3f4c2f0f4a62e2398601c8a555
Thomas G. Lockhart <lockhart@alumni.caltech.edu>
2000-11-07 16:01:01 +0000

    Enable fallback to string type when argument(s) are of UNKNOWN type.  Same code exactly as for function resolution.  An obvious example is for   select '1' = '01';  which used to throw an error and which now resolves to two text strings.

--- src/backend/parser/parse_oper.c
+++ src/backend/parser/parse_oper.c
@@ -443,8 +443,21 @@ oper_select_candidate(int nargs,
 				}
 				else if (current_category != slot_category)
 				{
-					/* punt if more than one category for this slot */
-					return NULL;
+					/* started out as unknown type, so give preference to string type, if available */
+					if (current_category == STRING_TYPE)
+					{
+						/* forget all previous candidates */
+						candidates = current_candidate;
+						last_candidate = current_candidate;
+					}
+					else if (slot_category == STRING_TYPE)
+					{
+						/* forget this candidate */
+						if (last_candidate)
+							last_candidate->next = current_candidate->next;
+						else
+							candidates = current_candidate->next;
+					}
 				}
 				else if (current_type != slot_type)
 				{
