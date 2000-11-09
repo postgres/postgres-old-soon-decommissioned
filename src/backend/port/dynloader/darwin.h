29c3a4b4b16bb2a9825563e9780e2682cf38ed5e
Peter Eisentraut <peter_e@gmx.net>
2000-11-09 19:00:50 +0000

    Real dynamic loader code

--- src/backend/port/dynloader/darwin.h
+++ src/backend/port/dynloader/darwin.h
@@ -1,5 +1,8 @@
 /* $Header$ */
-void	   *pg_dlopen(char *filename);
-PGFunction	pg_dlsym(void *handle, char *funcname);
+
+#include "fmgr.h"
+
+void	   *pg_dlopen(const char *filename);
+PGFunction	pg_dlsym(void *handle, const char *funcname);
 void		pg_dlclose(void *handle);
-char	   *pg_dlerror();
+const char *pg_dlerror(void);
