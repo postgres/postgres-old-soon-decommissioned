013e761d4861ef88f1550064e2c65d1fdb2d775b
Tatsuo Ishii <ishii@postgresql.org>
2000-11-26 11:35:23 +0000

    Fix for inserting/copying longer multibyte strings into bpchar data types.

--- src/backend/utils/adt/varchar.c
+++ src/backend/utils/adt/varchar.c
@@ -26,12 +26,6 @@
 #endif
 
 
-#ifdef CYR_RECODE
-/* XXX no points for style --- this is actually in utils/init/miscinit.c */
-extern char *convertstr(char *, int, int);
-#endif
-
-
 /*
  * CHAR() and VARCHAR() types are part of the ANSI SQL standard. CHAR()
  * is for blank-padded string whose length is specified in CREATE TABLE.
@@ -85,7 +79,17 @@ bpcharin(PG_FUNCTION_ARGS)
 		atttypmod = len + VARHDRSZ;
 	}
 	else
+#ifdef MULTIBYTE
+	{
+		/*
+		 * truncate multi-byte string preserving multi-byte
+		 * boundary
+		 */
+		len = pg_mbcliplen(s, atttypmod - VARHDRSZ, atttypmod - VARHDRSZ);
+	}
+#else
 		len = atttypmod - VARHDRSZ;
+#endif
 
 	result = (BpChar *) palloc(atttypmod);
 	VARATT_SIZEP(result) = atttypmod;
@@ -102,7 +106,11 @@ bpcharin(PG_FUNCTION_ARGS)
 #endif
 
 	/* blank pad the string if necessary */
+#ifdef MULTIBYTE
+	for (; i < atttypmod - VARHDRSZ; i++)
+#else
 	for (; i < len; i++)
+#endif
 		*r++ = ' ';
 
 	PG_RETURN_BPCHAR_P(result);
@@ -335,7 +343,11 @@ varcharin(PG_FUNCTION_ARGS)
 
 	len = strlen(s) + VARHDRSZ;
 	if (atttypmod >= (int32) VARHDRSZ && len > atttypmod)
+#ifdef MULTIBYTE
+ 		len = pg_mbcliplen(s, len - VARHDRSZ, atttypmod - VARHDRSZ) + VARHDRSZ;
+#else
 		len = atttypmod;		/* clip the string at max length */
+#endif
 
 	result = (VarChar *) palloc(len);
 	VARATT_SIZEP(result) = len;
@@ -389,7 +401,7 @@ varchar(PG_FUNCTION_ARGS)
 #ifdef MULTIBYTE
 
 	/*
-	 * truncate multi-byte string in a way not to break multi-byte
+	 * truncate multi-byte string preserving multi-byte
 	 * boundary
 	 */
 	len = pg_mbcliplen(VARDATA(s), slen - VARHDRSZ, slen - VARHDRSZ);
