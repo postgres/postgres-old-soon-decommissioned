54d1a1d28aa30441b4df49fb21537ac439b182eb
Bruce Momjian <bruce@momjian.us>
2001-01-19 07:03:53 +0000

    Fix alignment

--- src/include/storage/s_lock.h
+++ src/include/storage/s_lock.h
@@ -391,10 +391,11 @@ tas(volatile slock_t *lock)
  */
 
 #define S_UNLOCK(lock) \
-do { \
-	volatile slock_t *lock_ = (volatile slock_t *) (lock); \
-	lock_->sema[0] = lock_->sema[1] = lock_->sema[2] = lock_->sema[3] = -1; \
-} while (0)
+	do { \
+		volatile slock_t *lock_ = (volatile slock_t *) (lock); \
+		lock_->sema[0] = lock_->sema[1] = \
+		lock_->sema[2] = lock_->sema[3] = -1; \
+	} while (0)
 
 #define S_LOCK_FREE(lock)	( *(int *) (((long) (lock) + 15) & ~15) != 0)
 
