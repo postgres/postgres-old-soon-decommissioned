c4da90323e0f8d5a1ec74ff21feb3456f6c1e040
Peter Eisentraut <peter_e@gmx.net>
2001-03-01 19:03:44 +0000

    Add missing include.

--- src/backend/port/dynloader/qnx4.h
+++ src/backend/port/dynloader/qnx4.h
@@ -13,7 +13,9 @@
  *
  *-------------------------------------------------------------------------
  */
-/* System includes */
+
+#include "fmgr.h"
+
 void	   *pg_dlopen(char *filename);
 PGFunction	pg_dlsym(void *handle, char *funcname);
 void		pg_dlclose(void *handle);
