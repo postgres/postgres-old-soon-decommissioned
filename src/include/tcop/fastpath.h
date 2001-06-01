20f64feaaa5602771f0bee0277748d80f556eea8
Tom Lane <tgl@sss.pgh.pa.us>
2001-06-01 15:45:42 +0000

    Remove fastpath.c's lame attempt at caching function lookup info across calls.  This has never actually cached anything, because postgres.c does each fastpath call as a separate transaction command, and so fastpath.c would always decide that its cache was outdated.  If it had worked, it would now be failing for calls of oldstyle functions due to dangling pointers in the FmgrInfo struct.  Rip it out for simplicity and bug- proofing.

--- src/include/tcop/fastpath.h
+++ src/include/tcop/fastpath.h
@@ -3,28 +3,15 @@
  * fastpath.h
  *
  *
- *
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * $Id$
  *
- * NOTES
- *	  This information pulled out of tcop/fastpath.c and put
- *	  here so that the PQfn() in be-pqexec.c could access it.
- *		-cim 2/26/91
- *
  *-------------------------------------------------------------------------
  */
 #ifndef FASTPATH_H
 #define FASTPATH_H
-
-/* ----------------
- *		fastpath #defines
- * ----------------
- */
-#define VAR_LENGTH_RESULT		(-1)
-#define VAR_LENGTH_ARG			(-5)
 
 extern int	HandleFunctionRequest(void);
 
