aefc8e7ad4e3e7148be5fd025b27f1feba8a90bf
Peter Eisentraut <peter_e@gmx.net>
2001-08-07 11:41:17 +0000

    Russian translation by Serguei Mokhov

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
 # $Header$
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= de
+AVAIL_LANGUAGES	:= de ru
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c
 GETTEXT_TRIGGERS:= libpq_gettext
