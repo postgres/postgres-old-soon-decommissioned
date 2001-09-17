6c474297cf0a3a339d1c9e9907f37b8d4839462b
Tom Lane <tgl@sss.pgh.pa.us>
2001-09-17 00:42:42 +0000

    Use portable putenv(), not unportable setenv().

--- src/interfaces/libpgtcl/pgtcl.c
+++ src/interfaces/libpgtcl/pgtcl.c
@@ -45,7 +45,8 @@ Pgtcl_Init(Tcl_Interp *interp)
 	 * Therefore PGCLIENTENCODING must be set to UNICODE for these versions.
 	 */
 	Tcl_GetDouble(interp, Tcl_GetVar(interp, "tcl_version", TCL_GLOBAL_ONLY), &tclversion);
-	if (tclversion >= 8.1) setenv("PGCLIENTENCODING", "UNICODE", 1);
+	if (tclversion >= 8.1)
+		putenv("PGCLIENTENCODING=UNICODE");
 
 	/* register all pgtcl commands */
 	Tcl_CreateCommand(interp,
