c3fa296f8e98927f2b536504edcbd9d8de1195f9
Tatsuo Ishii <ishii@postgresql.org>
2001-10-15 04:52:59 +0000

    Forgot to add this file...

---
+++ src/bin/psql/mbprint.h
@@ -0,0 +1,18 @@
+/* $Id$ */
+#ifndef MBPRINT_H
+#define MBPRINT_H
+
+
+#ifdef MULTIBYTE
+
+#include "mb/pg_wchar.h"
+
+pg_wchar utf2ucs(const unsigned char *c);
+
+unsigned char *mbvalidate(unsigned char *pwcs);
+#endif /* MULTIBYTE */
+
+int pg_wcswidth(unsigned char *pwcs, int len);
+
+#endif /* MBPRINT_H */
+
