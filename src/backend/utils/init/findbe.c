b17a1066c5e36175d82874a98cfa49b3ca510ee9
Bruce Momjian <bruce@momjian.us>
2001-10-29 17:55:41 +0000

    Check for NULL return from getgrgid(), per Seth Hettich

--- src/backend/utils/init/findbe.c
+++ src/backend/utils/init/findbe.c
@@ -103,7 +103,7 @@ ValidateBinary(char *path)
 		if (pwp->pw_gid == buf.st_gid)
 			++in_grp;
 		else if (pwp->pw_name &&
-				 (gp = getgrgid(buf.st_gid)))
+				 (gp = getgrgid(buf.st_gid)) != NULL)
 		{
 			for (i = 0; gp->gr_mem[i]; ++i)
 			{
