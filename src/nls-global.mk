770a14df69602f8e0a84634719bed9c70ff5c834
Peter Eisentraut <peter_e@gmx.net>
2001-11-29 18:59:28 +0000

    NLS updates, most notably fixed zh_TW translations

--- src/nls-global.mk
+++ src/nls-global.mk
@@ -94,8 +94,7 @@ init-po: $(srcdir)/$(CATALOG_NAME).pot
 
 update-po: $(srcdir)/$(CATALOG_NAME).pot
 ifdef MSGMERGE
-#	XXX  should be $(LANGUAGES)?
-	@for lang in $(AVAIL_LANGUAGES); do \
+	@for lang in $(LANGUAGES); do \
 	  echo "merging $$lang:"; \
 	  if $(MSGMERGE) $(srcdir)/$$lang.po $< -o $$lang.po.new; \
 	  then \
