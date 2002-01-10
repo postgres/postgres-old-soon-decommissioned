51bf33beeddfd27d63be93c51a7426382d2ad1c4
Michael Meskes <meskes@postgresql.org>
2002-01-10 10:42:54 +0000

    Include sqlca.h automatically.

--- src/interfaces/ecpg/preproc/ecpg.c
+++ src/interfaces/ecpg/preproc/ecpg.c
@@ -296,8 +296,8 @@ main(int argc, char *const argv[])
 				/* initialize lex */
 				lex_init();
 
-				/* we need two includes */
-				fprintf(yyout, "/* Processed by ecpg (%d.%d.%d) */\n/* These three include files are added by the preprocessor */\n#include <ecpgtype.h>\n#include <ecpglib.h>\n#include <ecpgerrno.h>\n#line 1 \"%s\"\n", MAJOR_VERSION, MINOR_VERSION, PATCHLEVEL, input_filename);
+				/* we need several includes */
+				fprintf(yyout, "/* Processed by ecpg (%d.%d.%d) */\n/* These three include files are added by the preprocessor */\n#include <ecpgtype.h>\n#include <ecpglib.h>\n#include <ecpgerrno.h>\n#include <sqlca.h>\n#line 1 \"%s\"\n", MAJOR_VERSION, MINOR_VERSION, PATCHLEVEL, input_filename);
 
 				/* and parse the source */
 				yyparse();
