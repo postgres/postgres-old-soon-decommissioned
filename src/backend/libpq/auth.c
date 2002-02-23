f1e9b58e6befa442023155476c7085f8d2e99b07
Peter Eisentraut <peter_e@gmx.net>
2002-02-23 04:17:47 +0000

    Heimdal support (Kerberos V implementation from KTH)

--- src/backend/libpq/auth.c
+++ src/backend/libpq/auth.c
@@ -229,7 +229,7 @@ pg_krb5_init(void)
 				 " Kerberos error %d\n", retval);
 		com_err("postgres", retval,
 				"while getting server principal for service %s",
-				pg_krb_server_keyfile);
+				PG_KRB_SRVNAM);
 		krb5_kt_close(pg_krb5_context, pg_krb5_keytab);
 		krb5_free_context(pg_krb5_context);
 		return STATUS_ERROR;
@@ -283,8 +283,15 @@ pg_krb5_recvauth(Port *port)
 	 *
 	 * I have no idea why this is considered necessary.
 	 */
+#if defined(HAVE_KRB5_TICKET_ENC_PART2)
 	retval = krb5_unparse_name(pg_krb5_context,
 							   ticket->enc_part2->client, &kusername);
+#elif defined(HAVE_KRB5_TICKET_CLIENT)
+	retval = krb5_unparse_name(pg_krb5_context,
+							   ticket->client, &kusername);
+#else
+#error "bogus configuration"
+#endif
 	if (retval)
 	{
 		snprintf(PQerrormsg, PQERRORMSG_LENGTH,
