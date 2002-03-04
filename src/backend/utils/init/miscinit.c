760b5f0e485c7a08087d760b575cb98b827270f4
Tom Lane <tgl@sss.pgh.pa.us>
2002-03-04 04:45:27 +0000

    If presented db path has a trailing slash, remove it to avoid generating double slashes in generated filenames.  This is not strictly necessary on standard Unixen, but I'm being a neatnik...

--- src/backend/utils/init/miscinit.c
+++ src/backend/utils/init/miscinit.c
@@ -121,9 +121,11 @@ void
 SetDataDir(const char *dir)
 {
 	char	   *new;
+	int			newlen;
 
 	AssertArg(dir);
 
+	/* If presented path is relative, convert to absolute */
 	if (dir[0] != '/')
 	{
 		char	   *buf;
@@ -163,6 +165,14 @@ SetDataDir(const char *dir)
 		if (!new)
 			elog(FATAL, "out of memory");
 	}
+
+	/*
+	 * Strip any trailing slash.  Not strictly necessary, but avoids
+	 * generating funny-looking paths to individual files.
+	 */
+	newlen = strlen(new);
+	if (newlen > 1 && new[newlen-1] == '/')
+		new[newlen-1] = '\0';
 
 	if (DataDir)
 		free(DataDir);
