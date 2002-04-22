18a9786deb7a805b2cc9269d7f552944f7d4e821
Tom Lane <tgl@sss.pgh.pa.us>
2002-04-22 21:56:06 +0000

    Oops, forgot we had a macro to encapsulate test for type toastability.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -888,10 +888,9 @@ AlterTableAlterColumnFlags(Oid myrelid,
 	{
 		/*
 		 * safety check: do not allow toasted storage modes unless column
-		 * datatype is TOAST-aware.  We assume the datatype's typstorage
-		 * will be 'p' if and only if it ain't TOAST-aware.
+		 * datatype is TOAST-aware.
 		 */
-		if (newstorage == 'p' || get_typstorage(attrtuple->atttypid) != 'p')
+		if (newstorage == 'p' || TypeIsToastable(attrtuple->atttypid))
 			attrtuple->attstorage = newstorage;
 		else
 			elog(ERROR, "ALTER TABLE: Column datatype %s can only have storage \"plain\"",
