9a6748749ebe1a92117aacc017725be2ec7e90c9
Tom Lane <tgl@sss.pgh.pa.us>
2002-04-30 21:01:52 +0000

    Fix obj_description() and col_description() functions to work reliably in presence of schemas.

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -1493,9 +1493,9 @@ DESCR("date difference preserving months and years");
 DATA(insert OID = 1200 (  reltime		   PGNSP PGUID 12 f t f t f i 1  703 "23" 100 0 0 100  int4reltime - _null_ ));
 DESCR("convert int4 to reltime");
 
-DATA(insert OID = 1215 (  obj_description	PGNSP PGUID 14 f t f t f s 2	25 "26 19" 100 0 0 100	"select description from pg_description where objoid = $1 and classoid = (select oid from pg_class where relname = $2) and objsubid = 0" - _null_ ));
+DATA(insert OID = 1215 (  obj_description	PGNSP PGUID 14 f t f t f s 2	25 "26 19" 100 0 0 100	"select description from pg_description where objoid = $1 and classoid = (select oid from pg_class where relname = $2 and relnamespace = PGNSP) and objsubid = 0" - _null_ ));
 DESCR("get description for object id and catalog name");
-DATA(insert OID = 1216 (  col_description	PGNSP PGUID 14 f t f t f s 2	25 "26 23" 100 0 0 100	"select description from pg_description where objoid = $1 and classoid = (select oid from pg_class where relname = \'pg_class\') and objsubid = $2" - _null_ ));
+DATA(insert OID = 1216 (  col_description	PGNSP PGUID 14 f t f t f s 2	25 "26 23" 100 0 0 100	"select description from pg_description where objoid = $1 and classoid = \'pg_catalog.pg_class\'::regclass and objsubid = $2" - _null_ ));
 DESCR("get description for table column");
 
 DATA(insert OID = 1217 (  date_trunc	   PGNSP PGUID 12 f t f t f i 2 1184 "25 1184" 100 0 0 100	timestamptz_trunc - _null_ ));
