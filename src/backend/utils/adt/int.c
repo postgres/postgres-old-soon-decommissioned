72d109d6e438271cd9b8258ed2a3695b3472ca7c
Bruce Momjian <bruce@momjian.us>
2002-06-07 23:41:53 +0000

    Fix for  factorial(0::int2) returning 1, from  sugita@sra.co.jp.

--- src/backend/utils/adt/int.c
+++ src/backend/utils/adt/int.c
@@ -800,7 +800,9 @@ int2fac(PG_FUNCTION_ARGS)
 	int16		arg1 = PG_GETARG_INT16(0);
 	int32		result;
 
-	if (arg1 < 1)
+	if (arg1 == 0)
+		result = 1;
+	else if (arg1 < 1)
 		result = 0;
 	else
 		for (result = 1; arg1 > 0; --arg1)
