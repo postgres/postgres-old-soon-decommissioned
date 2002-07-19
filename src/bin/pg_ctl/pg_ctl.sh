25b0471a99c9abf1dc05d614b18103924e1c2fba
Bruce Momjian <bruce@momjian.us>
2002-07-19 13:50:06 +0000

    Fix "$@" to ${1:+"$@"} for older shells.

--- src/bin/pg_ctl/pg_ctl.sh
+++ src/bin/pg_ctl/pg_ctl.sh
@@ -332,12 +332,12 @@ if [ "$op" = "start" -o "$op" = "restart" ];then
     fi
 
     if [ -n "$logfile" ]; then
-        "$po_path" "$@" </dev/null >>$logfile 2>&1 &
+        "$po_path" ${1:+"$@"} </dev/null >>$logfile 2>&1 &
     else
         # when starting without log file, redirect stderr to stdout, so
         # pg_ctl can be invoked with >$logfile and still have pg_ctl's
         # stderr on the terminal.
-        "$po_path" "$@" </dev/null 2>&1 &
+        "$po_path" ${1:+"$@"} </dev/null 2>&1 &
     fi
 
     # if had an old lockfile, check to see if we were able to start
