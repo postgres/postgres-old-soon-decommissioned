bc8cf1864ca5371c33a5ddd219b16ed85cc7dab8
Bruce Momjian <bruce@momjian.us>
2002-08-15 02:53:35 +0000

    Appears there is a problem finding the opclass when indexing a domain.
    CREATE DOMAIN newint as int4;
    CREATE TABLE tab (col newint unique);
    ERROR:  data type newint has no default operator class for access method
    "btree"
            You must specify an operator class for the index or define a
            default operator class for the data type
    
    
    Specifically, GetDefaultOpClass() finds 0 exact matches and 3 binary
    compatible matches.  Fetching getBaseType() of the attribute fixes the
    problem for domains (see attachment).
    
    Rod Taylor

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -482,6 +482,9 @@ GetDefaultOpClass(Oid attrType, Oid accessMethodId)
 	Oid			exactOid = InvalidOid;
 	Oid			compatibleOid = InvalidOid;
 
+	/* It could be a domain */
+	attrType = getBaseType(attrType);
+
 	/*
 	 * We scan through all the opclasses available for the access method,
 	 * looking for one that is marked default and matches the target type
