dfb0dd717b45ad92ce3e5dc1d1cd0bc0ae94c898
Bruce Momjian <bruce@momjian.us>
2002-08-18 01:39:43 +0000

    Back out tcl patch, per Tom Lane:
    Everytime if I do PQconsumeInput (when the backend channel gets
    readable) I check for the return value. (0 == error) and generate a
    notification manually, e.g. fixed string connection_closed) and pass it to the

--- src/interfaces/libpgtcl/pgtclCmds.c
+++ src/interfaces/libpgtcl/pgtclCmds.c
@@ -419,11 +419,8 @@ Pg_disconnect(ClientData cData, Tcl_Interp *interp, int argc, char *argv[])
 
 #if TCL_MAJOR_VERSION >= 8
 	conn = PgGetConnectionId(interp, argv[1], &connid);
-	if (connid->notifier_channel != NULL) {
-		/* stop listening for NOTIFY events on that channel */
-		PgStopNotifyEventSource(connid,1);
+	if (connid->notifier_channel != NULL)
 		Tcl_UnregisterChannel(interp, connid->notifier_channel);
-	}
 #endif
 
 	return Tcl_UnregisterChannel(interp, conn_chan);
