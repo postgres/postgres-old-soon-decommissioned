9cc6da16e1e4186fa4f2ea0ee9060ffb223ae446
Peter Eisentraut <peter_e@gmx.net>
2002-09-14 13:46:24 +0000

    Translation updates

--- src/backend/commands/user.c
+++ src/backend/commands/user.c
@@ -928,8 +928,12 @@ DropUser(DropUserStmt *stmt)
 							   PointerGetDatum(user),
 							   0, 0, 0);
 		if (!HeapTupleIsValid(tuple))
-			elog(ERROR, "DROP USER: user \"%s\" does not exist%s", user,
-				 (length(stmt->users) > 1) ? " (no users removed)" : "");
+		{
+			if (length(stmt->users) > 1)
+				elog(ERROR, "DROP USER: user \"%s\" does not exist (no users removed)", user);
+			else
+				elog(ERROR, "DROP USER: user \"%s\" does not exist", user);
+		}
 
 		usesysid = ((Form_pg_shadow) GETSTRUCT(tuple))->usesysid;
 
--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
 # $Header$
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= 
+AVAIL_LANGUAGES	:= ru
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
