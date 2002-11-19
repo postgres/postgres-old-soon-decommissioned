b15a707abbeb3ed672ddda2e17c769f88899932f
Bruce Momjian <bruce@momjian.us>
2002-11-19 17:21:40 +0000

    Most of the code follows the American spelling of the word, which is "canceled", so I changed the one remaining usage of the British spelling ("cancelled") over to the former, and updated the translation files appropriately.
    Neil Conway

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -1158,7 +1158,7 @@ ProcessInterrupts(void)
 		QueryCancelPending = false;
 		ImmediateInterruptOK = false;	/* not idle anymore */
 		DisableNotifyInterrupt();
-		elog(ERROR, "Query was cancelled.");
+		elog(ERROR, "Query was canceled.");
 	}
 	/* If we get here, do nothing (probably, QueryCancelPending was reset) */
 }
@@ -1781,7 +1781,7 @@ PostgresMain(int argc, char *argv[], const char *username)
 	if (!IsUnderPostmaster)
 	{
 		puts("\nPOSTGRES backend interactive interface ");
-		puts("$Revision: 1.309 $ $Date: 2002/11/15 00:47:22 $\n");
+		puts("$Revision: 1.310 $ $Date: 2002/11/19 17:21:40 $\n");
 	}
 
 	/*
