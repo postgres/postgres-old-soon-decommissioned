f7f983b316221b84a1bb6abf89d10a7d7ae6401b
Bruce Momjian <bruce@momjian.us>
2003-01-11 05:01:03 +0000

    Rewrite for-loop, because this is not the Obfuscated C Code Contest.
    Manfred Koizar

--- src/backend/storage/page/bufpage.c
+++ src/backend/storage/page/bufpage.c
@@ -409,8 +409,7 @@ PageIndexTupleDelete(Page page, OffsetNumber offnum)
 	unsigned	offset;
 	int			nbytes;
 	int			offidx;
-	int			nline,
-				i;
+	int			nline;
 
 	/*
 	 * As with PageRepairFragmentation, paranoia seems justified.
@@ -479,11 +478,12 @@ PageIndexTupleDelete(Page page, OffsetNumber offnum)
 	 */
 	if (!PageIsEmpty(page))
 	{
+		int i;
 		nline--;				/* there's one less than when we started */
-		for (i = nline; --i >= 0;)
+		for (i = 1; i <= nline; i++)
 		{
-			if (PageGetItemId(phdr, i + 1)->lp_off <= offset)
-				PageGetItemId(phdr, i + 1)->lp_off += size;
+			if (PageGetItemId(phdr, i)->lp_off <= offset)
+				PageGetItemId(phdr, i)->lp_off += size;
 		}
 	}
 }
