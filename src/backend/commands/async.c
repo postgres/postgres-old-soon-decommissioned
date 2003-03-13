c2aaacba656aa91cc4dc52f6722931f83ee28214
Tom Lane <tgl@sss.pgh.pa.us>
2003-03-13 23:44:07 +0000

    Async_NotifyHandler must save and restore ImmediateInterruptOK.  Fixes known problem with failure to respond to 'pg_ctl stop -m fast', and probable problems if SIGINT or SIGTERM arrives while processing a SIGUSR2 interrupt that arrived while waiting for a new client query.

--- src/backend/commands/async.c
+++ src/backend/commands/async.c
@@ -599,6 +599,16 @@ Async_NotifyHandler(SIGNAL_ARGS)
 
 	if (notifyInterruptEnabled)
 	{
+		bool		save_ImmediateInterruptOK = ImmediateInterruptOK;
+
+		/*
+		 * We may be called while ImmediateInterruptOK is true; turn it off
+		 * while messing with the NOTIFY state.  (We would have to save
+		 * and restore it anyway, because PGSemaphore operations inside
+		 * ProcessIncomingNotify() might reset it.)
+		 */
+		ImmediateInterruptOK = false;
+
 		/*
 		 * I'm not sure whether some flavors of Unix might allow another
 		 * SIGUSR2 occurrence to recursively interrupt this routine. To
@@ -626,6 +636,13 @@ Async_NotifyHandler(SIGNAL_ARGS)
 					elog(LOG, "Async_NotifyHandler: done");
 			}
 		}
+
+		/*
+		 * Restore ImmediateInterruptOK, and check for interrupts if needed.
+		 */
+		ImmediateInterruptOK = save_ImmediateInterruptOK;
+		if (save_ImmediateInterruptOK)
+			CHECK_FOR_INTERRUPTS();
 	}
 	else
 	{
