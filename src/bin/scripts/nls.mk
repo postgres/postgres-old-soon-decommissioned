0bd055405d6cf42741f870959b6ac2f1a4906116
Peter Eisentraut <peter_e@gmx.net>
2003-03-18 22:19:47 +0000

    Reimplement create and drop scripts in C, to reduce repetitive connections, increase robustness, add NLS, and prepare for Windows port. (vacuumdb and clusterdb will follow later.)

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,8 +1,7 @@
 # $Header$
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := de
+AVAIL_LANGUAGES := 
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
-                   clusterdb.c vacuumdb.c \
                    common.c
 GETTEXT_TRIGGERS:= _ simple_prompt
