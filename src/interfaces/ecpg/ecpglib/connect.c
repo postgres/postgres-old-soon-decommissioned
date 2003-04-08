448b08e7f68a699a683ce71eb69eee291bbfe6cf
Michael Meskes <meskes@postgresql.org>
2003-04-08 12:34:25 +0000

    Added some more informix compatibility functions.

--- src/interfaces/ecpg/ecpglib/connect.c
+++ src/interfaces/ecpg/ecpglib/connect.c
@@ -11,6 +11,8 @@
 static struct connection *all_connections = NULL,
 		   *actual_connection = NULL;
 
+extern enum COMPAT_MODE ecpg_compat_mode;
+
 struct connection *
 ECPGget_connection(const char *connection_name)
 {
@@ -267,13 +269,26 @@ ECPGconnect(int lineno, const char *name, const char *user, const char *passwd, 
 			   *tmp,
 			   *port = NULL,
 			   *realname = NULL,
-			   *options = NULL;
+			   *options = NULL,
+			   *envname;
 
 	ECPGinit_sqlca();
 
 	if ((this = (struct connection *) ECPGalloc(sizeof(struct connection), lineno)) == NULL)
 		return false;
 
+	if (ecpg_compat_mode == ECPG_COMPAT_INFORMIX)
+	{
+		/* Informix uses an environment variable DBPATH that overrides
+		 * the connection parameters given here */
+		envname = getenv("DBPATH");
+		if (envname)
+		{
+			free(dbname);
+			dbname=strdup(envname);
+		}
+	}
+	
 	if (dbname == NULL && connection_name == NULL)
 		connection_name = "DEFAULT";
 
