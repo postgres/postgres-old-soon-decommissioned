2d5a5c84319763a0e3c23e9ffb665e9c0b4d8a61
Tom Lane <tgl@sss.pgh.pa.us>
2003-04-27 23:56:53 +0000

    Make getaddrinfo's behavior with NULL node parameter agree with the Linux man page for it.  This error was preventing CVS tip from accepting remote connections.

--- src/port/getaddrinfo.c
+++ src/port/getaddrinfo.c
@@ -77,12 +77,14 @@ getaddrinfo(const char *node, const char *service,
 			if (hp->h_addrtype != AF_INET)
 				return EAI_ADDRFAMILY;
 
-			memmove(&(sin.sin_addr), hp->h_addr, hp->h_length);
+			memcpy(&(sin.sin_addr), hp->h_addr, hp->h_length);
 		}
 	}
 	else
 	{
 		if (hints->ai_flags & AI_PASSIVE)
+			sin.sin_addr.s_addr = htonl(INADDR_ANY);
+		else
 			sin.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	}
 
