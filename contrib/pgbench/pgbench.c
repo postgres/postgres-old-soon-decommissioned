3c26756e2797222e57c4f86e571087ef8580bc65
Tatsuo Ishii <ishii@postgresql.org>
2003-06-10 09:07:15 +0000

    Fix uninitialized memory bug Add support for PGHOST, PGPORT, PGUSER environment variables

--- contrib/pgbench/pgbench.c
+++ contrib/pgbench/pgbench.c
@@ -694,6 +694,14 @@ main(int argc, char **argv)
 
 	PGconn	   *con;
 	PGresult   *res;
+	char	   *env;
+
+	if ((env = getenv("PGHOST")) != NULL && *env != '\0')
+		pghost = env;
+	if ((env = getenv("PGPORT")) != NULL && *env != '\0')
+		pgport = env;
+	else if ((env = getenv("PGUSER")) != NULL && *env != '\0')
+		login = env;
 
 	while ((c = getopt(argc, argv, "ih:nvp:dc:t:s:U:P:CNSl")) != -1)
 	{
@@ -788,8 +796,11 @@ main(int argc, char **argv)
 		dbName = argv[optind];
 	else
 	{
-		dbName = getenv("USER");
-		if (dbName == NULL)
+		if ((env = getenv("PGDATABASE")) != NULL && *env != '\0')
+			dbName = env;
+		else if (login != NULL && *login != '\0')
+			dbName = login;
+		else
 			dbName = "";
 	}
 
@@ -802,7 +813,7 @@ main(int argc, char **argv)
 	remains = nclients;
 
 	state = (CState *) malloc(sizeof(*state) * nclients);
-	memset(state, 0, sizeof(*state));
+	memset(state, 0, sizeof(*state) * nclients);
 
 	if (use_log)
 	{
