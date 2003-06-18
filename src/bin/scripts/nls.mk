fbb06c6e2738ce9a53739d6aa070ad2b7e93c9af
Peter Eisentraut <peter_e@gmx.net>
2003-06-18 12:19:11 +0000

    Change clusterdb and vacuumdb into C programs.

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -3,5 +3,6 @@ CATALOG_NAME    := pgscripts
 AVAIL_LANGUAGES := 
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
+                   clusterdb.c vacuumdb.c \
                    common.c
 GETTEXT_TRIGGERS:= _ simple_prompt
