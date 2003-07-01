324daaeec9c1bf688541112c87630bea9586ba7e
Tom Lane <tgl@sss.pgh.pa.us>
2003-07-01 01:28:32 +0000

    Department of second thoughts: even if we can't run the full parser on a SQL function with polymorphic inputs, we can at least run the raw parser to catch silly syntactic errors.

--- src/backend/catalog/pg_proc.c
+++ src/backend/catalog/pg_proc.c
@@ -639,23 +639,31 @@ fmgr_sql_validator(PG_FUNCTION_ARGS)
 		}
 	}
 
+	tmp = SysCacheGetAttr(PROCOID, tuple, Anum_pg_proc_prosrc, &isnull);
+	if (isnull)
+		elog(ERROR, "null prosrc");
+
+	prosrc = DatumGetCString(DirectFunctionCall1(textout, tmp));
+
 	/*
-	 * We can't precheck the function definition if there are any polymorphic
-	 * input types, because actual datatypes of expression results will be
-	 * unresolvable.  The check will be done at runtime instead.
+	 * We can't do full prechecking of the function definition if there are
+	 * any polymorphic input types, because actual datatypes of expression
+	 * results will be unresolvable.  The check will be done at runtime
+	 * instead.
+	 *
+	 * We can run the text through the raw parser though; this will at
+	 * least catch silly syntactic errors.
 	 */
 	if (!haspolyarg)
 	{
-		tmp = SysCacheGetAttr(PROCOID, tuple, Anum_pg_proc_prosrc, &isnull);
-		if (isnull)
-			elog(ERROR, "null prosrc");
-
-		prosrc = DatumGetCString(DirectFunctionCall1(textout, tmp));
-
 		querytree_list = pg_parse_and_rewrite(prosrc,
 											  proc->proargtypes,
 											  proc->pronargs);
 		check_sql_fn_retval(proc->prorettype, functyptype, querytree_list);
+	}
+	else
+	{
+		querytree_list = pg_parse_query(prosrc);
 	}
 
 	ReleaseSysCache(tuple);
