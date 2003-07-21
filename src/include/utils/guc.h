161c1bc741eb0ae29b53a9bd532845d9ae3e293a
Bruce Momjian <bruce@momjian.us>
2003-07-21 21:02:12 +0000

    Back out comment on  GucContexts.

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -52,9 +52,6 @@
  * non-super users.
  *
  * USERSET options can be set by anyone any time.
- *
- * When updating the GucContexts, please make sure to update the
- * corresponding GucContext_names [] entries in pg_guc.c.
  */
 typedef enum
 {
