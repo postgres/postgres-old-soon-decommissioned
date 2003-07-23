fe2d1a5c7311689cea0419511d6feb59a45ab2e3
Peter Eisentraut <peter_e@gmx.net>
2003-07-23 08:49:30 +0000

    Update German translations.

---
+++ src/bin/scripts/nls.mk
@@ -0,0 +1,8 @@
+# $Header$
+CATALOG_NAME    := pgscripts
+AVAIL_LANGUAGES := de
+GETTEXT_FILES   := createdb.c createlang.c createuser.c \
+                   dropdb.c droplang.c dropuser.c \
+                   clusterdb.c vacuumdb.c \
+                   common.c
+GETTEXT_TRIGGERS:= _ simple_prompt
