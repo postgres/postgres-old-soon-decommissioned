ac40dca19f7b41e1fdcb1f7e25fc49abeadffc64
Peter Eisentraut <peter_e@gmx.net>
2003-08-01 16:19:14 +0000

    Russian translation updates by Serguei Mokhov

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
 # $Header$
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de
+AVAIL_LANGUAGES := cs de ru
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
