b67a8e8e0cd7b2bafde8a4619c599c3429db81a9
Bruce Momjian <bruce@momjian.us>
2003-09-01 04:15:51 +0000

    Rename DoIt to changeVar, for clarity.

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -127,7 +127,7 @@ extern void BeginReportingGUCOptions(void);
 extern void ParseLongOption(const char *string, char **name, char **value);
 extern bool set_config_option(const char *name, const char *value,
 				  GucContext context, GucSource source,
-				  bool isLocal, bool DoIt);
+				  bool isLocal, bool changeVal);
 extern void ShowGUCConfigOption(const char *name, DestReceiver *dest);
 extern void ShowAllGUCConfig(DestReceiver *dest);
 extern char *GetConfigOptionByName(const char *name, const char **varname);
