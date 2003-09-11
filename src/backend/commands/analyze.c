62f53035e362d94168f71245c4536fe955a84b6c
Tom Lane <tgl@sss.pgh.pa.us>
2003-09-11 23:12:31 +0000

    Message in the other exit from acquire_sample_rows(), as per update from Mark Kirkwood.  Also show the sample size.

--- src/backend/commands/analyze.c
+++ src/backend/commands/analyze.c
@@ -539,6 +539,12 @@ acquire_sample_rows(Relation onerel, HeapTuple *rows, int targrows,
 	if (!HeapTupleIsValid(tuple))
 	{
 		*totalrows = (double) numrows;
+
+		ereport(elevel,
+				(errmsg("\"%s\": %u pages, %d rows sampled, %.0f estimated total rows",
+						RelationGetRelationName(onerel),
+						onerel->rd_nblocks, numrows, *totalrows)));
+
 		return numrows;
 	}
 
@@ -691,9 +697,9 @@ pageloop:;
 	 * Emit some interesting relation info 
 	 */
 	ereport(elevel,
-			(errmsg("\"%s\": %u pages, %.1f average rows/page in sample, %.0f estimated rows",
+			(errmsg("\"%s\": %u pages, %d rows sampled, %.0f estimated total rows",
 					RelationGetRelationName(onerel),
-					onerel->rd_nblocks, tuplesperpage, *totalrows)));
+					onerel->rd_nblocks, numrows, *totalrows)));
 
 	return numrows;
 }
