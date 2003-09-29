76b3a6ffa52da9a0ae33afa3c5fc5f8ca7a30ff1
Peter Eisentraut <peter_e@gmx.net>
2003-09-29 16:37:29 +0000

    Eliminate another gratuitous message wording difference.

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -106,7 +106,7 @@ DefineIndex(RangeVar *heapRelation,
 		rel->rd_rel->relkind != RELKIND_UNCATALOGED)
 		ereport(ERROR,
 				(errcode(ERRCODE_WRONG_OBJECT_TYPE),
-				 errmsg("relation \"%s\" is not a table",
+				 errmsg("\"%s\" is not a table",
 						heapRelation->relname)));
 
 	relationId = RelationGetRelid(rel);
@@ -636,7 +636,7 @@ ReindexTable(RangeVar *relation, bool force /* currently unused */ )
 		((Form_pg_class) GETSTRUCT(tuple))->relkind != RELKIND_TOASTVALUE)
 		ereport(ERROR,
 				(errcode(ERRCODE_WRONG_OBJECT_TYPE),
-				 errmsg("relation \"%s\" is not a table",
+				 errmsg("\"%s\" is not a table",
 						relation->relname)));
 
 	/* Check permissions */
