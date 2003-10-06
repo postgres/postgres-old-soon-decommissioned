2f4a1e08940e7ba9348279b00f0847e2763663cd
Peter Eisentraut <peter_e@gmx.net>
2003-10-06 16:31:16 +0000

    New Slovene translations

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
 # $Header$
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= cs de es fr hr nb pt_BR ru sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hr nb pt_BR ru sl sv zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-secure.c
 GETTEXT_TRIGGERS:= libpq_gettext pqInternalNotice:2
