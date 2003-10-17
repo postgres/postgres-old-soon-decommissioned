eeab8ebb22413e16cb55385310ebf3cf1ebb95c1
Tom Lane <tgl@sss.pgh.pa.us>
2003-10-17 01:14:26 +0000

    Adjust display of actual runtimes in EXPLAIN output to use three fractional digits, and label it 'ms' not 'msec', for consistency with psql's \timing display.  Per recent discussions.

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -270,7 +270,7 @@ ExplainOnePlan(QueryDesc *queryDesc, ExplainStmt *stmt,
 	if (es->printCost)
 	{
 		if (stmt->analyze)
-			appendStringInfo(str, "Total runtime: %.2f msec\n",
+			appendStringInfo(str, "Total runtime: %.3f ms\n",
 							 1000.0 * totaltime);
 		do_text_output_multiline(tstate, str->data);
 	}
@@ -582,7 +582,7 @@ explain_outNode(StringInfo str,
 		{
 			double		nloops = planstate->instrument->nloops;
 
-			appendStringInfo(str, " (actual time=%.2f..%.2f rows=%.0f loops=%.0f)",
+			appendStringInfo(str, " (actual time=%.3f..%.3f rows=%.0f loops=%.0f)",
 						1000.0 * planstate->instrument->startup / nloops,
 						  1000.0 * planstate->instrument->total / nloops,
 							 planstate->instrument->ntuples / nloops,
