377a9bcfe45fc09d8cf19e48d440b509f0988859
Tom Lane <tgl@sss.pgh.pa.us>
2003-11-24 17:25:14 +0000

    Avoid using string literal with embedded newline.

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -4774,13 +4774,13 @@ dumpOneConversion(Archive *fout, ConvInfo *convinfo)
 	selectSourceSchema(convinfo->connamespace->nspname);
 
 	/* Get conversion-specific details */
-	appendPQExpBuffer(query, "SELECT conname,
-					pg_catalog.pg_encoding_to_char(conforencoding) AS conforencoding,
-					pg_catalog.pg_encoding_to_char(contoencoding) AS contoencoding,
-					conproc, condefault
-				FROM pg_catalog.pg_conversion c
-				WHERE c.oid = '%s'::pg_catalog.oid",
-					convinfo->oid);
+	appendPQExpBuffer(query, "SELECT conname, "
+					  "pg_catalog.pg_encoding_to_char(conforencoding) AS conforencoding, "
+					  "pg_catalog.pg_encoding_to_char(contoencoding) AS contoencoding, "
+					  "conproc, condefault "
+					  "FROM pg_catalog.pg_conversion c "
+					  "WHERE c.oid = '%s'::pg_catalog.oid",
+					  convinfo->oid);
 
 	res = PQexec(g_conn, query->data);
 	if (!res ||
