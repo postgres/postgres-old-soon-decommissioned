73957aa403e99aa85d6fe5c501fde1dd60a157eb
Bruce Momjian <bruce@momjian.us>
2003-12-01 22:34:48 +0000

    This makes help like this:
       \lo_export LOBOID FILE
       \lo_import FILE [COMMENT]
       \lo_list
       \lo_unlink LOBOID    large object operations
    
    Instead of not saying anything about what arguments are required.
    
    Christopher Kings-Lynne

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -246,10 +246,10 @@ slashUsage(unsigned short int pager)
 
 	fprintf(output, _("Copy, Large Object\n"));
 	fprintf(output, _("  \\copy ...      perform SQL COPY with data stream to the client host\n"));
-	fprintf(output, _("  \\lo_export\n"
-					  "  \\lo_import\n"
+	fprintf(output, _("  \\lo_export LOBOID FILE\n"
+					  "  \\lo_import FILE [COMMENT] \n"
 					  "  \\lo_list\n"
-					  "  \\lo_unlink     large object operations\n"));
+					  "  \\lo_unlink LOBOID    large object operations\n"));
 
 	if (output != stdout)
 	{
