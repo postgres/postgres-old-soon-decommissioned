ae92feae947cd1de733e0ec10e352f0a513016ef
Bruce Momjian <bruce@momjian.us>
2003-12-25 03:36:24 +0000

    > > I have no idea if this in Oracle or not.  But it's something I > > needed, and other people in the past asked about it too. > > It is in Oracle, but you aren't exactly on the spot.  It should be > > IYYY - 4 digits  ('2003') > IYY  - 3 digits  ('003') > IY   - 2 digits  ('03') > I    - 1 digit   ('3')
    Here is an updated patch that does that.
    
    Kurt Roeckx

--- src/include/utils/timestamp.h
+++ src/include/utils/timestamp.h
@@ -248,5 +248,6 @@ extern void GetEpochTime(struct tm * tm);
 
 extern void isoweek2date(int woy, int *year, int *mon, int *mday);
 extern int	date2isoweek(int year, int mon, int mday);
+extern int	date2isoyear(int year, int mon, int mday);
 
 #endif   /* TIMESTAMP_H */
