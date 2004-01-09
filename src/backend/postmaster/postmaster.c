3a9e94818439b93c69834c6b8f806b0ee0fd3930
Bruce Momjian <bruce@momjian.us>
2004-01-09 23:27:20 +0000

    Remove ereport() from failed connection; already done, per Tom.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2447,11 +2447,7 @@ BackendInit(Port *port)
 	status = ProcessStartupPacket(port, false);
 
 	if (status != STATUS_OK)
-	{
-		ereport(LOG,
-				(errmsg("connection startup failed")));
 		proc_exit(0);
-	}
 
 	/*
 	 * Now that we have the user and database name, we can set the process
