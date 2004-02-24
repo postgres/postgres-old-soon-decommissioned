2003d879da7894bacf58023f454c0919d3e829b9
Tom Lane <tgl@sss.pgh.pa.us>
2004-02-24 22:59:22 +0000

    anyarray really needs to be declared with typalign = 'd', so that entries in pg_statistic are correctly aligned if they contain values that require double alignment.  Too bad we cannot force initdb for this in 7.4 branch.

--- src/include/catalog/pg_type.h
+++ src/include/catalog/pg_type.h
@@ -525,7 +525,7 @@ DATA(insert OID = 2275 ( cstring		PGNSP PGUID -2 f p t \054 0 0 cstring_in cstri
 #define CSTRINGOID		2275
 DATA(insert OID = 2276 ( any			PGNSP PGUID  4 t p t \054 0 0 any_in any_out - -	i p f 0 -1 0 _null_ _null_ ));
 #define ANYOID			2276
-DATA(insert OID = 2277 ( anyarray		PGNSP PGUID -1 f p t \054 0 0 anyarray_in anyarray_out anyarray_recv anyarray_send	i x f 0 -1 0 _null_ _null_ ));
+DATA(insert OID = 2277 ( anyarray		PGNSP PGUID -1 f p t \054 0 0 anyarray_in anyarray_out anyarray_recv anyarray_send	d x f 0 -1 0 _null_ _null_ ));
 #define ANYARRAYOID		2277
 DATA(insert OID = 2278 ( void			PGNSP PGUID  4 t p t \054 0 0 void_in void_out - -	i p f 0 -1 0 _null_ _null_ ));
 #define VOIDOID			2278
