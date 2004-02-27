639a32ac94103c7526eb29986af9a9d097bcbb99
Tom Lane <tgl@sss.pgh.pa.us>
2004-02-27 21:42:10 +0000

    process_implied_equality must copy the substructure of the clauses it is generating, to avoid problems when subselects are involved.  Per report from Damon Hart.

--- src/backend/optimizer/plan/initsplan.c
+++ src/backend/optimizer/plan/initsplan.c
@@ -768,11 +768,16 @@ process_implied_equality(Query *root,
 				 errmsg("equality operator for types %s and %s should be merge-joinable, but isn't",
 						format_type_be(ltype), format_type_be(rtype))));
 
+	/*
+	 * Now we can build the new clause.  Copy to ensure it shares no
+	 * substructure with original (this is necessary in case there are
+	 * subselects in there...)
+	 */
 	clause = make_opclause(oprid(eq_operator),	/* opno */
 						   BOOLOID,		/* opresulttype */
 						   false,		/* opretset */
-						   (Expr *) item1,
-						   (Expr *) item2);
+						   (Expr *) copyObject(item1),
+						   (Expr *) copyObject(item2));
 
 	ReleaseSysCache(eq_operator);
 
