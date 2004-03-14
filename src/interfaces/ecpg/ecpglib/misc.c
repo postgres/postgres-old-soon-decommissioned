c77444cf044ad4fc5f89d7f80b7af2f0e7b03738
Michael Meskes <meskes@postgresql.org>
2004-03-14 12:16:30 +0000

    - Fixed Informix compat math functions to cope with the situations   where one argument takes the result. - Applied thread patches by Lee Kindness

--- src/interfaces/ecpg/ecpglib/misc.c
+++ src/interfaces/ecpg/ecpglib/misc.c
@@ -118,10 +118,15 @@ ECPGinit(const struct connection * con, const char *connection_name, const int l
 }
 
 #ifdef ENABLE_THREAD_SAFETY
-static void
-ecpg_sqlca_key_init(void)
+static void *ecpg_sqlca_key_destructor(void *arg)
 {
-	pthread_key_create(&sqlca_key, NULL);
+  if( arg != NULL )
+    free(arg); /* sqlca structure allocated in ECPGget_sqlca */
+}
+
+static void ecpg_sqlca_key_init(void)
+{
+  pthread_key_create(&sqlca_key, ecpg_sqlca_key_destructor);
 }
 #endif
 
