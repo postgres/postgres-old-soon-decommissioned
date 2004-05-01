550bac7c40c9d5e4e5249974f273fac54519e9ac
Bruce Momjian <bruce@momjian.us>
2004-05-01 01:34:47 +0000

    Rename function to be less win32 specific.

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------------
  *
  * pgtz.c
- *    Timezone Library Integration Functions
+ *	  Timezone Library Integration Functions
  *
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  *
@@ -16,27 +16,33 @@
 
 
 static char tzdir[MAXPGPATH];
-static int done_tzdir = 0;
-char *pgwin32_TZDIR(void) {
-	char *p;
+static int	done_tzdir = 0;
+char *
+pg_TZDIR(void)
+{
+	char	   *p;
+
 	if (done_tzdir)
 		return tzdir;
 
 #ifndef WIN32
 	StrNCpy(tzdir, PGDATADIR, MAXPGPATH);
 #else
-	if (GetModuleFileName(NULL,tzdir,MAXPGPATH) == 0)
+	if (GetModuleFileName(NULL, tzdir, MAXPGPATH) == 0)
 		return NULL;
 #endif
-	
+
 	canonicalize_path(tzdir);
+#if 0
 	if ((p = last_path_separator(tzdir)) == NULL)
 		return NULL;
 	else
 		*p = '\0';
-	
-	strcat(tzdir,"/timezone");
+#endif
 
-	done_tzdir=1;
+	strcat(tzdir, "/timezone");
+	puts(tzdir);
+
+	done_tzdir = 1;
 	return tzdir;
 }
