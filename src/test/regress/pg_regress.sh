8b73ef75dcda88b3a8f3a25488324845f3591a97
Bruce Momjian <bruce@momjian.us>
2004-05-02 21:30:08 +0000

    Use $build_platform for PWD flag test, per suggestion from Peter.

--- src/test/regress/pg_regress.sh
+++ src/test/regress/pg_regress.sh
@@ -211,8 +211,8 @@ esac
 # Set up pwd to give a win32 happy pathname
 # ----------
 
-case `uname` in
-     MINGW32*)
+case $build_platform in
+    *-*-mingw32*)
         PWDFLAGS=-W;;
     *)
         PWDFLAGS=;;
