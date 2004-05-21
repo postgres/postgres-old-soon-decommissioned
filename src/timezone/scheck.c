a5f582c3985fd4c3bc1dd248259b77d205fdd0ad
Tom Lane <tgl@sss.pgh.pa.us>
2004-05-21 20:59:10 +0000

    pgindent did a pretty awful job on the timezone code, particularly with respect to doubly-starred comment blocks.  Do some manual cleanup.

--- src/timezone/scheck.c
+++ src/timezone/scheck.c
@@ -1,3 +1,11 @@
+/*
+ * This file is in the public domain, so clarified as of
+ * 1996-06-05 by Arthur David Olson (arthur_david_olson@nih.gov).
+ *
+ * IDENTIFICATION
+ *	  $PostgreSQL$
+ */
+
 #include "postgres.h"
 
 #include "private.h"
