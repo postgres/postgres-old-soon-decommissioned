73b59e12e2daa173b7d852e954e95750c0497a0d
Tom Lane <tgl@sss.pgh.pa.us>
2004-05-21 16:06:23 +0000

    Fix random breakage in exec.c for platforms where strdup is a macro.

--- src/include/port.h
+++ src/include/port.h
@@ -60,7 +60,7 @@ extern void get_pkglib_path(const char *my_exec_path, char *ret_path);
 
 /* Portable way to find binaries */
 extern int find_my_exec(const char *argv0, char *retpath);
-extern int find_other_exec(const char *argv0, char const *target,
+extern int find_other_exec(const char *argv0, const char *target,
 						   const char *versionstr, char *retpath);
 #if defined(__CYGWIN__) || defined(WIN32)
 #define EXE ".exe"
