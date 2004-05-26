dae4ef09399cfb97bd1a17bd86e2f0842ae27b46
Tom Lane <tgl@sss.pgh.pa.us>
2004-05-26 19:44:15 +0000

    Reduce the minimum allocable chunk size to 8 bytes (from 16).  Now that ListCells are only 8 bytes instead of 12 (on 4-byte-pointer machines anyway), it's worth maintaining a separate freelist for 8-byte objects. Remembering that alloc chunks carry 8 bytes of overhead, this should reduce the net storage requirement for a long List by about a third.

--- src/backend/utils/mmgr/aset.c
+++ src/backend/utils/mmgr/aset.c
@@ -85,7 +85,7 @@
  * CAUTION: ALLOC_MINBITS must be large enough so that
  * 1<<ALLOC_MINBITS is at least MAXALIGN,
  * or we may fail to align the smallest chunks adequately.
- * 16-byte alignment is enough on all currently known machines.
+ * 8-byte alignment is enough on all currently known machines.
  *
  * With the current parameters, request sizes up to 8K are treated as chunks,
  * larger requests go into dedicated blocks.  Change ALLOCSET_NUM_FREELISTS
@@ -93,8 +93,8 @@
  *--------------------
  */
 
-#define ALLOC_MINBITS		4	/* smallest chunk size is 16 bytes */
-#define ALLOCSET_NUM_FREELISTS	10
+#define ALLOC_MINBITS		3	/* smallest chunk size is 8 bytes */
+#define ALLOCSET_NUM_FREELISTS	11
 #define ALLOC_CHUNK_LIMIT	(1 << (ALLOCSET_NUM_FREELISTS-1+ALLOC_MINBITS))
 /* Size of largest chunk that we use a fixed size for */
 
