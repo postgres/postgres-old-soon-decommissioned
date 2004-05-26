3c66850beb08eafb9bb8ced9926eb68e3f02f981
Bruce Momjian <bruce@momjian.us>
2004-05-26 19:00:31 +0000

    Move setlocale() outside of NLS-only defines.

--- src/port/path.c
+++ src/port/path.c
@@ -234,11 +234,13 @@ set_pglocale(const char *argv0, const char *app)
 #ifdef ENABLE_NLS
 	char path[MAXPGPATH];
 	char my_exec_path[MAXPGPATH];
+#endif
 
 	/* don't set LC_ALL in the backend */
 	if (strcmp(app, "postgres") != 0)
 		setlocale(LC_ALL, "");
 
+#ifdef ENABLE_NLS
 	if (find_my_exec(argv0, my_exec_path) < 0)
 		return;
 		
