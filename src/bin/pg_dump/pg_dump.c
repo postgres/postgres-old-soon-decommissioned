e586942cccbad8c9006978f1277b4c504b7a3151
Bruce Momjian <bruce@momjian.us>
2004-05-26 18:24:22 +0000

    Fix problem with doing 7.0.X dumps on character varying[] fields.
    Christopher Kings-Lynne

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -7722,7 +7722,15 @@ static char *
 myFormatType(const char *typname, int32 typmod)
 {
 	char	   *result;
+	bool	isarray = false;
 	PQExpBuffer buf = createPQExpBuffer();
+
+	/* Handle array types */
+	if (typname[0] == '_')
+	{
+		isarray = true;
+		typname++;
+	}
 
 	/* Show lengths on bpchar and varchar */
 	if (!strcmp(typname, "bpchar"))
@@ -7766,6 +7774,10 @@ myFormatType(const char *typname, int32 typmod)
 		appendPQExpBuffer(buf, "\"char\"");
 	else
 		appendPQExpBuffer(buf, "%s", fmtId(typname));
+
+	/* Append array qualifier for array types */
+	if (isarray)
+		appendPQExpBuffer(buf, "[]");
 
 	result = strdup(buf->data);
 	destroyPQExpBuffer(buf);
