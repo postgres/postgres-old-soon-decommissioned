177fb880cf143da5e60afc07d0186fb9de661380
Peter Eisentraut <peter_e@gmx.net>
2004-08-10 19:06:21 +0000

    Translation updates

--- src/bin/initdb/nls.mk
+++ src/bin/initdb/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= de fr it pt_BR ru sk sv zh_TW
+AVAIL_LANGUAGES	:= de fr it pt_BR ru sk sv zh_CN zh_TW
 GETTEXT_FILES	:= initdb.c
 GETTEXT_TRIGGERS:= _ simple_prompt
--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= ru sk sv zh_TW
+AVAIL_LANGUAGES	:= ru sk sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c
 GETTEXT_TRIGGERS:= _ simple_prompt
