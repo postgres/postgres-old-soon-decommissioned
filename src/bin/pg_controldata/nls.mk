7f4dfdcb6408f4c3dab07917e83d049faad3f5c3
Peter Eisentraut <peter_e@gmx.net>
2004-09-13 12:14:50 +0000

    New translation

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sk sl sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fa fr hu it nb pt_BR ru sk sl sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
