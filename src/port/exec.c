43b182fe68906564fddaa3d41b1725450505fd3f
Bruce Momjian <bruce@momjian.us>
2004-09-27 23:58:09 +0000

    Small WIN32_CLIENT_ONLY cleanup for new build patch.

--- src/port/exec.c
+++ src/port/exec.c
@@ -22,7 +22,7 @@
 #include <pwd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#if !defined(_MSC_VER) && !defined(__BORLANDC__)
+#ifndef WIN32_CLIENT_ONLY
 #include <unistd.h>
 #endif
 
@@ -178,7 +178,7 @@ find_my_exec(const char *argv0, char *retpath)
 				test_path[MAXPGPATH];
 	char	   *path;
 
-#if !defined(_MSC_VER) && !defined(__BORLANDC__)
+#ifndef WIN32_CLIENT_ONLY
 	if (!getcwd(cwd, MAXPGPATH))
 #else
 	if (!GetCurrentDirectory(MAXPGPATH, cwd))
