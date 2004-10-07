c419ec8db8a76fa40947d576a2cca7423a61ae77
Bruce Momjian <bruce@momjian.us>
2004-10-07 17:03:50 +0000

    Update comment to fix nibble mention:
    	 * We are not sure how much precision is in tv_usec, so we
    	 * swap the high and low 16 bits of 'later' and XOR them with
    	 * 'earlier'. On the off chance that the result is 0, we
    	 * loop until it isn't.
    
    Greg Stark

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -1194,7 +1194,7 @@ ServerLoop(void)
 
 				/*
 				 * We are not sure how much precision is in tv_usec, so we
-				 * swap the nibbles of 'later' and XOR them with
+				 * swap the high and low 16-bits of 'later' and XOR them with
 				 * 'earlier'. On the off chance that the result is 0, we
 				 * loop until it isn't.
 				 */
