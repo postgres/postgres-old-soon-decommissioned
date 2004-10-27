e5b27698392d856febc4330b1e3f52a08809557e
Peter Eisentraut <peter_e@gmx.net>
2004-10-27 11:09:33 +0000

    Translation updates

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= af cs de es fr hr it nb pt_BR ru sk sl sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= af cs de es fr hr it nb pt_BR ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-protocol2.c fe-protocol3.c fe-secure.c
 GETTEXT_TRIGGERS:= libpq_gettext pqInternalNotice:2
