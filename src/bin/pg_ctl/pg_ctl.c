db03110f778dc8c191480cba8783c345b4d93623
Bruce Momjian <bruce@momjian.us>
2004-10-27 17:17:09 +0000

    Canonicalize Win32 path coming in from pg_ctl -D, idea from Magnus.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -1279,19 +1279,23 @@ main(int argc, char **argv)
 			{
 				case 'D':
 					{
-						int			len = strlen(optarg);
-						char	   *env_var;
+						char	   *pgdata_D = xmalloc(strlen(optarg));
+						char	   *env_var = xmalloc(strlen(optarg) + 8);
 
-						env_var = xmalloc(len + 8);
-						snprintf(env_var, len + 8, "PGDATA=%s", optarg);
+						strcpy(pgdata_D, optarg);
+						canonicalize_path(pgdata_D);
+						snprintf(env_var, strlen(pgdata_D) + 8, "PGDATA=%s",
+								 pgdata_D);
 						putenv(env_var);
 
 						/*
-						 * Show -D for easier postmaster 'ps'
-						 * identification
+						 *	We could pass PGDATA just in an environment
+						 *	variable but we do -D too for clearer
+						 *	postmaster 'ps' display
 						 */
-						pgdata_opt = xmalloc(len + 7);
-						snprintf(pgdata_opt, len + 7, "-D \"%s\" ", optarg);
+						pgdata_opt = xmalloc(strlen(pgdata_D) + 7);
+						snprintf(pgdata_opt, strlen(pgdata_D) + 7, "-D \"%s\" ",
+								 pgdata_D);
 						break;
 					}
 				case 'l':
