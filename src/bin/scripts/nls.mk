d01a1598ea229d3fd53bc88c1179f90e7857ce1c
Peter Eisentraut <peter_e@gmx.net>
2004-10-30 08:22:17 +0000

    New translations

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
 # $Header$
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de es fr it pt_BR ru sl sv zh_CN
+AVAIL_LANGUAGES := cs de es fr it pt_BR ru sl sv tr zh_CN
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
