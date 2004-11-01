e7d6b17594ec74fe60a1d6570d19413447af4758
Bruce Momjian <bruce@momjian.us>
2004-11-01 04:46:56 +0000

    Adjust use of sizeof(EXE).

--- src/port/path.c
+++ src/port/path.c
@@ -195,8 +195,8 @@ get_progname(const char *argv0)
 
 #if defined(__CYGWIN__) || defined(WIN32)
 	/* strip .exe suffix, regardless of case */
-	if (strlen(nodir_name) > sizeof(EXE) &&
-		pg_strcasecmp(nodir_name + strlen(nodir_name) - sizeof(EXE), EXE) == 0)
+	if (strlen(nodir_name) > sizeof(EXE) - 1 &&
+		pg_strcasecmp(nodir_name + strlen(nodir_name)-sizeof(EXE)-1, EXE) == 0)
 	{
 		char *progname;
 
@@ -206,7 +206,7 @@ get_progname(const char *argv0)
 			fprintf(stderr, "%s: out of memory\n", nodir_name);
 			exit(1);
 		}
-		progname[strlen(progname) - sizeof(EXE)] = '\0';
+		progname[strlen(progname) - sizeof(EXE) - 1] = '\0';
 		nodir_name = progname; 
 	}
 #endif
