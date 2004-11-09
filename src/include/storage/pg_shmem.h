c0648a592b611c14050ab5a152f01fc63bca20fa
Tom Lane <tgl@sss.pgh.pa.us>
2004-11-09 21:30:18 +0000

    Put in place some defenses against being fooled by accidental match of shared memory segment ID.  If we can't access the existing shmem segment, it must not be relevant to our data directory.  If we can access it, then attach to it and check for an actual match to the data directory. This should avoid some cases of failure-to-restart-after-boot without introducing any significant risk of failing to detect a still-running old backend.

--- src/include/storage/pg_shmem.h
+++ src/include/storage/pg_shmem.h
@@ -27,10 +27,14 @@
 typedef struct PGShmemHeader	/* standard header for all Postgres shmem */
 {
 	int32		magic;			/* magic # to identify Postgres segments */
-#define PGShmemMagic  679834892
+#define PGShmemMagic  679834893
 	pid_t		creatorPID;		/* PID of creating process */
 	uint32		totalsize;		/* total size of segment */
 	uint32		freeoffset;		/* offset to first free space */
+#ifndef WIN32					/* Windows doesn't have useful inode#s */
+	dev_t		device;			/* device data directory is on */
+	ino_t		inode;			/* inode number of data directory */
+#endif
 } PGShmemHeader;
 
 
