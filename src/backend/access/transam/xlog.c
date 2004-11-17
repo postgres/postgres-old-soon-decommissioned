f33ababff9a59479cba6e7ffb98d889511972c41
Tom Lane <tgl@sss.pgh.pa.us>
2004-11-17 16:26:59 +0000

    Minor adjustment of message style.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -5270,9 +5270,9 @@ pg_start_backup(PG_FUNCTION_ARGS)
 	if (!XLogArchivingActive())
 		ereport(ERROR,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 (errmsg("WAL archiving is not configured"),
+				 (errmsg("WAL archiving is not active"),
 				 (errhint("archive_command must be defined before "
-						  "online backups can be safely made.")))));
+						  "online backups can be made safely.")))));
 
 	backupidstr = DatumGetCString(DirectFunctionCall1(textout,
 											 PointerGetDatum(backupid)));
