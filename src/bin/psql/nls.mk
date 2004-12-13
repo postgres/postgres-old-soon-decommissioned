e5a2095bec8d8148295385e6314f44c1de7bdf7e
Peter Eisentraut <peter_e@gmx.net>
2004-12-13 16:30:53 +0000

    New translations

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
 # $PostgreSQL$
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= cs de es fa fr hu it nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c describe.c sql_help.h \
                    ../../port/exec.c
