52a0ae13c00e6611c80414c3ded48e56753d80ce
Tom Lane <tgl@sss.pgh.pa.us>
2004-12-19 19:39:47 +0000

    Prevent evaluation of backticks while discarding unwanted arguments after an unknown or failed psql backslash command, and also while discarding "extra" arguments of a putatively valid backslash command. In the case of an unknown/failed command, make sure we discard the whole rest of the line, rather than trying to resume at the next backslash.  Per discussion with Thomer Gil.

--- src/bin/psql/psqlscan.h
+++ src/bin/psql/psqlscan.h
@@ -32,7 +32,8 @@ enum slash_option_type
 	OT_SQLID,					/* treat as SQL identifier */
 	OT_SQLIDHACK,				/* SQL identifier, but don't downcase */
 	OT_FILEPIPE,				/* it's a filename or pipe */
-	OT_WHOLE_LINE				/* just snarf the rest of the line */
+	OT_WHOLE_LINE,				/* just snarf the rest of the line */
+	OT_VERBATIM					/* literal (no backticks or variables) */
 };
 
 
