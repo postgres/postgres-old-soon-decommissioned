259633507814dbc5842a468e4166da6a12b68614
branch-fixup <branch-fixup>
2005-01-09 17:10:29 +0000

    Removing files not present on branch REL8_0_STABLE: 	configure 	configure.in 	contrib/tsearch2/ispell/spell.c 	contrib/tsearch2/ispell/spell.h 	contrib/xml2/xpath.c 	doc/FAQ 	doc/FAQ_brazilian 	doc/FAQ_german 	doc/FAQ_japanese 	doc/bug.template 	doc/src/FAQ/FAQ.html 	doc/src/FAQ/FAQ_brazilian.html 	doc/src/FAQ/FAQ_german.html 	doc/src/FAQ/FAQ_japanese.html 	doc/src/sgml/datatype.sgml 	doc/src/sgml/datetime.sgml 	doc/src/sgml/ddl.sgml 	doc/src/sgml/diskusage.sgml 	doc/src/sgml/filelayout.sgml 	doc/src/sgml/filelist.sgml 	doc/src/sgml/func.sgml 	doc/src/sgml/installation.sgml 	doc/src/sgml/libpq.sgml 	doc/src/sgml/lobj.sgml 	doc/src/sgml/page.sgml 	doc/src/sgml/plperl.sgml 	doc/src/sgml/plpgsql.sgml 	doc/src/sgml/postgres.sgml 	doc/src/sgml/ref/alter_table.sgml 	doc/src/sgml/ref/create_opclass.sgml 	doc/src/sgml/ref/pg_dump.sgml 	doc/src/sgml/ref/psql-ref.sgml 	doc/src/sgml/release.sgml 	doc/src/sgml/xfunc.sgml 	doc/src/sgml/xtypes.sgml 	src/Makefile 	src/backend/access/transam/xlogutils.c 	src/backend/bootstrap/bootstrap.c 	src/backend/catalog/heap.c 	src/backend/catalog/index.c 	src/backend/commands/tablecmds.c 	src/backend/executor/execMain.c 	src/backend/libpq/auth.c 	src/backend/libpq/pqcomm.c 	src/backend/nls.mk 	src/backend/parser/gram.c 	src/backend/parser/parse_expr.c 	src/backend/parser/parse_target.c 	src/backend/po/de.po 	src/backend/po/es.po 	src/backend/po/pt_BR.po 	src/backend/po/ru.po 	src/backend/po/tr.po 	src/backend/postmaster/bgwriter.c 	src/backend/postmaster/postmaster.c 	src/backend/rewrite/rewriteDefine.c 	src/backend/storage/buffer/bufmgr.c 	src/backend/storage/buffer/localbuf.c 	src/backend/storage/smgr/smgr.c 	src/backend/utils/adt/datetime.c 	src/backend/utils/adt/formatting.c 	src/backend/utils/adt/numutils.c 	src/backend/utils/adt/ruleutils.c 	src/backend/utils/cache/inval.c 	src/backend/utils/cache/relcache.c 	src/bin/initdb/nls.mk 	src/bin/initdb/po/de.po 	src/bin/initdb/po/es.po 	src/bin/initdb/po/pt_BR.po 	src/bin/initdb/po/ro.po 	src/bin/initdb/po/ru.po 	src/bin/initdb/po/sl.po 	src/bin/initdb/po/tr.po 	src/bin/pg_config/nls.mk 	src/bin/pg_config/po/de.po 	src/bin/pg_config/po/es.po 	src/bin/pg_config/po/pt_BR.po 	src/bin/pg_config/po/ro.po 	src/bin/pg_config/po/ru.po 	src/bin/pg_config/po/sl.po 	src/bin/pg_config/po/tr.po 	src/bin/pg_controldata/nls.mk 	src/bin/pg_controldata/po/pt_BR.po 	src/bin/pg_controldata/po/ru.po 	src/bin/pg_ctl/po/de.po 	src/bin/pg_ctl/po/es.po 	src/bin/pg_ctl/po/pt_BR.po 	src/bin/pg_ctl/po/ro.po 	src/bin/pg_ctl/po/ru.po 	src/bin/pg_ctl/po/sl.po 	src/bin/pg_ctl/po/tr.po 	src/bin/pg_dump/pg_backup_archiver.c 	src/bin/pg_dump/pg_dump.c 	src/bin/pg_dump/po/de.po 	src/bin/pg_dump/po/es.po 	src/bin/pg_dump/po/pt_BR.po 	src/bin/pg_dump/po/ro.po 	src/bin/pg_dump/po/ru.po 	src/bin/pg_dump/po/sl.po 	src/bin/pg_dump/po/tr.po 	src/bin/pg_resetxlog/po/es.po 	src/bin/pg_resetxlog/po/pt_BR.po 	src/bin/pg_resetxlog/po/ru.po 	src/bin/pg_resetxlog/po/sl.po 	src/bin/psql/po/de.po 	src/bin/psql/po/es.po 	src/bin/psql/po/pt_BR.po 	src/bin/psql/po/ro.po 	src/bin/psql/po/ru.po 	src/bin/psql/po/sl.po 	src/bin/psql/po/tr.po 	src/bin/psql/startup.c 	src/bin/scripts/nls.mk 	src/bin/scripts/po/pt_BR.po 	src/bin/scripts/po/ru.po 	src/include/pg_config.h.win32 	src/include/storage/sinval.h 	src/include/storage/smgr.h 	src/include/utils/rel.h 	src/include/utils/relcache.h 	src/interfaces/ecpg/ChangeLog 	src/interfaces/ecpg/pgtypeslib/interval.c 	src/interfaces/ecpg/preproc/pgc.c 	src/interfaces/ecpg/preproc/preproc.c 	src/interfaces/ecpg/preproc/preproc.y 	src/interfaces/libpq/fe-auth.c 	src/interfaces/libpq/fe-connect.c 	src/interfaces/libpq/fe-secure.c 	src/interfaces/libpq/nls.mk 	src/interfaces/libpq/po/pt_BR.po 	src/interfaces/libpq/po/ru.po 	src/interfaces/libpq/win32.mak 	src/pl/plperl/plperl.c 	src/pl/plpgsql/src/pl_exec.c 	src/port/exec.c 	src/port/path.c 	src/test/regress/expected/case.out 	src/test/regress/pg_regress.sh 	src/test/regress/sql/case.sql 	src/tutorial/Makefile

--- src/bin/initdb/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL$
-CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= cs de es fr it ko pt_BR ro ru sk sl sv tr zh_CN zh_TW
-GETTEXT_FILES	:= initdb.c ../../port/dirmod.c ../../port/exec.c
-GETTEXT_TRIGGERS:= _ simple_prompt
--- src/bin/scripts/nls.mk
+++
@@ -1,8 +0,0 @@
-# $PostgreSQL$
-CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de es fr it ko pt_BR ru sk sl sv tr zh_CN zh_TW
-GETTEXT_FILES   := createdb.c createlang.c createuser.c \
-                   dropdb.c droplang.c dropuser.c \
-                   clusterdb.c vacuumdb.c \
-                   common.c
-GETTEXT_TRIGGERS:= _ simple_prompt
