ecc41fe2f4c281451c8f321271038773e5155998
Bruce Momjian <bruce@momjian.us>
2005-03-02 23:56:53 +0000

    Move snprintf int64 compatibility letters into a NOT_USED block.

--- src/port/snprintf.c
+++ src/port/snprintf.c
@@ -259,6 +259,7 @@ dopr(char *buffer, const char *format, va_list args, char *end)
 						else
 							longflag = 1;
 						goto nextch;
+#ifdef NOT_USED
 					/*
 					 *	We might export this to client apps so we should
 					 *	support 'qd' and 'I64d'(MinGW) also in case the
@@ -277,6 +278,7 @@ dopr(char *buffer, const char *format, va_list args, char *end)
 							goto nextch;
 						}
 						break;
+#endif
 					case 'u':
 					case 'U':
 						/* fmtnum(value,base,dosign,ljust,len,zpad,&output) */
