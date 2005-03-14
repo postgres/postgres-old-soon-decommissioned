c7d52d6baac2a5733af6d4b8ba5c365b216c2957
Tom Lane <tgl@sss.pgh.pa.us>
2005-03-14 18:57:33 +0000

    Make pg_dump emit a useful error message, instead of just dumping core, if it finds a pg_rewrite entry for which there is no pg_class entry. Per report from Andrew Slobodyanyk.

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -3154,6 +3154,13 @@ getRules(int *numRules)
 		ruleinfo[i].dobj.name = strdup(PQgetvalue(res, i, i_rulename));
 		ruletableoid = atooid(PQgetvalue(res, i, i_ruletable));
 		ruleinfo[i].ruletable = findTableByOid(ruletableoid);
+		if (ruleinfo[i].ruletable == NULL)
+		{
+			write_msg(NULL, "failed sanity check, parent table OID %u of pg_rewrite entry OID %u not found\n",
+					  ruletableoid,
+					  ruleinfo[i].dobj.catId.oid);
+			exit_nicely();
+		}
 		ruleinfo[i].dobj.namespace = ruleinfo[i].ruletable->dobj.namespace;
 		ruleinfo[i].ev_type = *(PQgetvalue(res, i, i_ev_type));
 		ruleinfo[i].is_instead = *(PQgetvalue(res, i, i_is_instead)) == 't';
