07329f4f87ffe119c87560f0bfd112bc32ca5214
Tom Lane <tgl@sss.pgh.pa.us>
2005-04-23 17:45:35 +0000

    Recent changes got the sense of the notnull bit backwards in the 2.0 protocol output routines.  Mea culpa :-(.  Per report from Kris Jurka.

--- src/backend/access/common/printtup.c
+++ src/backend/access/common/printtup.c
@@ -402,7 +402,7 @@ printtup_20(TupleTableSlot *slot, DestReceiver *self)
 	k = 1 << 7;
 	for (i = 0; i < natts; ++i)
 	{
-		if (slot->tts_isnull[i])
+		if (!slot->tts_isnull[i])
 			j |= k;				/* set bit if not null */
 		k >>= 1;
 		if (k == 0)				/* end of byte? */
@@ -610,7 +610,7 @@ printtup_internal_20(TupleTableSlot *slot, DestReceiver *self)
 	k = 1 << 7;
 	for (i = 0; i < natts; ++i)
 	{
-		if (slot->tts_isnull[i])
+		if (!slot->tts_isnull[i])
 			j |= k;				/* set bit if not null */
 		k >>= 1;
 		if (k == 0)				/* end of byte? */
