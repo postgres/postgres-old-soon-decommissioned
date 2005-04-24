f5409f86e8e4b1109daa329afe8a374f96d67acd
Tom Lane <tgl@sss.pgh.pa.us>
2005-04-24 18:16:38 +0000

    Adjust nodeBitmapIndexscan.c to not keep the index open across calls, but just to open and close it during MultiExecBitmapIndexScan.  This avoids acquiring duplicate resources (eg, multiple locks on the same relation) in a tree with many bitmap scans.  Also, don't bother to lock the parent heap at all here, since we must be underneath a BitmapHeapScan node that will be holding a suitable lock.

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -908,8 +908,6 @@ typedef struct IndexScanState
  *						   that will be evaluated at runtime
  *		RuntimeContext	   expr context for evaling runtime Skeys
  *		RuntimeKeysReady   true if runtime Skeys have been computed
- *		RelationDesc	   relation descriptor
- *		ScanDesc		   scan descriptor
  * ----------------
  */
 typedef struct BitmapIndexScanState
@@ -921,8 +919,6 @@ typedef struct BitmapIndexScanState
 	ExprState **biss_RuntimeKeyInfo;
 	ExprContext *biss_RuntimeContext;
 	bool		biss_RuntimeKeysReady;
-	Relation	biss_RelationDesc;
-	IndexScanDesc biss_ScanDesc;
 } BitmapIndexScanState;
 
 /* ----------------
