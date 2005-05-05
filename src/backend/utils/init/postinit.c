c1bae9feb0ce291aa3f18faa30a1139260e35705
Tom Lane <tgl@sss.pgh.pa.us>
2005-05-05 19:54:13 +0000

    Make standalone backends ignore pg_database.datallowconn, so that there is a way to recover from disabling connections to all databases at once.

--- src/backend/utils/init/postinit.c
+++ src/backend/utils/init/postinit.c
@@ -165,9 +165,11 @@ ReverifyMyDatabase(const char *name)
 
 	/*
 	 * Also check that the database is currently allowing connections.
+	 * (We do not enforce this in standalone mode, however, so that there is
+	 * a way to recover from "UPDATE pg_database SET datallowconn = false;")
 	 */
 	dbform = (Form_pg_database) GETSTRUCT(tup);
-	if (!dbform->datallowconn)
+	if (IsUnderPostmaster && !dbform->datallowconn)
 		ereport(FATAL,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
 		 errmsg("database \"%s\" is not currently accepting connections",
