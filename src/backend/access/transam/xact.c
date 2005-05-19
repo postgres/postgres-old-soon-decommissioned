41ba6f2745f285bc5604c2238d157279e3746597
Tom Lane <tgl@sss.pgh.pa.us>
2005-05-19 23:58:51 +0000

    Make a comment pgindent-proof, per suggestion from Alvaro.

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -1498,7 +1498,7 @@ CommitTransaction(void)
 	 */
 	RecordTransactionCommit();
 
-	/*
+	/*----------
 	 * Let others know about no transaction in progress by me. Note that
 	 * this must be done _before_ releasing locks we hold and _after_
 	 * RecordTransactionCommit.
@@ -1510,6 +1510,7 @@ CommitTransaction(void)
 	 * xid 0 as running as well, or it will be able to see two tuple versions
 	 * - one deleted by xid 1 and one inserted by xid 0.  See notes in
 	 * GetSnapshotData.
+	 *----------
 	 */
 	if (MyProc != NULL)
 	{
