c2549cc968779c4d017583bd0b15672927a7fa6c
Bruce Momjian <bruce@momjian.us>
2005-05-23 21:54:02 +0000

    More macro cleanups for date/time.

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -1062,7 +1062,7 @@ tz_acceptable(pg_tz *tz)
 	 * be GMT midnight, 2000-01-01.  Insist that the tm_sec value be zero;
 	 * any other result has to be due to leap seconds.
 	 */
-	time2000 = (POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * 86400;
+	time2000 = (POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * SECS_PER_DAY;
 	tt = pg_localtime(&time2000, tz);
 	if (!tt || tt->tm_sec != 0)
 		return false;
