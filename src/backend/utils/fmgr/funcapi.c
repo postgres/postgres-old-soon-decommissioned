b0fa93f2c12858b122e8d6e7d93e0212813f4b1f
Tom Lane <tgl@sss.pgh.pa.us>
2005-05-28 05:10:47 +0000

    get_expr_result_type has to be prepared to pull type information from a RECORD Const node, because that's what it may be faced with after constant-folding of a function returning RECORD.  Per example from Michael Fuhr.

--- src/backend/utils/fmgr/funcapi.c
+++ src/backend/utils/fmgr/funcapi.c
@@ -235,6 +235,36 @@ get_expr_result_type(Node *expr,
 										  NULL,
 										  resultTypeId,
 										  resultTupleDesc);
+	else if (expr && IsA(expr, Const) &&
+			 ((Const *) expr)->consttype == RECORDOID &&
+			 !((Const *) expr)->constisnull)
+	{
+		/*
+		 * Pull embedded type info from a RECORD constant.  We have to be
+		 * prepared to handle this case in the wake of constant-folding of
+		 * record-returning functions.
+		 */
+		HeapTupleHeader td;
+		int32	typmod;
+
+		td = DatumGetHeapTupleHeader(((Const *) expr)->constvalue);
+		Assert(HeapTupleHeaderGetTypeId(td) == RECORDOID);
+		typmod = HeapTupleHeaderGetTypMod(td);
+		if (resultTypeId)
+			*resultTypeId = RECORDOID;
+		if (typmod >= 0)
+		{
+			if (resultTupleDesc)
+				*resultTupleDesc = lookup_rowtype_tupdesc(RECORDOID, typmod);
+			result = TYPEFUNC_COMPOSITE;
+		}
+		else
+		{
+			if (resultTupleDesc)
+				*resultTupleDesc = NULL;
+			result = TYPEFUNC_RECORD;
+		}
+	}
 	else
 	{
 		/* handle as a generic expression; no chance to resolve RECORD */
