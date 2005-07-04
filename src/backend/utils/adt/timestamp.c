bd395170c58e6b112bb2a2ba51ced51592666fd5
Bruce Momjian <bruce@momjian.us>
2005-07-04 14:12:45 +0000

    Fix date_trunct for December dates that are in the next year, e.g.:
    	SELECT date_trunc('week', '2002-12-31'::date);
    
    Backpatch to 8.0.X.
    
    Per report from Nick Johnson.

--- src/backend/utils/adt/timestamp.c
+++ src/backend/utils/adt/timestamp.c
@@ -77,12 +77,10 @@ timestamp_in(PG_FUNCTION_ARGS)
 	int			dterr;
 	char	   *field[MAXDATEFIELDS];
 	int			ftype[MAXDATEFIELDS];
-	char		lowstr[MAXDATELEN + MAXDATEFIELDS];
+	char		workbuf[MAXDATELEN + MAXDATEFIELDS];
 
-	if (strlen(str) >= sizeof(lowstr))
-		dterr = DTERR_BAD_FORMAT;
-	else
-		dterr = ParseDateTime(str, lowstr, field, ftype, MAXDATEFIELDS, &nf);
+	dterr = ParseDateTime(str, workbuf, sizeof(workbuf),
+						  field, ftype, MAXDATEFIELDS, &nf);
 	if (dterr == 0)
 		dterr = DecodeDateTime(field, ftype, nf, &dtype, tm, &fsec, &tz);
 	if (dterr != 0)
@@ -318,12 +316,10 @@ timestamptz_in(PG_FUNCTION_ARGS)
 	int			dterr;
 	char	   *field[MAXDATEFIELDS];
 	int			ftype[MAXDATEFIELDS];
-	char		lowstr[MAXDATELEN + MAXDATEFIELDS];
+	char		workbuf[MAXDATELEN + MAXDATEFIELDS];
 
-	if (strlen(str) >= sizeof(lowstr))
-		dterr = DTERR_BAD_FORMAT;
-	else
-		dterr = ParseDateTime(str, lowstr, field, ftype, MAXDATEFIELDS, &nf);
+	dterr = ParseDateTime(str, workbuf, sizeof(workbuf),
+						  field, ftype, MAXDATEFIELDS, &nf);
 	if (dterr == 0)
 		dterr = DecodeDateTime(field, ftype, nf, &dtype, tm, &fsec, &tz);
 	if (dterr != 0)
@@ -494,7 +490,7 @@ interval_in(PG_FUNCTION_ARGS)
 	int			dterr;
 	char	   *field[MAXDATEFIELDS];
 	int			ftype[MAXDATEFIELDS];
-	char		lowstr[MAXDATELEN + MAXDATEFIELDS];
+	char		workbuf[256];
 
 	tm->tm_year = 0;
 	tm->tm_mon = 0;
@@ -504,10 +500,8 @@ interval_in(PG_FUNCTION_ARGS)
 	tm->tm_sec = 0;
 	fsec = 0;
 
-	if (strlen(str) >= sizeof(lowstr))
-		dterr = DTERR_BAD_FORMAT;
-	else
-		dterr = ParseDateTime(str, lowstr, field, ftype, MAXDATEFIELDS, &nf);
+	dterr = ParseDateTime(str, workbuf, sizeof(workbuf), field,
+						  ftype, MAXDATEFIELDS, &nf);
 	if (dterr == 0)
 		dterr = DecodeInterval(field, ftype, nf, &dtype, tm, &fsec);
 	if (dterr != 0)
@@ -2754,12 +2748,26 @@ timestamp_trunc(PG_FUNCTION_ARGS)
 		switch (val)
 		{
 			case DTK_WEEK:
-				isoweek2date(date2isoweek(tm->tm_year, tm->tm_mon, tm->tm_mday), &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday));
+			{
+				int woy;
+				
+				woy = date2isoweek(tm->tm_year, tm->tm_mon, tm->tm_mday);
+				/*
+				 *	If it is week 52/53 and the month is January,
+				 *	then the week must belong to the previous year.
+				 *	Also, some December dates belong to the next year.
+				 */
+				if (woy >= 52 && tm->tm_mon == 1)
+					--tm->tm_year;
+				if (woy <= 1 && tm->tm_mon == 12)
+					++tm->tm_year;
+				isoweek2date(woy, &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday));
 				tm->tm_hour = 0;
 				tm->tm_min = 0;
 				tm->tm_sec = 0;
 				fsec = 0;
 				break;
+			}
 			case DTK_MILLENNIUM:
 				/* see comments in timestamptz_trunc */
 				if (tm->tm_year > 0)
@@ -2874,13 +2882,27 @@ timestamptz_trunc(PG_FUNCTION_ARGS)
 		switch (val)
 		{
 			case DTK_WEEK:
-				isoweek2date(date2isoweek(tm->tm_year, tm->tm_mon, tm->tm_mday), &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday));
+			{
+				int woy;
+				
+				woy = date2isoweek(tm->tm_year, tm->tm_mon, tm->tm_mday);
+				/*
+				 *	If it is week 52/53 and the month is January,
+				 *	then the week must belong to the previous year.
+				 *	Also, some December dates belong to the next year.
+				 */
+				if (woy >= 52 && tm->tm_mon == 1)
+					--tm->tm_year;
+				if (woy <= 1 && tm->tm_mon == 12)
+					++tm->tm_year;
+				isoweek2date(woy, &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday));
 				tm->tm_hour = 0;
 				tm->tm_min = 0;
 				tm->tm_sec = 0;
 				fsec = 0;
 				redotz = true;
 				break;
+			}
 				/* one may consider DTK_THOUSAND and DTK_HUNDRED... */
 			case DTK_MILLENNIUM:
 
@@ -3142,7 +3164,7 @@ date2isoweek(int year, int mon, int mday)
 	 * Sometimes the last few days in a year will fall into the first week
 	 * of the next year, so check for this.
 	 */
-	if (result >= 53)
+	if (result >= 52)
 	{
 		day4 = date2j(year + 1, 1, 4);
 
@@ -3198,7 +3220,7 @@ date2isoyear(int year, int mon, int mday)
 	 * Sometimes the last few days in a year will fall into the first week
 	 * of the next year, so check for this.
 	 */
-	if (result >= 53)
+	if (result >= 52)
 	{
 		day4 = date2j(year + 1, 1, 4);
 
