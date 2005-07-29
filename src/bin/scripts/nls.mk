4c8456afc94df8c768e68e80f924d4e2a2711aee
Bruce Momjian <bruce@momjian.us>
2005-07-29 15:13:11 +0000

    Move reindexdb from /contrib to /bin.
    Euler Taveira de Oliveira

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -3,6 +3,6 @@ CATALOG_NAME    := pgscripts
 AVAIL_LANGUAGES := cs de es fr it ko pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
-                   clusterdb.c vacuumdb.c \
+                   clusterdb.c vacuumdb.c reindexdb.c \
                    common.c
 GETTEXT_TRIGGERS:= _ simple_prompt
