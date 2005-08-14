04e7c81c6613e04f34749c3e9b0a39e09e8433ea
Peter Eisentraut <peter_e@gmx.net>
2005-08-14 22:19:50 +0000

    Change standard_compliant_strings to standard_conforming_strings.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -202,7 +202,7 @@ static int	max_index_keys;
 static int	max_identifier_length;
 static int	block_size;
 static bool	integer_datetimes;
-static bool	standard_compliant_strings;
+static bool	standard_conforming_strings;
 
 /* should be static, but commands/variable.c needs to get at these */
 char	   *role_string;
@@ -930,12 +930,12 @@ static struct config_bool ConfigureNamesBool[] =
 	},
 
 	{
-		{"standard_compliant_strings", PGC_INTERNAL, PRESET_OPTIONS,
+		{"standard_conforming_strings", PGC_INTERNAL, PRESET_OPTIONS,
 			gettext_noop("'...' strings treat backslashes literally."),
 			NULL,
 			GUC_REPORT | GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE
 		},
-		&standard_compliant_strings,
+		&standard_conforming_strings,
 		false, NULL, NULL
 	},
 
