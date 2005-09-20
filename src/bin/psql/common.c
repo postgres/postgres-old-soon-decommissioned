485dcf6cb0b159dd3167ea144905bd24ecf00e25
Bruce Momjian <bruce@momjian.us>
2005-09-20 21:43:08 +0000

    Fix interaction between psql \set AUTOCOMMIT and "ON_ERROR_ROLLBACK" by properly updating transaction state after AUTOCOMMIT.

--- src/bin/psql/common.c
+++ src/bin/psql/common.c
@@ -1010,13 +1010,15 @@ SendQuery(const char *query)
 			return false;
 		}
 		PQclear(results);
+		transaction_status = PQtransactionStatus(pset.db);
 	}
-	else if (transaction_status == PQTRANS_INTRANS &&
-			 (rollback_str = GetVariable(pset.vars, "ON_ERROR_ROLLBACK")) != NULL &&
-			 /* !off and !interactive is 'on' */
-			 pg_strcasecmp(rollback_str, "off") != 0 &&
-			 (pset.cur_cmd_interactive ||
-			  pg_strcasecmp(rollback_str, "interactive") != 0))
+
+	if (transaction_status == PQTRANS_INTRANS &&
+		(rollback_str = GetVariable(pset.vars, "ON_ERROR_ROLLBACK")) != NULL &&
+		/* !off and !interactive is 'on' */
+		pg_strcasecmp(rollback_str, "off") != 0 &&
+		(pset.cur_cmd_interactive ||
+		 pg_strcasecmp(rollback_str, "interactive") != 0))
 	{
 		if (on_error_rollback_warning == false && pset.sversion < 80000)
 		{
