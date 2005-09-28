bf40278227f9218527843bf037b8477db9a8ad08
Tom Lane <tgl@sss.pgh.pa.us>
2005-09-28 21:17:50 +0000

    Repair planning bug introduced in 7.4: outer-join ON clauses that referenced only the inner-side relation would be considered as potential equijoin clauses, which is wrong because the condition doesn't necessarily hold above the point of the outer join.  Per test case from Kevin Grittner (bug#1916).

--- src/include/optimizer/planmain.h
+++ src/include/optimizer/planmain.h
@@ -57,7 +57,8 @@ extern Result *make_result(List *tlist, Node *resconstantqual, Plan *subplan);
  */
 extern void add_base_rels_to_query(Query *root, Node *jtnode);
 extern void build_base_rel_tlists(Query *root, List *final_tlist);
-extern Relids distribute_quals_to_rels(Query *root, Node *jtnode);
+extern Relids distribute_quals_to_rels(Query *root, Node *jtnode,
+									   bool below_outer_join);
 extern void process_implied_equality(Query *root,
 						 Node *item1, Node *item2,
 						 Oid sortop1, Oid sortop2,
