6577e2017c4d1f7a4eabc6d557f00ab07f07c5dd
Tom Lane <tgl@sss.pgh.pa.us>
2005-11-03 21:35:57 +0000

    Fix a couple of missed None -> DestNone in comments.

--- src/backend/tcop/pquery.c
+++ src/backend/tcop/pquery.c
@@ -376,7 +376,7 @@ PortalStart(Portal portal, ParamListInfo params, Snapshot snapshot)
 
 				/*
 				 * Create QueryDesc in portal's context; for the moment, set
-				 * the destination to None.
+				 * the destination to DestNone.
 				 */
 				queryDesc = CreateQueryDesc((Query *) linitial(portal->parseTrees),
 										(Plan *) linitial(portal->planTrees),
@@ -757,8 +757,8 @@ PortalRunSelect(Portal portal,
 
 	/*
 	 * Force the queryDesc destination to the right thing.	This supports
-	 * MOVE, for example, which will pass in dest = None.  This is okay to
-	 * change as long as we do it on every fetch.  (The Executor must not
+	 * MOVE, for example, which will pass in dest = DestNone.  This is okay
+	 * to change as long as we do it on every fetch.  (The Executor must not
 	 * assume that dest never changes.)
 	 */
 	if (queryDesc)
