18edfada57abb44670d65e38cf9bc8a4887f0c64
Tom Lane <tgl@sss.pgh.pa.us>
2005-12-14 16:30:20 +0000

    Fix a *different* problem with whole-row Vars afflicting 8.0 branch. This is a back-port of a fix made to markTargetListOrigin() on 2005-04-25.

--- src/backend/parser/parse_target.c
+++ src/backend/parser/parse_target.c
@@ -209,8 +209,9 @@ markTargetListOrigin(ParseState *pstate, Resdom *res, Var *var, int levelsup)
 			res->resorigcol = attnum;
 			break;
 		case RTE_SUBQUERY:
+			/* Subselect-in-FROM: copy up from the subselect */
+			if (attnum != InvalidAttrNumber)
 			{
-				/* Subselect-in-FROM: copy up from the subselect */
 				TargetEntry *te = get_tle_by_resno(rte->subquery->targetList,
 												   attnum);
 
@@ -222,8 +223,9 @@ markTargetListOrigin(ParseState *pstate, Resdom *res, Var *var, int levelsup)
 			}
 			break;
 		case RTE_JOIN:
+			/* Join RTE --- recursively inspect the alias variable */
+			if (attnum != InvalidAttrNumber)
 			{
-				/* Join RTE --- recursively inspect the alias variable */
 				Var		   *aliasvar;
 
 				Assert(attnum > 0 && attnum <= list_length(rte->joinaliasvars));
