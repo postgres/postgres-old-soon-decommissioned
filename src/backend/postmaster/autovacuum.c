3f6d96a24e2d22669d7e992772bced7e75219e17
Tom Lane <tgl@sss.pgh.pa.us>
2006-01-20 15:17:13 +0000

    Fix thinko in autovacuum's test to skip temp tables: want to skip any temp table not only our own process' tables.  It's not real important since vacuum.c will skip temp tables anyway, but might as well make the code do what it claims to do.

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -635,7 +635,7 @@ do_autovacuum(PgStat_StatDBEntry *dbentry)
 		 * Skip temp tables (i.e. those in temp namespaces).  We cannot safely
 		 * process other backends' temp tables.
 		 */
-		if (isTempNamespace(classForm->relnamespace))
+		if (isAnyTempNamespace(classForm->relnamespace))
 			continue;
 
 		relid = HeapTupleGetOid(tuple);
