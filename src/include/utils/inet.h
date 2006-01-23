07d6e8bcfceef88cad3aa31e05a9e2ac5fc50881
Bruce Momjian <bruce@momjian.us>
2006-01-23 21:45:47 +0000

    Use is_cidr in INET/CIDR structure, rather than the generic 'type'.

--- src/include/utils/inet.h
+++ src/include/utils/inet.h
@@ -22,7 +22,7 @@ typedef struct
 {
 	unsigned char family;		/* PGSQL_AF_INET or PGSQL_AF_INET6 */
 	unsigned char bits;			/* number of bits in netmask */
-	unsigned char type;			/* 0 = inet, 1 = cidr */
+	bool is_cidr;				/* is cidr? */
 	unsigned char ipaddr[16];	/* up to 128 bits of address */
 } inet_struct;
 
