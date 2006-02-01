09d568a9fefaa7b8141fca5f34b6cd34ee277750
Bruce Momjian <bruce@momjian.us>
2006-02-01 00:32:06 +0000

    Set progname early in the postmaster/postgres binary, rather than doing it later.  This fixes a problem where EXEC_BACKEND didn't have progname set, causing a segfault if log_min_messages was set below debug2 and our own snprintf.c was being used.
    Also alway strdup() progname.
    
    Backpatch to 8.1.X and 8.0.X.

--- src/backend/main/main.c
+++ src/backend/main/main.c
@@ -45,7 +45,7 @@
 #include "libpq/pqsignal.h"
 #endif
 
-
+const char *progname;
 
 int
 main(int argc, char *argv[])
@@ -76,6 +76,8 @@ main(int argc, char *argv[])
 #ifdef WIN32
 	char	   *env_locale;
 #endif
+
+	progname = get_progname(argv[0]);
 
 	/*
 	 * On some platforms, unaligned memory accesses result in a kernel trap;
@@ -257,7 +259,7 @@ main(int argc, char *argv[])
 	 * possibly first argument) we were called with. The lack of consistency
 	 * here is historical.
 	 */
-	if (strcmp(get_progname(argv[0]), "postmaster") == 0)
+	if (strcmp(progname, "postmaster") == 0)
 	{
 		/* Called as "postmaster" */
 		exit(PostmasterMain(argc, argv));
