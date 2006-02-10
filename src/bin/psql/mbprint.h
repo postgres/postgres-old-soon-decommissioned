b51e1b55fd4d50f907f22a0fdcabdc9a347fefff
Bruce Momjian <bruce@momjian.us>
2006-02-10 00:39:04 +0000

    Allow psql multi-line column values to align in the proper columns
      If the second output column value is 'a\nb', the 'b' should appear
      in the second display column, rather than the first column as it
      does now.
    
    Change libpq's PQdsplen() to return more useful values.
    
    > Note: this changes the PQdsplen function, it can now return zero or
    > minus one which was not possible before. It doesn't appear anyone is
    > actually using the functions other than psql but it is a change. The
    > functions are not actually documentated anywhere so it's not like we're
    > breaking a defined interface. The new semantics follow the Unicode
    > standard.
    
    BACKWARD COMPATIBLE CHANGE.
    
    The only user-visible change I saw in the regression tests is that a
    SELECT * on a table where all the columns have been dropped doesn't
    return a blank line like before.  This seems like a step forward.
    
    Martijn van Oosterhout

--- src/bin/psql/mbprint.h
+++ src/bin/psql/mbprint.h
@@ -4,8 +4,15 @@
 
 #include "mb/pg_wchar.h"
 
-extern char *mbvalidate(char *pwcs, int encoding);
+struct lineptr {
+	unsigned char *ptr;
+	int width;
+};
 
-extern int	pg_wcswidth(const char *pwcs, size_t len, int encoding);
+extern unsigned char *mbvalidate(unsigned char *pwcs, int encoding);
+
+extern int	pg_wcswidth(const unsigned char *pwcs, size_t len, int encoding);
+extern void	pg_wcsformat(unsigned char *pwcs, size_t len, int encoding, struct lineptr *lines, int count);
+extern int	pg_wcssize(unsigned char *pwcs, size_t len, int encoding, int *width, int *height, int *format_size);
 
 #endif   /* MBPRINT_H */
