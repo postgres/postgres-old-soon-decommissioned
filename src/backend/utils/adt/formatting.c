6438d3589e507a2b9905c71c14832a215ed793d5
Bruce Momjian <bruce@momjian.us>
2006-02-12 23:48:23 +0000

    Revert because C locale uses "" for thousands_sep, meaning "n/a", while French uses "" for "don't want".  Seems we have to keep the existing behavior.

--- src/backend/utils/adt/formatting.c
+++ src/backend/utils/adt/formatting.c
@@ -3720,16 +3720,15 @@ NUM_prepare_locale(NUMProc *Np)
 		else
 			Np->L_negative_sign = "-";
 
-		/* Might be "" */
-		if (lconv->positive_sign)
+		if (lconv->positive_sign && *lconv->positive_sign)
 			Np->L_positive_sign = lconv->positive_sign;
 		else
 			Np->L_positive_sign = "+";
 
 		/*
-		 * Number thousands separator (might be "")
+		 * Number thousands separator
 		 */
-		if (lconv->thousands_sep)
+		if (lconv->thousands_sep && *lconv->thousands_sep)
 			Np->L_thousands_sep = lconv->thousands_sep;
 		else
 			Np->L_thousands_sep = ",";
