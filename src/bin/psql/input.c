1f5c35099af5856429b63918d56aa866bda14588
Tom Lane <tgl@sss.pgh.pa.us>
2006-02-13 14:57:15 +0000

    Fix build failure when not USE_READLINE.  Sergey E. Koposov

--- src/bin/psql/input.c
+++ src/bin/psql/input.c
@@ -213,7 +213,8 @@ gets_fromFile(FILE *source)
 }
 
 
-static void encode_history()
+#ifdef USE_READLINE
+static void encode_history(void)
 {
 	HIST_ENTRY *cur_hist;
 	char *cur_ptr;
@@ -225,7 +226,7 @@ static void encode_history()
 				*cur_ptr = NL_IN_HISTORY;
 }
 
-static void decode_history()
+static void decode_history(void)
 {
 	HIST_ENTRY *cur_hist;
 	char *cur_ptr;
@@ -236,6 +237,7 @@ static void decode_history()
 			if (*cur_ptr == NL_IN_HISTORY)
 				*cur_ptr = '\n';
 }
+#endif /* USE_READLINE */
 
 
 /*
@@ -290,7 +292,6 @@ initializeInput(int flags)
 }
 
 
-
 bool
 saveHistory(char *fname)
 {
@@ -309,7 +310,6 @@ saveHistory(char *fname)
 
 	return false;
 }
-
 
 
 static void
