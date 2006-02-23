596eda3b3c585dd13482491ebb230a5c1111dd50
Tom Lane <tgl@sss.pgh.pa.us>
2006-02-23 22:34:07 +0000

    Fix vacuumlo to avoid unnecessary use of backslash in search pattern. Per Michael Fuhr.

--- contrib/vacuumlo/vacuumlo.c
+++ contrib/vacuumlo/vacuumlo.c
@@ -167,7 +167,7 @@ vacuumlo(char *database, struct _param * param)
 	strcat(buf, "      AND c.relnamespace = s.oid ");
 	strcat(buf, "      AND t.typname in ('oid', 'lo') ");
 	strcat(buf, "      AND c.relkind = 'r'");
-	strcat(buf, "      AND s.nspname NOT LIKE 'pg\\\\_%'");
+	strcat(buf, "      AND s.nspname !~ '^pg_'");
 	res = PQexec(conn, buf);
 	if (PQresultStatus(res) != PGRES_TUPLES_OK)
 	{
