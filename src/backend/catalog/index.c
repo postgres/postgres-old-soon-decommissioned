7fe47b399728bec8996a6ea00341abb2e399780b
Tom Lane <tgl@sss.pgh.pa.us>
2006-03-24 23:02:23 +0000

    Comments in IndexBuildHeapScan describe the indexing of recently-dead tuples as needed "to keep VACUUM from complaining", but actually there is a more compelling reason to do it: failure to do so violates MVCC semantics. This is because a pre-existing serializable transaction might try to use the index after we finish (re)building it, and it might fail to find tuples it should be able to see.  We got this mostly right, but not in the case of partial indexes: the code mistakenly discarded recently-dead tuples for partial indexes.  Fix that, and adjust the comments.

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -1473,7 +1473,9 @@ IndexBuildHeapScan(Relation heapRelation,
 
 					/*
 					 * If tuple is recently deleted then we must index it
-					 * anyway to keep VACUUM from complaining.
+					 * anyway to preserve MVCC semantics.  (Pre-existing
+					 * transactions could try to use the index after we
+					 * finish building it, and may need to see such tuples.)
 					 */
 					indexIt = true;
 					tupleIsAlive = false;
@@ -1541,13 +1543,10 @@ IndexBuildHeapScan(Relation heapRelation,
 
 		/*
 		 * In a partial index, discard tuples that don't satisfy the
-		 * predicate.  We can also discard recently-dead tuples, since VACUUM
-		 * doesn't complain about tuple count mismatch for partial indexes.
+		 * predicate.
 		 */
 		if (predicate != NIL)
 		{
-			if (!tupleIsAlive)
-				continue;
 			if (!ExecQual(predicate, econtext, false))
 				continue;
 		}
