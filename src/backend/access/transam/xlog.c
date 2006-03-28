a895a4b5bbc0a9ae9cb0feb557533199a6d3861b
Tom Lane <tgl@sss.pgh.pa.us>
2006-03-28 22:01:25 +0000

    Disable full_page_writes, because turning it off risks causing crash-recovery failures even when the hardware and OS did nothing wrong.  Per recent analysis of a problem report from Alex Bahdushka.
    For the moment I've just diked out the test of the parameter, rather than
    removing the GUC infrastructure and documentation, in case we conclude that
    there's something salvageable there.  There seems no chance of it being
    resurrected in the 8.1 branch though.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -980,8 +980,7 @@ XLogCheckBuffer(XLogRecData *rdata,
 	 */
 	*lsn = page->pd_lsn;
 
-	if (fullPageWrites &&
-		XLByteLE(page->pd_lsn, RedoRecPtr))
+	if (XLByteLE(page->pd_lsn, RedoRecPtr))
 	{
 		/*
 		 * The page needs to be backed up, so set up *bkpb
