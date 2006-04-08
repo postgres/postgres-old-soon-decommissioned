285bb2b7dae36f4ade4872af1d729cb2a4669829
Tom Lane <tgl@sss.pgh.pa.us>
2006-04-08 18:49:52 +0000

    Fix EXPLAIN so that it can drill down through multiple levels of subplan when trying to locate the referent of a RECORD variable.  This fixes the 'record type has not been registered' failure reported by Stefan Kaltenbrunner about a month ago.  A side effect of the way I chose to fix it is that most variable references in join conditions will now be properly labeled with the variable's source table name, instead of the not-too-helpful 'outer' or 'inner' we used to use.

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -526,9 +526,7 @@ extern List *deparse_context_for(const char *aliasname, Oid relid);
 extern List *deparse_context_for_plan(int outer_varno, Node *outercontext,
 						 int inner_varno, Node *innercontext,
 						 List *rtable);
-extern Node *deparse_context_for_rte(RangeTblEntry *rte);
-extern Node *deparse_context_for_subplan(const char *name, List *tlist,
-							List *rtable);
+extern Node *deparse_context_for_subplan(const char *name, Node *subplan);
 extern const char *quote_identifier(const char *ident);
 extern char *quote_qualified_identifier(const char *namespace,
 						   const char *ident);
