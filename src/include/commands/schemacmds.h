39c7d62ecbae37c1304e0ae3e2cd59116034f6a0
Tom Lane <tgl@sss.pgh.pa.us>
2006-04-09 22:01:19 +0000

    Fix another const-decoration mismatch, per Magnus.

--- src/include/commands/schemacmds.h
+++ src/include/commands/schemacmds.h
@@ -24,6 +24,6 @@ extern void RemoveSchemaById(Oid schemaOid);
 
 extern void RenameSchema(const char *oldname, const char *newname);
 extern void AlterSchemaOwner(const char *name, Oid newOwnerId);
-extern void AlterSchemaOwner_oid(const Oid schemaOid, Oid newOwnerId);
+extern void AlterSchemaOwner_oid(Oid schemaOid, Oid newOwnerId);
 
 #endif   /* SCHEMACMDS_H */
