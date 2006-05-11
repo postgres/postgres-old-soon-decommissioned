b30b3a0c5192158168b6c6a84f48426b8a08c84a
Bruce Momjian <bruce@momjian.us>
2006-05-11 23:27:35 +0000

    Fix typo in comment.

--- src/interfaces/libpq/fe-secure.c
+++ src/interfaces/libpq/fe-secure.c
@@ -681,7 +681,7 @@ static unsigned long
 pq_threadidcallback(void)
 {
 	/*
-	 * This is not starndard-compliant.  pthread_self() returns pthread_t, and
+	 * This is not standards-compliant.  pthread_self() returns pthread_t, and
 	 * shouldn't be cast to unsigned long, but CRYPTO_set_id_callback requires
 	 * it, so we have to do it.
 	 */
