00744393c0f8b744fe5b27b35277306a3a1f1bfd
Alvaro Herrera <alvherre@alvh.no-ip.org>
2006-05-19 15:15:38 +0000

    Have autovacuum report its activities to the stat collector.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2135,6 +2135,9 @@ reaper(SIGNAL_ARGS)
 		{
 			AutoVacPID = 0;
 			autovac_stopped();
+			/* Tell the collector about process termination */
+			pgstat_beterm(pid);
+
 			if (exitstatus != 0)
 				HandleChildCrash(pid, exitstatus,
 								 _("autovacuum process"));
