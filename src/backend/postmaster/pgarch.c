6d0a422afe994dfce563195f2b7456a7e7314098
Tom Lane <tgl@sss.pgh.pa.us>
2006-05-30 17:08:20 +0000

    PostmasterIsAlive test really ought to be in the inner loop for safety.

--- src/backend/postmaster/pgarch.c
+++ src/backend/postmaster/pgarch.c
@@ -352,12 +352,16 @@ pgarch_ArchiverCopyLoop(void)
 	 * some backend will add files onto the list of those that need archiving
 	 * while we are still copying earlier archives
 	 */
-	while (PostmasterIsAlive(true) && pgarch_readyXlog(xlog))
+	while (pgarch_readyXlog(xlog))
 	{
 		int			failures = 0;
 
 		for (;;)
 		{
+			/* Abandon processing if we notice our postmaster has died */
+			if (!PostmasterIsAlive(true))
+				return;
+
 			if (pgarch_archiveXlog(xlog))
 			{
 				/* successful */
