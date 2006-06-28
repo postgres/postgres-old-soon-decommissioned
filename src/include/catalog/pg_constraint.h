3e0022b518c68033297141b682af2dc539d1b1ef
Neil Conway <neilc@samurai.com>
2006-06-28 22:11:01 +0000

    Add missing #include directive: pg_constraint.h declares some functions whose prototypes include the "List" type, so it ought to include the list header file.

--- src/include/catalog/pg_constraint.h
+++ src/include/catalog/pg_constraint.h
@@ -19,6 +19,8 @@
 #ifndef PG_CONSTRAINT_H
 #define PG_CONSTRAINT_H
 
+#include "nodes/pg_list.h"
+
 /* ----------------
  *		postgres.h contains the system type definitions and the
  *		CATALOG(), BKI_BOOTSTRAP and DATA() sugar words so this file
