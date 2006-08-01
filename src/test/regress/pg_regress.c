aa7e84c830fe8b621d9a6a8625d4a01471cca196
Andrew Dunstan <andrew@dunslane.net>
2006-08-01 14:56:29 +0000

    have pg_regress fall back on testing with the canonical results file if an alternative test is specified but none succeeds.

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -913,6 +913,36 @@ results_differ(const char *testname)
 		}
 	}
 
+	/* 
+	 * fall back on the canonical results file if we haven't tried it yet
+	 * and haven't found a complete match yet.
+	 */
+
+	if (strcmp(expectname, testname) != 0)
+	{
+		snprintf(expectfile, sizeof(expectfile), "%s/expected/%s.out",
+				 inputdir, testname, i);
+
+		snprintf(cmd, sizeof(cmd),
+				 SYSTEMQUOTE "diff %s \"%s\" \"%s\" > \"%s\"" SYSTEMQUOTE,
+				 basic_diff_opts, expectfile, resultsfile, diff);
+
+		if (run_diff(cmd, diff) == 0)
+		{
+			/* No diff = no changes = good */
+			unlink(diff);
+			return false;
+		}
+
+		l = file_line_count(diff);
+		if (l < best_line_count)
+		{
+			/* This diff was a better match than the last one */
+			best_line_count = l;
+			strcpy(best_expect_file, expectfile);
+		}
+	}
+
 	/*
 	 * Use the best comparison file to generate the "pretty" diff, which
 	 * we append to the diffs summary file.
