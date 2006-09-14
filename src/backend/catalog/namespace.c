5c800695bbaafca8890acf1b024e9eff1cf50ea5
Tom Lane <tgl@sss.pgh.pa.us>
2006-09-14 22:05:06 +0000

    Add a couple of information functions to support direct checks on whether a schema is our own temp schema or another backend's temp schema, and use these in place of some former kluges in information_schema.  Per my proposal of yesterday.

--- src/backend/catalog/namespace.c
+++ src/backend/catalog/namespace.c
@@ -143,6 +143,8 @@ Datum		pg_function_is_visible(PG_FUNCTION_ARGS);
 Datum		pg_operator_is_visible(PG_FUNCTION_ARGS);
 Datum		pg_opclass_is_visible(PG_FUNCTION_ARGS);
 Datum		pg_conversion_is_visible(PG_FUNCTION_ARGS);
+Datum		pg_my_temp_schema(PG_FUNCTION_ARGS);
+Datum		pg_is_other_temp_schema(PG_FUNCTION_ARGS);
 
 
 /*
@@ -2034,4 +2036,18 @@ pg_conversion_is_visible(PG_FUNCTION_ARGS)
 	Oid			oid = PG_GETARG_OID(0);
 
 	PG_RETURN_BOOL(ConversionIsVisible(oid));
+}
+
+Datum
+pg_my_temp_schema(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_OID(myTempNamespace);
+}
+
+Datum
+pg_is_other_temp_schema(PG_FUNCTION_ARGS)
+{
+	Oid			oid = PG_GETARG_OID(0);
+
+	PG_RETURN_BOOL(isOtherTempNamespace(oid));
 }
