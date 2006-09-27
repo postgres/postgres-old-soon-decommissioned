49677f4100787d83db7b83c491d66cf3100eb70d
Tom Lane <tgl@sss.pgh.pa.us>
2006-09-27 16:19:42 +0000

    Fix some misuses of strncat().

--- src/tutorial/funcs.c
+++ src/tutorial/funcs.c
@@ -85,13 +85,14 @@ copytext(text *t)
 text *
 concat_text(text *arg1, text *arg2)
 {
-	int32		new_text_size = VARSIZE(arg1) + VARSIZE(arg2) - VARHDRSZ;
+	int32		arg1_size = VARSIZE(arg1) - VARHDRSZ;
+	int32		arg2_size = VARSIZE(arg2) - VARHDRSZ;
+	int32		new_text_size = arg1_size + arg2_size + VARHDRSZ;
 	text	   *new_text = (text *) palloc(new_text_size);
 
-	memset((void *) new_text, 0, new_text_size);
 	VARATT_SIZEP(new_text) = new_text_size;
-	strncpy(VARDATA(new_text), VARDATA(arg1), VARSIZE(arg1) - VARHDRSZ);
-	strncat(VARDATA(new_text), VARDATA(arg2), VARSIZE(arg2) - VARHDRSZ);
+	memcpy(VARDATA(new_text), VARDATA(arg1), arg1_size);
+	memcpy(VARDATA(new_text) + arg1_size, VARDATA(arg2), arg2_size);
 	return new_text;
 }
 
