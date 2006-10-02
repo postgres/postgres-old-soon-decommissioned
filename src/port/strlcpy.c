45a740f09e4230bc1efd57a4a9e84fc87a31e300
Bruce Momjian <bruce@momjian.us>
2006-10-02 23:58:59 +0000

    Add URL about strlcpy() creation history.

--- src/port/strlcpy.c
+++ src/port/strlcpy.c
@@ -39,6 +39,7 @@
  * Copy src to string dst of size siz.  At most siz-1 characters
  * will be copied.  Always NUL terminates (unless siz == 0).
  * Returns strlen(src); if retval >= siz, truncation occurred.
+ * Function creation history:  http://www.gratisoft.us/todd/papers/strlcpy.html
  */
 size_t
 strlcpy(char *dst, const char *src, size_t siz)
