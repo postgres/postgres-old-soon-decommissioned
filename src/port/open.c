3e16e06ab8a862c6f00dd641c86cce56a4864e20
Bruce Momjian <bruce@momjian.us>
2006-10-03 20:44:18 +0000

    Cleanup pgwin32_open() 'if' test, and avoid possible error.

--- src/port/open.c
+++ src/port/open.c
@@ -105,9 +105,15 @@ pgwin32_open(const char *fileName, int fileFlags,...)
 	}
 
 	/* _open_osfhandle will, on error, set errno accordingly */
-	if ((fd = _open_osfhandle((long) h, fileFlags & O_APPEND)) < 0 ||
-		(fileFlags & (O_TEXT | O_BINARY) && (_setmode(fd, fileFlags & (O_TEXT | O_BINARY)) < 0)))
+	if ((fd = _open_osfhandle((long) h, fileFlags & O_APPEND)) < 0)
 		CloseHandle(h);			/* will not affect errno */
+	else if (fileFlags & (O_TEXT | O_BINARY) &&
+		_setmode(fd, fileFlags & (O_TEXT | O_BINARY)) < 0)
+	{
+		_close(fd);
+		return -1;
+	}
+
 	return fd;
 }
 
