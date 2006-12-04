8a20e430518ee95a15614679335693994bc98b61
Teodor Sigaev <teodor@sigaev.ru>
2006-12-04 09:27:45 +0000

    Fix core dump of ispell for case of non-successfull initialization. Previous versions aren't affected.
    Fix synonym dictionary init: string should be malloc'ed, not palloc'ed. Bug
    introduced recently while fixing lowerstr().

--- contrib/tsearch2/dict_syn.c
+++ contrib/tsearch2/dict_syn.c
@@ -132,8 +132,8 @@ syn_init(PG_FUNCTION_ARGS)
 			continue;
 		*end = '\0';
 
-		d->syn[cur].in = lowerstr(starti);
-		d->syn[cur].out = lowerstr(starto);
+		d->syn[cur].in = strdup(lowerstr(starti));
+		d->syn[cur].out = strdup(lowerstr(starto));
 		if (!(d->syn[cur].in && d->syn[cur].out))
 		{
 			fclose(fin);
