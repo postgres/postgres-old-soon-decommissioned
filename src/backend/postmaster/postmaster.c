8dca9714ed167dfa53b3457555548487938db448
Bruce Momjian <bruce@momjian.us>
2007-01-23 03:28:49 +0000

    Back out use of FormatMessage(), does error values, not exception values.  Point to /include/ntstatus.h for an exception list, rather than a URL.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2430,30 +2430,14 @@ LogChildExit(int lev, const char *procname, int pid, int exitstatus)
 				(errmsg("%s (PID %d) was terminated by signal %d",
 						procname, pid, WTERMSIG(exitstatus))));
 #else
-	{
-		static char last_system_error[512];
-
-		if (WERRORCODE(exitstatus) == 0 ||
-			FormatMessage(FORMAT_MESSAGE_IGNORE_INSERTS |
-						  FORMAT_MESSAGE_FROM_SYSTEM,
-						  NULL,
-						  WERRORCODE(exitstatus),
-						  MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT),
-						  last_system_error,
-						  sizeof(last_system_error) - 1,
-						  NULL) == 0)
-			snprintf(last_system_error, sizeof(last_system_error) - 1,
-					 "Unknown error %X.", WEXITSTATUS(exitstatus));
-
 		ereport(lev,
-	
+
 		/*------
 		  translator: %s is a noun phrase describing a child process, such as
 		  "server process" */
-				(errmsg("%s (PID %d) was terminated by the operating system",
-						procname, pid),
-				 errdetail("%s", last_system_error)));
-	}
+				(errmsg("%s (PID %d) was terminated by exception %X",
+						procname, pid, WTERMSIG(exitstatus)),
+				 errhint("See /include/ntstatus.h for a description of the hex value.")));
 #endif
 	else
 		ereport(lev,
