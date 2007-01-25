8d793683d8f716190c9d90771ce06e5cf98588ad
Bruce Momjian <bruce@momjian.us>
2007-01-25 04:17:56 +0000

    Properly detoast access to bytea field pg_trigger.tgargs.  Old code might cause server crash.
    Backpatch to 8.2.X.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -1790,8 +1790,7 @@ update_ri_trigger_args(Oid relid,
 		 * line; so does trigger.c ...
 		 */
 		tgnargs = pg_trigger->tgnargs;
-		val = (bytea *)
-			DatumGetPointer(fastgetattr(tuple,
+		val = DatumGetByteaP(fastgetattr(tuple,
 										Anum_pg_trigger_tgargs,
 										tgrel->rd_att, &isnull));
 		if (isnull || tgnargs < RI_FIRST_ATTNAME_ARGNO ||
