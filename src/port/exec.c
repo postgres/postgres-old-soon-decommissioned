dccec51f53012b6ad901c0d0164ceec1a2f9f93f
Bruce Momjian <bruce@momjian.us>
2007-01-29 20:22:35 +0000

    Use sizeof() for snprintf() buffer length.

--- src/port/exec.c
+++ src/port/exec.c
@@ -590,7 +590,7 @@ pclose_check(FILE *stream)
 	{
 		char str[256];
 
-		snprintf(str, 256, "%d: %s", WTERMSIG(exitstatus),
+		snprintf(str, sizeof(str), "%d: %s", WTERMSIG(exitstatus),
 			  WTERMSIG(exitstatus) < NSIG ?
 			  sys_siglist[WTERMSIG(exitstatus)] : "(unknown)");
 		log_error(_("child process was terminated by signal %s"), str);
