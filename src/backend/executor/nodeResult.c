dcf5aac9568d4f5458e757db258a2ae17f7b2e41
Tom Lane <tgl@sss.pgh.pa.us>
2007-02-16 03:49:10 +0000

    Fix another problem in 8.2 changes that allowed "one-time" qual conditions to be checked at plan levels below the top; namely, we have to allow for Result nodes inserted just above a nestloop inner indexscan.  Should think about using the general Param mechanism to pass down outer-relation variables, but for the moment we need a back-patchable solution.  Per report from Phil Frost.

--- src/backend/executor/nodeResult.c
+++ src/backend/executor/nodeResult.c
@@ -309,10 +309,12 @@ ExecReScanResult(ResultState *node, ExprContext *exprCtxt)
 	node->rs_checkqual = (node->resconstantqual == NULL) ? false : true;
 
 	/*
-	 * if chgParam of subnode is not null then plan will be re-scanned by
-	 * first ExecProcNode.
+	 * If chgParam of subnode is not null then plan will be re-scanned by
+	 * first ExecProcNode.  However, if caller is passing us an exprCtxt
+	 * then forcibly rescan the subnode now, so that we can pass the
+	 * exprCtxt down to the subnode (needed for gated indexscan).
 	 */
-	if (((PlanState *) node)->lefttree &&
-		((PlanState *) node)->lefttree->chgParam == NULL)
-		ExecReScan(((PlanState *) node)->lefttree, exprCtxt);
+	if (node->ps.lefttree &&
+		(node->ps.lefttree->chgParam == NULL || exprCtxt != NULL))
+		ExecReScan(node->ps.lefttree, exprCtxt);
 }
