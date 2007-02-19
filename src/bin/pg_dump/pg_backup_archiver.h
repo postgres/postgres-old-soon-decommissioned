4f6677be5524a0d17d2378a50abf0f4c199c48e5
Magnus Hagander <magnus@hagander.net>
2007-02-19 15:05:21 +0000

    Fix pg_dump on win32 to properly dump files larger than 2Gb when using binary dump formats.

--- src/bin/pg_dump/pg_backup_archiver.h
+++ src/bin/pg_dump/pg_backup_archiver.h
@@ -199,7 +199,7 @@ typedef struct _archiveHandle
 								 * format */
 	size_t		lookaheadSize;	/* Size of allocated buffer */
 	size_t		lookaheadLen;	/* Length of data in lookahead */
-	off_t		lookaheadPos;	/* Current read position in lookahead buffer */
+	pgoff_t		lookaheadPos;	/* Current read position in lookahead buffer */
 
 	ArchiveEntryPtr ArchiveEntryPtr;	/* Called for each metadata object */
 	StartDataPtr StartDataPtr;	/* Called when table data is about to be
@@ -332,8 +332,8 @@ extern int	ReadInt(ArchiveHandle *AH);
 extern char *ReadStr(ArchiveHandle *AH);
 extern size_t WriteStr(ArchiveHandle *AH, const char *s);
 
-int			ReadOffset(ArchiveHandle *, off_t *);
-size_t		WriteOffset(ArchiveHandle *, off_t, int);
+int			ReadOffset(ArchiveHandle *, pgoff_t *);
+size_t		WriteOffset(ArchiveHandle *, pgoff_t, int);
 
 extern void StartRestoreBlobs(ArchiveHandle *AH);
 extern void StartRestoreBlob(ArchiveHandle *AH, Oid oid);
