ffdd09580bae8ae3f10982a94aaf369401fd9e7b
Tom Lane <tgl@sss.pgh.pa.us>
2007-02-23 21:59:45 +0000

    Now that plans have flat rangetable lists, it's a lot easier to get EXPLAIN to drill down into subplan targetlists to print the referent expression for an OUTER or INNER var in an upper plan node.  Hence, make it do that always, and banish the old hack of showing "?columnN?" when things got too complicated.
    Along the way, fix an EXPLAIN bug I introduced by suppressing subqueries from
    execution-time range tables: get_name_for_var_field() assumed it could look at
    rte->subquery to find out the real type of a RECORD var.  That doesn't work
    anymore, but instead we can look at the input plan of the SubqueryScan plan
    node.

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -537,10 +537,8 @@ extern Datum pg_get_serial_sequence(PG_FUNCTION_ARGS);
 extern char *deparse_expression(Node *expr, List *dpcontext,
 				   bool forceprefix, bool showimplicit);
 extern List *deparse_context_for(const char *aliasname, Oid relid);
-extern List *deparse_context_for_plan(int outer_varno, Node *outercontext,
-						 int inner_varno, Node *innercontext,
-						 List *rtable);
-extern Node *deparse_context_for_subplan(const char *name, Node *subplan);
+extern List *deparse_context_for_plan(Node *outer_plan, Node *inner_plan,
+									  List *rtable);
 extern const char *quote_identifier(const char *ident);
 extern char *quote_qualified_identifier(const char *namespace,
 						   const char *ident);
