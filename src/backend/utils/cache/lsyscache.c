95091c3b6840b4410eb7d20c114293e59d203940
Tom Lane <tgl@sss.pgh.pa.us>
2007-03-19 16:30:40 +0000

    Fix 8.2 breakage of domains over array types, and add a regression test case to cover it.  Per report from Anton Pikhteryev.

--- src/backend/utils/cache/lsyscache.c
+++ src/backend/utils/cache/lsyscache.c
@@ -1366,10 +1366,10 @@ getTypeIOParam(HeapTuple typeTuple)
 
 	/*
 	 * Array types get their typelem as parameter; everybody else gets their
-	 * own type OID as parameter.  (This is a change from 8.0, in which only
-	 * composite types got their own OID as parameter.)
+	 * own type OID as parameter.  (As of 8.2, domains must get their own OID
+	 * even if their base type is an array.)
 	 */
-	if (OidIsValid(typeStruct->typelem))
+	if (typeStruct->typtype == 'b' && OidIsValid(typeStruct->typelem))
 		return typeStruct->typelem;
 	else
 		return HeapTupleGetOid(typeTuple);
