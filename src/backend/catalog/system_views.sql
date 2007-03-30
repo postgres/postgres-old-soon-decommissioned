4cf656db843a9cf2a2bb93da057459636765719e
Magnus Hagander <magnus@hagander.net>
2007-03-30 18:34:56 +0000

    Add some instrumentation to the bgwriter, through the stats collector. New view pg_stat_bgwriter, and the functions required to build it.

--- src/backend/catalog/system_views.sql
+++ src/backend/catalog/system_views.sql
@@ -364,3 +364,13 @@ CREATE VIEW pg_stat_database AS
             pg_stat_get_db_tuples_updated(D.oid) AS tup_updated,
             pg_stat_get_db_tuples_deleted(D.oid) AS tup_deleted
     FROM pg_database D;
+
+CREATE VIEW pg_stat_bgwriter AS
+	SELECT
+			pg_stat_get_bgwriter_timed_checkpoints() AS checkpoints_timed,
+			pg_stat_get_bgwriter_requested_checkpoints() AS checkpoints_req,
+			pg_stat_get_bgwriter_buf_written_checkpoints() AS buffers_checkpoint,
+			pg_stat_get_bgwriter_buf_written_lru() AS buffers_lru,
+			pg_stat_get_bgwriter_buf_written_all() AS buffers_all,
+			pg_stat_get_bgwriter_maxwritten_lru() AS maxwritten_lru,
+			pg_stat_get_bgwriter_maxwritten_all() AS maxwritten_all;
--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200703271
+#define CATALOG_VERSION_NO	200703301
 
 #endif
