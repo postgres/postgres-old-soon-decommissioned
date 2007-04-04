165d714bfa3457f7357bc77a551e8f6007412ce5
Magnus Hagander <magnus@hagander.net>
2007-04-04 16:34:43 +0000

    Don't install files for sslinfo when building without openssl. Per Andrew Dunstan.

--- src/tools/msvc/Install.pm
+++ src/tools/msvc/Install.pm
@@ -63,7 +63,7 @@ sub Install
     );
     GenerateConversionScript($target);
     GenerateTimezoneFiles($target,$conf);
-    CopyContribFiles($target);
+    CopyContribFiles($config,$target);
     CopyIncludeFiles($target);
 
     GenerateNLSFiles($target,$config->{nls}) if ($config->{nls});
@@ -215,6 +215,7 @@ sub GenerateTimezoneFiles
 
 sub CopyContribFiles
 {
+    my $config = shift;
     my $target = shift;
 
     print "Copying contrib data files...";
@@ -224,6 +225,7 @@ sub CopyContribFiles
     {
         next if ($d =~ /^\./);
         next unless (-f "contrib/$d/Makefile");
+        next if ($d eq "sslinfo" && !defined($config->{openssl}));
 
         my $mf = read_file("contrib/$d/Makefile");
         $mf =~ s{\\s*[\r\n]+}{}mg;
