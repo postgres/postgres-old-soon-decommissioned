acce7d055b50ed49a7410d3e3d31aad1c1844e9c
Tom Lane <tgl@sss.pgh.pa.us>
2007-04-16 18:50:49 +0000

    Tweak clean_encoding_name() API to avoid need to cast away const. Kris Jurka

--- src/backend/utils/mb/encnames.c
+++ src/backend/utils/mb/encnames.c
@@ -460,10 +460,10 @@ pg_valid_server_encoding(const char *name)
  * ----------
  */
 static char *
-clean_encoding_name(char *key, char *newkey)
+clean_encoding_name(const char *key, char *newkey)
 {
-	char	   *p,
-			   *np;
+	const char *p;
+	char 	   *np;
 
 	for (p = key, np = newkey; *p != '\0'; p++)
 	{
@@ -508,7 +508,7 @@ pg_char_to_encname_struct(const char *name)
 				 errmsg("encoding name too long")));
 #endif
 	}
-	key = clean_encoding_name((char *) name, buff);
+	key = clean_encoding_name(name, buff);
 
 	while (last >= base)
 	{
