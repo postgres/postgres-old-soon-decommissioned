601599b22be8b4465e5086833b52778766a6a1b2
Alvaro Herrera <alvherre@alvh.no-ip.org>
2007-05-07 20:41:24 +0000

    Make sure we don't skip databases that are supposed to be vacuumed "exactly now".  This can happen if the time granularity is not very high.
    Per ITAGAKI Takahiro.

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -1036,8 +1036,8 @@ do_start_worker(void)
 				 * Skip this database if its next_worker value falls between
 				 * the current time and the current time plus naptime.
 				 */
-				if (TimestampDifferenceExceeds(current_time,
-											   dbp->adl_next_worker, 0) &&
+				if (!TimestampDifferenceExceeds(dbp->adl_next_worker,
+											   current_time, 0) &&
 					!TimestampDifferenceExceeds(current_time,
 												dbp->adl_next_worker,
 												autovacuum_naptime * 1000))
