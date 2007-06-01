7323a0ea84f90d39ca68b824d4caea0e09a401ed
Tom Lane <tgl@sss.pgh.pa.us>
2007-06-01 15:58:02 +0000

    Fix performance problems in multi-batch hash joins by ensuring that we select a well-randomized batch number even when given a poorly-randomized hash value. This is a bit inefficient but seems the only practical solution given the constraint that we can't change the hash functions in released branches. Per report from Joseph Shraibman.
    Applied to 8.1 and 8.2 only --- HEAD is getting a cleaner fix, and 8.0 and
    before use different coding that seems less vulnerable.

--- src/backend/access/hash/hashfunc.c
+++ src/backend/access/hash/hashfunc.c
@@ -267,6 +267,31 @@ hash_any(register const unsigned char *k, register int keylen)
 			/* case 0: nothing left to add */
 	}
 	mix(a, b, c);
+
+	/* report the result */
+	return UInt32GetDatum(c);
+}
+
+/*
+ * hash_uint32() -- hash a 32-bit value
+ *
+ * This has the same result (at least on little-endian machines) as
+ *		hash_any(&k, sizeof(uint32))
+ * but is faster and doesn't force the caller to store k into memory.
+ */
+Datum
+hash_uint32(uint32 k)
+{
+	register uint32 a,
+				b,
+				c;
+
+	a = 0x9e3779b9 + k;
+	b = 0x9e3779b9;
+	c = 3923095 + (uint32) sizeof(uint32);
+
+	mix(a, b, c);
+
 	/* report the result */
 	return UInt32GetDatum(c);
 }
