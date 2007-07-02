9940464fcc4999bfc739f112cbd1d6f3d813df42
Tom Lane <tgl@sss.pgh.pa.us>
2007-07-02 20:12:26 +0000

    Fix failure to restart Postgres when Linux kernel returns EIDRM for shmctl().
    This is a Linux kernel bug that apparently exists in every extant kernel
    version: sometimes shmctl() will fail with EIDRM when EINVAL is correct.
    We were assuming that EIDRM indicates a possible conflict with pre-existing
    backends, and refusing to start the postmaster when this happens.  Fortunately,
    there does not seem to be any case where Linux can legitimately return EIDRM
    (it doesn't track shmem segments in a way that would allow that), so we can
    get away with just assuming that EIDRM means EINVAL on this platform.
    
    Per reports from Michael Fuhr and Jon Lapham --- it's a bit surprising
    we have not seen more reports, actually.

--- src/backend/port/sysv_shmem.c
+++ src/backend/port/sysv_shmem.c
@@ -221,6 +221,18 @@ PGSharedMemoryIsInUse(unsigned long id1, unsigned long id2)
 			return false;
 
 		/*
+		 * Some Linux kernel versions (in fact, all of them as of July 2007)
+		 * sometimes return EIDRM when EINVAL is correct.  The Linux kernel
+		 * actually does not have any internal state that would justify
+		 * returning EIDRM, so we can get away with assuming that EIDRM is
+		 * equivalent to EINVAL on that platform.
+		 */
+#ifdef HAVE_LINUX_EIDRM_BUG
+		if (errno == EIDRM)
+			return false;
+#endif
+
+		/*
 		 * Otherwise, we had better assume that the segment is in use. The
 		 * only likely case is EIDRM, which implies that the segment has been
 		 * IPC_RMID'd but there are still processes attached to it.
