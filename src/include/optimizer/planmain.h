a2f6ecb9e958d61c990c2a3a58561e94d87b44a1
Tom Lane <tgl@sss.pgh.pa.us>
2007-10-04 20:45:17 +0000

    Keep the planner from failing on "WHERE false AND something IN (SELECT ...)". eval_const_expressions simplifies this to just "WHERE false", but we have already done pull_up_IN_clauses so the IN join will be done, or at least planned, anyway.  The trouble case comes when the sub-SELECT is itself a join and we decide to implement the IN by unique-ifying the sub-SELECT outputs: with no remaining reference to the output Vars in WHERE, we won't have propagated the Vars up to the upper join point, leading to "variable not found in subplan target lists" error.  Fix by adding an extra scan of in_info_list and forcing all Vars mentioned therein to be propagated up to the IN join point.  Per bug report from Miroslav Sulc.

--- src/include/optimizer/planmain.h
+++ src/include/optimizer/planmain.h
@@ -57,6 +57,7 @@ extern Result *make_result(List *tlist, Node *resconstantqual, Plan *subplan);
  */
 extern void add_base_rels_to_query(Query *root, Node *jtnode);
 extern void build_base_rel_tlists(Query *root, List *final_tlist);
+extern void add_IN_vars_to_tlists(Query *root);
 extern Relids distribute_quals_to_rels(Query *root, Node *jtnode,
 									   bool below_outer_join);
 extern void process_implied_equality(Query *root,
