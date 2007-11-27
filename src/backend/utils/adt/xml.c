9635bc6d9cff9f9b393c57a05c8fa246b7ed092e
Peter Eisentraut <peter_e@gmx.net>
2007-11-27 12:21:05 +0000

    Make casts from xml to text independent of the XML option setting, thus immutable and indexable.  Also fix the volatility settings of some other XML-related functions.

--- src/backend/utils/adt/xml.c
+++ src/backend/utils/adt/xml.c
@@ -535,7 +535,8 @@ xmltotext(PG_FUNCTION_ARGS)
 {
 	xmltype    *data = PG_GETARG_XML_P(0);
 
-	PG_RETURN_TEXT_P(xmltotext_with_xmloption(data, xmloption));
+	/* It's actually binary compatible. */
+	return (text *) data;
 }
 
 
--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200710232
+#define CATALOG_VERSION_NO	200711271
 
 #endif
