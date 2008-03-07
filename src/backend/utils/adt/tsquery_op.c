36c3b62a8d34ce701274fefb31e0ba787cff5bf4
Teodor Sigaev <teodor@sigaev.ru>
2008-03-07 15:29:27 +0000

    Fix memory arrangement  of tsquery after removing stop words. It causes a unused memory holes in tsquery.
    Per report by Richard Huxton <dev@archonet.com>.
    
    It was working well because in fact tsquery->size is not used for any
    kind of operation except comparing tsqueries. To prevent requirement
    of renew all stored tsquery optimization in CompareTSQ is removed.

--- src/backend/utils/adt/tsquery_op.c
+++ src/backend/utils/adt/tsquery_op.c
@@ -141,27 +141,14 @@ tsquery_not(PG_FUNCTION_ARGS)
 static int
 CompareTSQ(TSQuery a, TSQuery b)
 {
-	if (a->size != b->size)
-	{
-		return (a->size < b->size) ? -1 : 1;
-	}
-	else if (VARSIZE(a) != VARSIZE(b))
-	{
-		return (VARSIZE(a) < VARSIZE(b)) ? -1 : 1;
-	}
-	else
-	{
-		QTNode	   *an = QT2QTN(GETQUERY(a), GETOPERAND(a));
-		QTNode	   *bn = QT2QTN(GETQUERY(b), GETOPERAND(b));
-		int			res = QTNodeCompare(an, bn);
+	QTNode	   *an = QT2QTN(GETQUERY(a), GETOPERAND(a));
+	QTNode	   *bn = QT2QTN(GETQUERY(b), GETOPERAND(b));
+	int			res = QTNodeCompare(an, bn);
 
-		QTNFree(an);
-		QTNFree(bn);
+	QTNFree(an);
+	QTNFree(bn);
 
-		return res;
-	}
-
-	return 0;
+	return res;
 }
 
 Datum
