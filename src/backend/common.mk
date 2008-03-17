9d19aeac71b9cffd65f80835a418c8ba3c63ce49
Peter Eisentraut <peter_e@gmx.net>
2008-03-17 18:24:56 +0000

    We need to rebuild objfiles.txt when one of the subdirectories' objfiles.txt changed in case a new file got added.

--- src/backend/common.mk
+++ src/backend/common.mk
@@ -27,8 +27,8 @@ SUBSYS.o: $(SUBDIROBJS) $(OBJS)
 	$(LD) $(LDREL) $(LDOUT) $@ $^
 
 objfiles.txt: Makefile $(SUBDIROBJS) $(OBJS)
-# Only rebuild the list if it does not exist or the Makefile has changed.
-	$(if $(filter $<,$?),( $(if $(SUBDIROBJS),cat $(SUBDIROBJS); )echo $(addprefix $(subdir)/,$(OBJS)) ) >$@,touch $@)
+# Don't rebuild the list if only the OBJS have changed.
+	$(if $(filter-out $(OBJS),$?),( $(if $(SUBDIROBJS),cat $(SUBDIROBJS); )echo $(addprefix $(subdir)/,$(OBJS)) ) >$@,touch $@)
 
 # make function to expand objfiles.txt contents
 expand_subsys = $(foreach file,$(1),$(if $(filter %/objfiles.txt,$(file)),$(patsubst ../../src/backend/%,%,$(addprefix $(top_builddir)/,$(shell cat $(file)))),$(file)))
