637856b7dfa9caedf1f19e849484499f01cc64b5
Tom Lane <tgl@sss.pgh.pa.us>
2008-03-24 19:12:58 +0000

    Fix various infelicities that have snuck into usage of errdetail() and friends.  Avoid double translation of some messages, ensure other messages are exposed for translation (and make them follow the style guidelines), avoid unsafe passing of an unpredictable message text as a format string.

--- src/backend/catalog/pg_shdepend.c
+++ src/backend/catalog/pg_shdepend.c
@@ -670,7 +670,7 @@ checkSharedDependencies(Oid classId, Oid objectId)
 		ereport(LOG,
 				(errmsg("there are objects dependent on %s",
 						getObjectDescription(&obj)),
-				 errdetail(alldescs.data)));
+				 errdetail("%s", alldescs.data)));
 	}
 
 	pfree(alldescs.data);
