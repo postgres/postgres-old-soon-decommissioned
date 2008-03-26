3428cddc4838589b39ee6c4f8eb9b04133901d52
Magnus Hagander <magnus@hagander.net>
2008-03-26 15:24:56 +0000

    Include \password in the psql help.
    While at it, change the order of the documented options to be
    alphabetically again.

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -177,14 +177,16 @@ slashUsage(unsigned short int pager)
 	fprintf(output, _("  \\encoding [ENCODING]\n"
 					  "                 show or set client encoding\n"));
 	fprintf(output, _("  \\h [NAME]      help on syntax of SQL commands, * for all commands\n"));
+	fprintf(output, _("  \\prompt [TEXT] NAME\n"
+				 "                 prompt user to set internal variable\n"));
+	fprintf(output, _("  \\password [USERNAME]\n"
+				 "                 securely change the password for a user\n"));
 	fprintf(output, _("  \\q             quit psql\n"));
 	fprintf(output, _("  \\set [NAME [VALUE]]\n"
 					  "                 set internal variable, or list all if no parameters\n"));
 	fprintf(output, _("  \\timing        toggle timing of commands (currently %s)\n"),
 			ON(pset.timing));
 	fprintf(output, _("  \\unset NAME    unset (delete) internal variable\n"));
-	fprintf(output, _("  \\prompt [TEXT] NAME\n"
-				 "                 prompt user to set internal variable\n"));
 	fprintf(output, _("  \\! [COMMAND]   execute command in shell or start interactive shell\n"));
 	fprintf(output, "\n");
 
