af1712a32547b98185813bf28dd6bbaa45b956a3
Tom Lane <tgl@sss.pgh.pa.us>
2008-05-09 05:25:04 +0000

    Fix memory stomp that's turning the whole buildfarm pink: you can't hack up pg_wcsformat without changing pg_wcssize to match.  Add some comments to try to make that clearer, and make a couple other minor editorializations.

--- src/bin/psql/mbprint.h
+++ src/bin/psql/mbprint.h
@@ -13,6 +13,7 @@ extern unsigned char *mbvalidate(unsigned char *pwcs, int encoding);
 
 extern int	pg_wcswidth(const unsigned char *pwcs, size_t len, int encoding);
 extern void pg_wcsformat(unsigned char *pwcs, size_t len, int encoding, struct lineptr * lines, int count);
-extern int	pg_wcssize(unsigned char *pwcs, size_t len, int encoding, int *width, int *height, int *format_size);
+extern void pg_wcssize(unsigned char *pwcs, size_t len, int encoding,
+					   int *width, int *height, int *format_size);
 
 #endif   /* MBPRINT_H */
