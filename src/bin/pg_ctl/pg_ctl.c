94433691e41c718ec21b4610bb1331327fd5fa67
Bruce Momjian <bruce@momjian.us>
2008-06-26 18:25:24 +0000

    Fix pg_ctl bug where detection of binary location from postmaster.opts wasn't working.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -610,7 +610,7 @@ read_post_opts(void)
 					*arg1 = '\0';	/* terminate so we get only program name */
 					post_opts = arg1 + 1; /* point past whitespace */
 				}
-				if (postgres_path != NULL)
+				if (postgres_path == NULL)
 					postgres_path = optline;
 			}
 		}
