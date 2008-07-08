e527655d3560a5c42abb8c0cc908ace0e29bdce0
Tom Lane <tgl@sss.pgh.pa.us>
2008-07-08 22:17:47 +0000

    Fix performance bug in write_syslog(): the code to preferentially break the log message at newlines cost O(N^2) for very long messages with few or no newlines.  For messages in the megabyte range this became the dominant cost. Per gripe from Achilleas Mantzios.
    Patch all the way back, since this is a safe change with no portability
    risks.  I am also thinking of increasing PG_SYSLOG_LIMIT, but that should
    be done separately.

--- src/backend/utils/error/elog.c
+++ src/backend/utils/error/elog.c
@@ -1277,6 +1277,7 @@ write_syslog(int level, const char *line)
 	static unsigned long seq = 0;
 
 	int			len;
+	const char *nlpos;
 
 	/* Open syslog connection if not done yet */
 	if (!openlog_done)
@@ -1299,17 +1300,17 @@ write_syslog(int level, const char *line)
 	 * fact, it does work around by splitting up messages into smaller pieces.
 	 *
 	 * We divide into multiple syslog() calls if message is too long or if the
-	 * message contains embedded NewLine(s) '\n'.
+	 * message contains embedded newline(s).
 	 */
 	len = strlen(line);
-	if (len > PG_SYSLOG_LIMIT || strchr(line, '\n') != NULL)
+	nlpos = strchr(line, '\n');
+	if (len > PG_SYSLOG_LIMIT || nlpos != NULL)
 	{
 		int			chunk_nr = 0;
 
 		while (len > 0)
 		{
 			char		buf[PG_SYSLOG_LIMIT + 1];
-			const char *nlpos;
 			int			buflen;
 			int			i;
 
@@ -1318,11 +1319,12 @@ write_syslog(int level, const char *line)
 			{
 				line++;
 				len--;
+				/* we need to recompute the next newline's position, too */
+				nlpos = strchr(line, '\n');
 				continue;
 			}
 
 			/* copy one line, or as much as will fit, to buf */
-			nlpos = strchr(line, '\n');
 			if (nlpos != NULL)
 				buflen = nlpos - line;
 			else
