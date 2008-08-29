6b7155203613ec2e8cdf551f7be2146747b876a6
Alvaro Herrera <alvherre@alvh.no-ip.org>
2008-08-29 15:52:07 +0000

    Document that \t and \x are now settable.

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -228,10 +228,10 @@ slashUsage(unsigned short int pager)
 	fprintf(output, _("  \\pset NAME [VALUE]  set table output option\n"
 					  "                 (NAME := {format|border|expanded|fieldsep|footer|null|\n"
 					  "                 numericlocale|recordsep|tuples_only|title|tableattr|pager})\n"));
-	fprintf(output, _("  \\t             show only rows (currently %s)\n"),
+	fprintf(output, _("  \\t [on|off]    show only rows (currently %s)\n"),
 			ON(pset.popt.topt.tuples_only));
 	fprintf(output, _("  \\T [STRING]    set HTML <table> tag attributes, or unset if none\n"));
-	fprintf(output, _("  \\x             toggle expanded output (currently %s)\n"),
+	fprintf(output, _("  \\x [on|off]    toggle expanded output (currently %s)\n"),
 			ON(pset.popt.topt.expanded));
 	fprintf(output, "\n");
 
