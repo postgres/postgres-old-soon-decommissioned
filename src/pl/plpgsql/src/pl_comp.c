818f6d7a30e5f2fda6c4130deed9ed09b5d793ab
Tom Lane <tgl@sss.pgh.pa.us>
2008-10-09 16:35:33 +0000

    Fix overly tense optimization of PLpgSQL_func_hashkey: we must represent the isTrigger state explicitly, not rely on nonzero-ness of trigrelOid to indicate trigger-hood, because trigrelOid will be left zero when compiling for validation.  The (useless) function hash entry built by the validator was able to match an ordinary non-trigger call later in the same session, thereby bypassing the check that is supposed to prevent such a call. Per report from Alvaro.
    It might be worth suppressing the useless hash entry altogether, but
    that's a bigger change than I want to consider back-patching.
    
    Back-patch to 8.0.  7.4 doesn't have the problem because it doesn't
    have validation mode.

--- src/pl/plpgsql/src/pl_comp.c
+++ src/pl/plpgsql/src/pl_comp.c
@@ -1880,12 +1880,15 @@ compute_function_hashkey(FunctionCallInfo fcinfo,
 	/* get function OID */
 	hashkey->funcOid = fcinfo->flinfo->fn_oid;
 
+	/* get call context */
+	hashkey->isTrigger = CALLED_AS_TRIGGER(fcinfo);
+
 	/*
 	 * if trigger, get relation OID.  In validation mode we do not know
 	 * what relation is intended to be used, so we leave trigrelOid zero;
 	 * the hash entry built in this case will never really be used.
 	 */
-	if (CALLED_AS_TRIGGER(fcinfo) && !forValidator)
+	if (hashkey->isTrigger && !forValidator)
 	{
 		TriggerData *trigdata = (TriggerData *) fcinfo->context;
 
