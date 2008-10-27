a9645ddec3e4c84c61e4bfd6fc5e0c7958a94e32
Tom Lane <tgl@sss.pgh.pa.us>
2008-10-27 19:37:42 +0000

    Install a more robust solution for the problem of infinite error-processing recursion when we are unable to convert a localized error message to the client's encoding.  We've been over this ground before, but as reported by Ibrar Ahmed, it still didn't work in the case of conversion failures for the conversion-failure message itself :-(.  Fix by installing a "circuit breaker" that disables attempts to localize this message once we get into recursion trouble.
    Patch all supported branches, because it is in fact broken in all of them;
    though I had to add some missing translations to the older branches in
    order to expose the failure in the particular test case I was using.

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -2,8 +2,6 @@
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= af cs de es fr hr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
-# you can add "elog:2" and "errmsg_internal" to this list if you want to
-# include internal messages in the translation list.
 GETTEXT_TRIGGERS:= _ errmsg errdetail errhint errcontext write_stderr yyerror
 
 gettext-files: distprep
