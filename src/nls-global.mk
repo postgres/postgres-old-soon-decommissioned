290fd9389d980ec1d70732f12257a60c23701d5a
Peter Eisentraut <peter_e@gmx.net>
2009-01-20 09:58:50 +0000

    Do not msgmerge against /dev/null; merge against the pot file itself instead.

--- src/nls-global.mk
+++ src/nls-global.mk
@@ -113,10 +113,12 @@ update-po: $(ALL_LANGUAGES:%=po/%.po.new)
 $(AVAIL_LANGUAGES:%=po/%.po.new): po/%.po.new: po/%.po po/$(CATALOG_NAME).pot $(all_compendia)
 	$(MSGMERGE) $(word 1, $^) $(word 2,$^) -o $@ $(addprefix --compendium=,$(filter %/$*.po,$(wordlist 3,$(words $^),$^)))
 
-# For languages not yet available, merge against empty file, to pick
-# up translations from the compendia.
+# For languages not yet available, merge against oneself, to pick
+# up translations from the compendia.  (Merging against /dev/null
+# doesn't work so well; it inserts the headers from the first-named
+# compendium.)
 po/%.po.new: po/$(CATALOG_NAME).pot $(all_compendia)
-	$(MSGMERGE) /dev/null $(word 1,$^) -o $@ $(addprefix --compendium=,$(filter %/$*.po,$(wordlist 2,$(words $^),$^)))
+	$(MSGMERGE) $(word 1,$^) $(word 1,$^) -o $@ $(addprefix --compendium=,$(filter %/$*.po,$(wordlist 2,$(words $^),$^)))
 
 
 all: all-po
