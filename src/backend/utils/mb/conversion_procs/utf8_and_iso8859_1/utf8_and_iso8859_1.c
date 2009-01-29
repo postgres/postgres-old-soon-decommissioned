9fada30b2e9c91951257d09cd91b817fba6c926e
Tom Lane <tgl@sss.pgh.pa.us>
2009-01-29 19:23:42 +0000

    Replace argument-checking Asserts with regular test-and-elog checks in all encoding conversion functions.  These are not can't-happen cases because it's possible to create a conversion with the wrong conversion function for the specified encoding pair.  That would lead to an Assert crash in an Assert-enabled build, or incorrect conversion otherwise, neither of which is desirable.  This would be a DOS issue if production databases were customarily built with asserts enabled, but fortunately that's not so. Per an observation by Heikki.
    Back-patch to all supported branches.

--- src/backend/utils/mb/conversion_procs/utf8_and_iso8859_1/utf8_and_iso8859_1.c
+++ src/backend/utils/mb/conversion_procs/utf8_and_iso8859_1/utf8_and_iso8859_1.c
@@ -42,9 +42,7 @@ iso8859_1_to_utf8(PG_FUNCTION_ARGS)
 	int			len = PG_GETARG_INT32(4);
 	unsigned short c;
 
-	Assert(PG_GETARG_INT32(0) == PG_LATIN1);
-	Assert(PG_GETARG_INT32(1) == PG_UTF8);
-	Assert(len >= 0);
+	CHECK_ENCODING_CONVERSION_ARGS(PG_LATIN1, PG_UTF8);
 
 	while (len > 0)
 	{
@@ -75,9 +73,7 @@ utf8_to_iso8859_1(PG_FUNCTION_ARGS)
 	unsigned short c,
 				c1;
 
-	Assert(PG_GETARG_INT32(0) == PG_UTF8);
-	Assert(PG_GETARG_INT32(1) == PG_LATIN1);
-	Assert(len >= 0);
+	CHECK_ENCODING_CONVERSION_ARGS(PG_UTF8, PG_LATIN1);
 
 	while (len > 0)
 	{
