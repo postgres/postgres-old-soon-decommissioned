ef5d7361db0e65ee3f141c27f28787661eaef061
Peter Eisentraut <peter_e@gmx.net>
2009-02-17 09:24:57 +0000

    Remove gettext trigger write_stderr(), which isn't used by PLs.

--- src/pl/plperl/nls.mk
+++ src/pl/plperl/nls.mk
@@ -2,4 +2,4 @@
 CATALOG_NAME	:= plperl
 AVAIL_LANGUAGES	:=
 GETTEXT_FILES	:= plperl.c SPI.xs
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr croak Perl_croak
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext croak Perl_croak
