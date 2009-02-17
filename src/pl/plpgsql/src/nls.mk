e7803245dcae3e23ab66b85e1a0f76f3222e596b
Peter Eisentraut <peter_e@gmx.net>
2009-02-17 12:51:59 +0000

    Mark 3rd argument of validate_tupdesc_compat() for translation, instead of marking up each instance separately.

--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -2,7 +2,7 @@
 CATALOG_NAME	:= plpgsql
 AVAIL_LANGUAGES	:= es
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scan.c
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext yyerror
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext validate_tupdesc_compat:3 yyerror
 
 .PHONY: gettext-files
 gettext-files: distprep
