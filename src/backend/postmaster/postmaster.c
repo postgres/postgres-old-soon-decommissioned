b50a8a0e465ed7edf9c18a984d853981940e3c59
Heikki Linnakangas <heikki@enterprisedb.com>
2009-02-19 16:43:13 +0000

    Fix bogus comment, from the patch to start bgwriter during archive recovery.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2652,7 +2652,8 @@ PostmasterStateMachine(void)
 	if (pmState == PM_RECOVERY && RecoveryStatus >= RecoveryConsistent)
 	{
 		/*
-		 * Go to shutdown mode if a shutdown request was pending.
+		 * Recovery has reached a consistent recovery point. Go to shutdown
+		 * mode if a shutdown request was pending.
 		 */
 		if (Shutdown > NoShutdown)
 		{
@@ -2661,10 +2662,6 @@ PostmasterStateMachine(void)
 		}
 		else
 		{
-			/*
-			 * Startup process has entered recovery. We consider that good
-			 * enough to reset FatalError.
-			 */
 			pmState = PM_RECOVERY_CONSISTENT;
 
 			/*
