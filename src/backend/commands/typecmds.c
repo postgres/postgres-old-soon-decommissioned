586ac631213fdbea72c00a9962893207a563f044
Tom Lane <tgl@sss.pgh.pa.us>
2009-02-24 01:39:01 +0000

    Repair a longstanding bug in CLUSTER and the rewriting variants of ALTER TABLE: if the command is executed by someone other than the table owner (eg, a superuser) and the table has a toast table, the toast table's pg_type row ends up with the wrong typowner, ie, the command issuer not the table owner. This is quite harmless for most purposes, since no interesting permissions checks consult the pg_type row.  However, it could lead to unexpected failures if one later tries to drop the role that issued the command (in 8.1 or 8.2), or strange warnings from pg_dump afterwards (in 8.3 and up, which will allow the DROP ROLE because we don't create a "redundant" owner dependency for table rowtypes).  Problem identified by Cott Lang.
    Back-patch to 8.1.  The problem is actually far older --- the CLUSTER variant
    can be demonstrated in 7.0 --- but it's mostly cosmetic before 8.1 because we
    didn't track ownership dependencies before 8.1.  Also, fixing it before 8.1
    would require changing the call signature of heap_create_with_catalog(), which
    seems to carry a nontrivial risk of breaking add-on modules.

--- src/backend/commands/typecmds.c
+++ src/backend/commands/typecmds.c
@@ -146,7 +146,7 @@ DefineType(List *names, List *parameters)
 							0, 0);
 	if (!OidIsValid(typoid))
 	{
-		typoid = TypeShellMake(typeName, typeNamespace);
+		typoid = TypeShellMake(typeName, typeNamespace, GetUserId());
 		/* Make new shell type visible for modification below */
 		CommandCounterIncrement();
 
@@ -374,6 +374,7 @@ DefineType(List *names, List *parameters)
 				   typeNamespace,		/* namespace */
 				   InvalidOid,	/* relation oid (n/a here) */
 				   0,			/* relation kind (ditto) */
+				   GetUserId(),	/* owner's ID */
 				   internalLength,		/* internal size */
 				   'b',			/* type-type (base type) */
 				   delimiter,	/* array element delimiter */
@@ -406,6 +407,7 @@ DefineType(List *names, List *parameters)
 			   typeNamespace,	/* namespace */
 			   InvalidOid,		/* relation oid (n/a here) */
 			   0,				/* relation kind (ditto) */
+			   GetUserId(),		/* owner's ID */
 			   -1,				/* internal size */
 			   'b',				/* type-type (base type) */
 			   DEFAULT_TYPDELIM,	/* array element delimiter */
@@ -774,6 +776,7 @@ DefineDomain(CreateDomainStmt *stmt)
 				   domainNamespace,		/* namespace */
 				   InvalidOid,	/* relation oid (n/a here) */
 				   0,			/* relation kind (ditto) */
+				   GetUserId(),	/* owner's ID */
 				   internalLength,		/* internal size */
 				   'd',			/* type-type (domain type) */
 				   delimiter,	/* array element delimiter */
