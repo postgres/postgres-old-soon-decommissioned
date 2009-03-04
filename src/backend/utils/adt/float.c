e7b61f6a8acd29ddc4db56f411c4340f0dbceb41
Tom Lane <tgl@sss.pgh.pa.us>
2009-03-04 22:08:34 +0000

    Put back our old workaround for machines that declare cbrt() in math.h but fail to provide the function itself.  Not sure how we escaped testing anything later than 7.3 on such cases, but they still exist, as per André Volpato's report about AIX 5.3.

--- src/backend/utils/adt/float.c
+++ src/backend/utils/adt/float.c
@@ -110,6 +110,15 @@ static int	float4_cmp_internal(float4 a, float4 b);
 static int	float8_cmp_internal(float8 a, float8 b);
 
 #ifndef HAVE_CBRT
+/*
+ * Some machines (in particular, some versions of AIX) have an extern
+ * declaration for cbrt() in <math.h> but fail to provide the actual
+ * function, which causes configure to not set HAVE_CBRT.  Furthermore,
+ * their compilers spit up at the mismatch between extern declaration
+ * and static definition.  We work around that here by the expedient
+ * of a #define to make the actual name of the static function different.
+ */
+#define cbrt my_cbrt
 static double cbrt(double x);
 #endif   /* HAVE_CBRT */
 
