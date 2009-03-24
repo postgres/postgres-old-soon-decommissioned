a1c5cffd6c7f1f38fa52f208d5acdef25991793c
Peter Eisentraut <peter_e@gmx.net>
2009-03-24 12:57:31 +0000

    Fix my typo

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -93,7 +93,7 @@ usage(void)
 	if (!env)
 		env = user;
 	printf(_("  -c, --command=COMMAND    run only single command (SQL or internal) and exit\n"));
-	printf(_("  -d, --dbnme=DBNAME       database name to connect to (default: \"%s\")\n"), env);
+	printf(_("  -d, --dbname=DBNAME      database name to connect to (default: \"%s\")\n"), env);
 	printf(_("  -f, --file=FILENAME      execute commands from file, then exit\n"));
 	printf(_("  -l, --list               list available databases, then exit\n"));
 	printf(_("  -v, --set=, --variable=NAME=VALUE\n"
