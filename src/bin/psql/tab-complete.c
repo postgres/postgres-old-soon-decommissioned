017ecc15620546d34a45ccf07d874319b138803e
Heikki Linnakangas <heikki@enterprisedb.com>
2009-03-27 14:58:54 +0000

    Fix tab completion of ANALYZE VERBOSE <tab>. It was previously confused with EXPLAIN ANALYZE VERBOSE.
    Greg Sabino Mullane, reformatted by myself. Backpatch to 8.1, where the
    bug was introduced.

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -1500,11 +1500,11 @@ psql_completion(char *text, int start, int end)
 
 		COMPLETE_WITH_LIST(list_EXPLAIN);
 	}
-	else if (pg_strcasecmp(prev_wd, "VERBOSE") == 0 &&
-			 pg_strcasecmp(prev3_wd, "VACUUM") != 0 &&
-			 pg_strcasecmp(prev4_wd, "VACUUM") != 0 &&
-			 (pg_strcasecmp(prev2_wd, "ANALYZE") == 0 ||
-			  pg_strcasecmp(prev2_wd, "EXPLAIN") == 0))
+	else if ((pg_strcasecmp(prev2_wd, "EXPLAIN") == 0 &&
+			  pg_strcasecmp(prev_wd, "VERBOSE") == 0) ||
+			 (pg_strcasecmp(prev3_wd, "EXPLAIN") == 0 &&
+			  pg_strcasecmp(prev2_wd, "ANALYZE") == 0 &&
+			  pg_strcasecmp(prev_wd, "VERBOSE") == 0))
 	{
 		static const char *const list_EXPLAIN[] =
 		{"SELECT", "INSERT", "DELETE", "UPDATE", "DECLARE", NULL};
