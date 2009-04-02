e3f3d7f5c52dc8fcd879c511a4a69b664d8fa342
Bruce Momjian <bruce@momjian.us>
2009-04-02 19:57:19 +0000

    Give a better error message when trying to change "effective_io_concurrency" on systems without posix_fadvise().

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -1713,7 +1713,13 @@ static struct config_int ConfigureNamesInt[] =
 	},
 
 	{
-		{"effective_io_concurrency", PGC_USERSET, RESOURCES,
+		{"effective_io_concurrency",
+#ifdef USE_PREFETCH
+		PGC_USERSET,
+#else
+		PGC_INTERNAL,
+#endif
+		RESOURCES,
 			gettext_noop("Number of simultaneous requests that can be handled efficiently by the disk subsystem."),
 			gettext_noop("For RAID arrays, this should be approximately the number of drive spindles in the array.")
 		},
