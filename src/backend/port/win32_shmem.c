4d754acf96ffa01c02dd9882ab0ebfe2fd6ad999
Magnus Hagander <magnus@hagander.net>
2009-05-04 08:36:40 +0000

    Call SetLastError(0) before calling the file mapping functions to make sure that the error code is reset, as a precaution in case the API doesn't properly reset it on success. This could be necessary, since we check the error value even if the function doesn't fail for specific success cases.

--- src/backend/port/win32_shmem.c
+++ src/backend/port/win32_shmem.c
@@ -131,6 +131,9 @@ PGSharedMemoryCreate(Size size, bool makePrivate, int port)
 
 	UsedShmemSegAddr = NULL;
 
+	/* In case CreateFileMapping() doesn't set the error code to 0 on success */
+	SetLastError(0);
+
 	hmap = CreateFileMapping((HANDLE) 0xFFFFFFFF,		/* Use the pagefile */
 							 NULL,		/* Default security attrs */
 							 PAGE_READWRITE,	/* Memory is Read/Write */
@@ -159,6 +162,9 @@ PGSharedMemoryCreate(Size size, bool makePrivate, int port)
 								 * one to the previous segment. */
 
 		Sleep(1000);
+
+		/* In case CreateFileMapping() doesn't set the error code to 0 on success */
+		SetLastError(0);
 
 		hmap = CreateFileMapping((HANDLE) 0xFFFFFFFF, NULL, PAGE_READWRITE, 0L, (DWORD) size, szShareMem);
 		if (!hmap)
