1f4cebe8eaae7f1992870842b1a9892e56856746
Tom Lane <tgl@sss.pgh.pa.us>
2009-05-15 15:56:39 +0000

    Fix all the server-side SIGQUIT handlers (grumble ... why so many identical copies?) to ensure they really don't run proc_exit/shmem_exit callbacks, as was intended.  I broke this behavior recently by installing atexit callbacks without thinking about the one case where we truly don't want to run those callback functions.  Noted in an example from Dave Page.

--- src/backend/storage/ipc/ipc.c
+++ src/backend/storage/ipc/ipc.c
@@ -166,7 +166,8 @@ proc_exit_prepare(int code)
 	/* do our shared memory exits first */
 	shmem_exit(code);
 
-	elog(DEBUG3, "proc_exit(%d)", code);
+	elog(DEBUG3, "proc_exit(%d): %d callbacks to make",
+		 code, on_proc_exit_index);
 
 	/*
 	 * call all the registered callbacks.
@@ -193,7 +194,8 @@ proc_exit_prepare(int code)
 void
 shmem_exit(int code)
 {
-	elog(DEBUG3, "shmem_exit(%d)", code);
+	elog(DEBUG3, "shmem_exit(%d): %d callbacks to make",
+		 code, on_shmem_exit_index);
 
 	/*
 	 * call all the registered callbacks.
