4eaa89100056e2cd0b0b931b913a8dcacccdac7e
Peter Eisentraut <peter_e@gmx.net>
2009-05-27 20:47:55 +0000

    Fix to use the same format specifiers in both branches of a ngettext().
    Zdenek Kotala

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -2036,7 +2036,7 @@ describeRoles(const char *pattern, bool verbose)
 			if (conns == 0)
 				appendPQExpBuffer(&buf, _("No connections"));
 			else
-				appendPQExpBuffer(&buf, ngettext("1 connection", "%d connections", conns), conns);
+				appendPQExpBuffer(&buf, ngettext("%d connection", "%d connections", conns), conns);
 		}
 
 		attr[i] = pg_strdup(buf.data);
--- src/bin/psql/print.c
+++ src/bin/psql/print.c
@@ -2348,7 +2348,7 @@ printQuery(const PGresult *result, const printQueryOpt *opt, FILE *fout, FILE *f
 		char		default_footer[100];
 
 		total_records = opt->topt.prior_records + cont.nrows;
-		snprintf(default_footer, 100, ngettext("(1 row)", "(%lu rows)", total_records), total_records);
+		snprintf(default_footer, 100, ngettext("(%lu row)", "(%lu rows)", total_records), total_records);
 
 		printTableAddFooter(&cont, default_footer);
 	}
