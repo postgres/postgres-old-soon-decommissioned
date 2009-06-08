896d5ca8038b866c14b28b0edaf6e15b35052da4
Tom Lane <tgl@sss.pgh.pa.us>
2009-06-08 16:22:44 +0000

    Fix contrib/pageinspect to not create an ABI breakage between 8.3 and 8.4. The original implementation of the 3-argument form of get_raw_page() risked core dumps if the 8.3 SQL function definition was mistakenly used with the 8.4 module, which is entirely likely after a dump-and-reload upgrade.  To protect 8.4 beta testers against upgrade problems, add a check on PG_NARGS.
    In passing, fix missed additions to the uninstall script, and polish the
    docs a trifle.

--- contrib/pageinspect/uninstall_pageinspect.sql
+++ contrib/pageinspect/uninstall_pageinspect.sql
@@ -4,8 +4,10 @@
 SET search_path = public;
 
 DROP FUNCTION get_raw_page(text, int4);
+DROP FUNCTION get_raw_page(text, text, int4);
 DROP FUNCTION page_header(bytea);
 DROP FUNCTION heap_page_items(bytea);
 DROP FUNCTION bt_metap(text);
 DROP FUNCTION bt_page_stats(text, int4);
 DROP FUNCTION bt_page_items(text, int4);
+DROP FUNCTION fsm_page_contents(bytea);
