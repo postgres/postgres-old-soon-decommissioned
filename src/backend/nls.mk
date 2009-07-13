44210093ff313a713fd563f2740628f3bdac0c67
Tom Lane <tgl@sss.pgh.pa.us>
2009-07-13 02:02:20 +0000

    Convert the core lexer and parser into fully reentrant code, by making use of features added to flex and bison since this code was originally written. This change doesn't in itself offer any new capability, but it's needed infrastructure for planned improvements in plpgsql.
    Another feature now available in flex is the ability to make it use palloc
    instead of malloc, so do that to avoid possible memory leaks.  (We should
    at some point change the other lexers likewise, but this commit doesn't
    touch them.)

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -2,7 +2,7 @@
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= de es fr ja pt_BR tr
 GETTEXT_FILES	:= + gettext-files
-GETTEXT_TRIGGERS:= _ errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext write_stderr yyerror
+GETTEXT_TRIGGERS:= _ errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext write_stderr yyerror parser_yyerror
 
 gettext-files: distprep
 	find $(srcdir)/ $(srcdir)/../port/ -name '*.c' -print >$@
