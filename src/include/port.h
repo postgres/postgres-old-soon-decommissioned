6a28296186590cf6f6b8d9f6eefa7619e0622256
Tom Lane <tgl@sss.pgh.pa.us>
2009-07-16 17:43:52 +0000

    Add erand48() to the set of functions supported by our src/port/ library, and extend configure to test for it properly instead of hard-wiring an assumption that everybody but Windows has the rand48 functions. (We do cheat to the extent of assuming that probing for erand48 will do for the entire rand48 family.)
    erand48() is unused as of this commit, but a followon patch will cause
    GEQO to depend on it.
    
    Andres Freund, additional hacking by Tom

--- src/include/port.h
+++ src/include/port.h
@@ -316,10 +316,6 @@ extern FILE *pgwin32_fopen(const char *, const char *);
 #define popen(a,b) _popen(a,b)
 #define pclose(a) _pclose(a)
 
-/* Missing rand functions */
-extern long lrand48(void);
-extern void srand48(long seed);
-
 /* New versions of MingW have gettimeofday, old mingw and msvc don't */
 #ifndef HAVE_GETTIMEOFDAY
 /* Last parameter not used */
@@ -349,6 +345,13 @@ extern char *crypt(const char *key, const char *setting);
 extern int	fseeko(FILE *stream, off_t offset, int whence);
 extern off_t ftello(FILE *stream);
 #endif
+#endif
+
+#ifndef HAVE_ERAND48
+/* we assume all of these are present or missing together */
+extern double erand48(unsigned short xseed[3]);
+extern long lrand48(void);
+extern void srand48(long seed);
 #endif
 
 #ifndef HAVE_FSEEKO
