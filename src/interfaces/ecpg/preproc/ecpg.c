5efffe0a228eebcb7860a98d2de5de1ccd1f8c5a
Michael Meskes <meskes@postgresql.org>
2009-08-05 11:42:20 +0000

    Fixed copyright notice.

--- src/interfaces/ecpg/preproc/ecpg.c
+++ src/interfaces/ecpg/preproc/ecpg.c
@@ -1,8 +1,7 @@
 /* $PostgreSQL$ */
 
-/* New main for ecpg, the PostgreSQL embedded SQL precompiler. */
-/* (C) Michael Meskes <meskes@postgresql.org> Feb 5th, 1998 */
-/* Placed under the same license as PostgreSQL */
+/* Main for ecpg, the PostgreSQL embedded SQL precompiler. */
+/* Copyright (c) 1996-2009, PostgreSQL Global Development Group */
 
 #include "postgres_fe.h"
 
