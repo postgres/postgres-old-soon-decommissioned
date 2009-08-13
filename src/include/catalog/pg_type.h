b563a8e8aa8ccb0d8710c9246048c5db5e11f5ab
Peter Eisentraut <peter_e@gmx.net>
2009-08-13 21:14:31 +0000

    Remove stray character in type description

--- src/include/catalog/pg_type.h
+++ src/include/catalog/pg_type.h
@@ -372,7 +372,7 @@ DESCR("geometric polygon '(pt1,...)'");
 #define POLYGONOID		604
 
 DATA(insert OID = 628 (  line	   PGNSP PGUID 32 f b G f t \054 0 701 629 line_in line_out line_recv line_send - - - d p f 0 -1 0 _null_ _null_ ));
-DESCR("geometric line (not implemented)'");
+DESCR("geometric line (not implemented)");
 #define LINEOID			628
 DATA(insert OID = 629 (  _line	   PGNSP PGUID	-1 f b A f t \054 0 628 0 array_in array_out array_recv array_send - - - d x f 0 -1 0 _null_ _null_ ));
 DESCR("");
