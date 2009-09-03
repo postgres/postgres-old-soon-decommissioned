e63ad4ccc8bab41b572955781585a1fcebdd7284
Peter Eisentraut <peter_e@gmx.net>
2009-09-03 21:01:26 +0000

    Translation updates

--- src/pl/tcl/nls.mk
+++ src/pl/tcl/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= pltcl
-AVAIL_LANGUAGES	:= de es fr ja pt_BR tr
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR tr
 GETTEXT_FILES	:= pltcl.c
 GETTEXT_TRIGGERS:= errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext
