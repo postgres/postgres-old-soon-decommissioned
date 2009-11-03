01f1d01db378bcd8f0e399ee8006996209fcaca7
Peter Eisentraut <peter_e@gmx.net>
2009-11-03 08:12:46 +0000

    Fix obscure segfault condition in PL/Python
    In PLy_output(), when the elog() call in the TRY branch throws an exception
    (this can happen when a statement timeout kicks in, for example), the
    PyErr_SetString() call in the CATCH branch can cause a segfault, because the
    Py_XDECREF(so) call before it releases memory that is still used by the sv
    variable that PyErr_SetString() uses as argument, because sv points into
    memory owned by so.
    
    Backpatched back to 8.0, where this code was introduced.
    
    I also threw in a couple of volatile declarations for variables that are used
    before and after the TRY.  I don't think they caused the crash that I
    observed, but they could become issues.

--- src/pl/plpython/plpython.c
+++ src/pl/plpython/plpython.c
@@ -2410,9 +2410,9 @@ PLy_fatal(PyObject * self, PyObject * args)
 static PyObject *
 PLy_output(volatile int level, PyObject * self, PyObject * args)
 {
-	PyObject   *so;
+	PyObject   *volatile so;
 	char	   *volatile sv;
-	MemoryContext oldcontext;
+	volatile MemoryContext oldcontext;
 
 	so = PyObject_Str(args);
 	if ((so == NULL) || ((sv = PyString_AsString(so)) == NULL))
@@ -2431,6 +2431,10 @@ PLy_output(volatile int level, PyObject * self, PyObject * args)
 		MemoryContextSwitchTo(oldcontext);
 		PLy_error_in_progress = CopyErrorData();
 		FlushErrorState();
+
+		PyErr_SetString(PLy_exc_error, sv);
+		/* Note: If sv came from PyString_AsString(), it points into
+		 * storage owned by so.  So free so after using sv. */
 		Py_XDECREF(so);
 
 		/*
@@ -2438,7 +2442,6 @@ PLy_output(volatile int level, PyObject * self, PyObject * args)
 		 * control passes back to PLy_procedure_call, we check for PG
 		 * exceptions and re-throw the error.
 		 */
-		PyErr_SetString(PLy_exc_error, sv);
 		return NULL;
 	}
 	PG_END_TRY();
