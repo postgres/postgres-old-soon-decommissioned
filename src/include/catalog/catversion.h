55b98755330a5ab8ff1d89700322f4d5d2d5bae4
Tom Lane <tgl@sss.pgh.pa.us>
2009-12-29 22:00:14 +0000

    Add an index on pg_inherits.inhparent, and use it to avoid seqscans in find_inheritance_children().  This is a complete no-op in databases without any inheritance.  In databases where there are just a few entries in pg_inherits, it could conceivably be a small loss.  However, in databases with many inheritance parents, it can be a big win.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200912281
+#define CATALOG_VERSION_NO	200912291
 
 #endif
--- src/include/catalog/indexing.h
+++ src/include/catalog/indexing.h
@@ -156,6 +156,9 @@ DECLARE_UNIQUE_INDEX(pg_index_indexrelid_index, 2679, on pg_index using btree(in
 
 DECLARE_UNIQUE_INDEX(pg_inherits_relid_seqno_index, 2680, on pg_inherits using btree(inhrelid oid_ops, inhseqno int4_ops));
 #define InheritsRelidSeqnoIndexId  2680
+/* This following index is not used for a cache and is not unique */
+DECLARE_INDEX(pg_inherits_parent_index, 2187, on pg_inherits using btree(inhparent oid_ops));
+#define InheritsParentIndexId  2187
 
 DECLARE_UNIQUE_INDEX(pg_language_name_index, 2681, on pg_language using btree(lanname name_ops));
 #define LanguageNameIndexId  2681
