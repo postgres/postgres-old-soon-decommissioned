96139057172a00624e8b94882cd305ae07e10e17
Heikki Linnakangas <heikki@enterprisedb.com>
2009-12-30 08:37:23 +0000

    Reset minRecoveryPoint at checkpoints, so that we don't uselessly update it in the control file at crash recovery following an archive recovery.
    Per Fujii Masao and subsequent discussion.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6659,6 +6659,8 @@ CreateCheckPoint(int flags)
 	ControlFile->checkPoint = ProcLastRecPtr;
 	ControlFile->checkPointCopy = checkPoint;
 	ControlFile->time = (pg_time_t) time(NULL);
+	/* crash recovery should always recover to the end of WAL */
+	MemSet(&ControlFile->minRecoveryPoint, 0, sizeof(XLogRecPtr));
 	UpdateControlFile();
 	LWLockRelease(ControlFileLock);
 
