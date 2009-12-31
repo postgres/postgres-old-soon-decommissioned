3d349e13df521f9c4267dc932745e1ac4111d916
Andrew Dunstan <andrew@dunslane.net>
2009-12-31 00:16:47 +0000

    enable examples to compile on Windows. patch from Hiroshi Saito.

--- src/test/examples/testlibpq2.c
+++ src/test/examples/testlibpq2.c
@@ -24,11 +24,16 @@
  *
  *	 INSERT INTO TBL1 VALUES (10);
  */
+
+#ifdef WIN32
+#include <windows.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include "libpq-fe.h"
 
 static void
--- src/test/examples/testlibpq3.c
+++ src/test/examples/testlibpq3.c
@@ -25,8 +25,14 @@
  *	t = (8 bytes) 'ho there'
  *	b = (5 bytes) \004\003\002\001\000
  */
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
 #include "libpq-fe.h"
