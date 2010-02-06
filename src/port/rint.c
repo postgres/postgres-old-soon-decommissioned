96de4c83894714443319c62ee51b4e9751cf46c8
Tom Lane <tgl@sss.pgh.pa.us>
2010-02-06 05:42:49 +0000

    Avoid changing the sign of zero.  Per buildfarm failures.

--- src/port/rint.c
+++ src/port/rint.c
@@ -8,12 +8,12 @@
  *
  *-------------------------------------------------------------------------
  */
-
 #include "c.h"
+
 #include <math.h>
 
 double
 rint(double x)
 {
-	return (x > 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
+	return (x >= 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
 }
