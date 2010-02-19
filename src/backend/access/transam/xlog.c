0e68f914def4b096c1f8e26e6cd940e8ddc242a5
Itagaki Takahiro <itagaki.takahiro@oss.ntt.co.jp>
2010-02-19 01:07:57 +0000

    Fix STOP WAL LOCATION in backup history files no to return the next segment of XLOG_BACKUP_END record even if the the record is placed at a segment boundary. Furthermore the previous implementation could return nonexistent segment file name when the boundary is in segments that has "FE" suffix; We never use segments with "FF" suffix.
    Backpatch to 8.0, where hot backup was introduced.
    
    Reported by Fujii Masao.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6382,7 +6382,7 @@ pg_stop_backup(PG_FUNCTION_ARGS)
 	 */
 	stoppoint = RequestXLogSwitch();
 
-	XLByteToSeg(stoppoint, _logId, _logSeg);
+	XLByteToPrevSeg(stoppoint, _logId, _logSeg);
 	XLogFileName(stopxlogfilename, ThisTimeLineID, _logId, _logSeg);
 
 	/*
