a20fd3b53c88cd2ea95158854de6b6aa98611043
Heikki Linnakangas <heikki@enterprisedb.com>
2010-03-01 21:27:26 +0000

    Fix translation of strings in psql \d output (translation in headers worked, but not in cells).

--- src/bin/psql/print.c
+++ src/bin/psql/print.c
@@ -2106,6 +2106,7 @@ printTableAddHeader(printTableContent *const content, const char *header,
  * Otherwise, the cell will not be translated.
  *
  * If mustfree is true, the cell string is freed by printTableCleanup().
+ * Note: Automatic freeing of translatable strings is not supported.
  */
 void
 printTableAddCell(printTableContent *const content, const char *cell,
@@ -2128,7 +2129,7 @@ printTableAddCell(printTableContent *const content, const char *cell,
 
 #ifdef ENABLE_NLS
 	if (translate)
-		*content->header = _(*content->header);
+		*content->cell = _(*content->cell);
 #endif
 
 	if (mustfree)
