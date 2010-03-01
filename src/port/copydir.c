173d4cd535661a36c9d3d1d2d6b9cf0b73f445d9
Greg Stark <stark@mit.edu>
2010-03-01 00:04:06 +0000

    add EPERM to the list of return codes to expect from opening directories based on Vista results

--- src/port/copydir.c
+++ src/port/copydir.c
@@ -234,8 +234,10 @@ fsync_fname(char *fname, bool isdir)
 						   O_RDONLY | PG_BINARY,
 						   S_IRUSR | S_IWUSR);
 
-	/* Some OSs don't allow us to open directories at all */
-	if (fd < 0 && isdir && errno == EISDIR)
+	/* Some OSs don't allow us to open directories at all 
+	 * (Windows returns EPERM) 
+	 */
+	if (fd < 0 && isdir && (errno == EISDIR || errno == EPERM))
 		return;
 
 	else if (fd < 0)
