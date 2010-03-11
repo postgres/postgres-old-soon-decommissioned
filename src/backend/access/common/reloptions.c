91b52844f37edc3d5b4f63dd148ffd994752f4a3
Tom Lane <tgl@sss.pgh.pa.us>
2010-03-11 21:47:19 +0000

    Add missing reset of need_initialization in reloptions code.
    This resulted in useless extra work during every call of parseRelOptions,
    but no bad effects other than that.  Noted by Alvaro.

--- src/backend/access/common/reloptions.c
+++ src/backend/access/common/reloptions.c
@@ -244,8 +244,9 @@ static void
 initialize_reloptions(void)
 {
 	int			i;
-	int			j = 0;
+	int			j;
 
+	j = 0;
 	for (i = 0; boolRelOpts[i].gen.name; i++)
 		j++;
 	for (i = 0; intRelOpts[i].gen.name; i++)
@@ -302,6 +303,9 @@ initialize_reloptions(void)
 
 	/* add a list terminator */
 	relOpts[j] = NULL;
+
+	/* flag the work is complete */
+	need_initialization = false;
 }
 
 /*
