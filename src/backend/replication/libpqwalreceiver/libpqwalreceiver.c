430e5a0ee91094c6ca81aec1a5aba43fd26207d7
Simon Riggs <simon@2ndquadrant.com>
2010-03-19 17:51:42 +0000

    Minor tweaks on libpqrcv_connect(): ensure conninfo_repl[] is correctly sized and expand comment to explain otherwise undocumented use of replication connection parameter.

--- src/backend/replication/libpqwalreceiver/libpqwalreceiver.c
+++ src/backend/replication/libpqwalreceiver/libpqwalreceiver.c
@@ -76,7 +76,7 @@ _PG_init(void)
 static bool
 libpqrcv_connect(char *conninfo, XLogRecPtr startpoint)
 {
-	char		conninfo_repl[MAXCONNINFO + 14];
+	char		conninfo_repl[MAXCONNINFO + 18];
 	char	   *primary_sysid;
 	char		standby_sysid[32];
 	TimeLineID	primary_tli;
@@ -84,7 +84,7 @@ libpqrcv_connect(char *conninfo, XLogRecPtr startpoint)
 	PGresult   *res;
 	char		cmd[64];
 
-	/* Connect */
+	/* Connect using deliberately undocumented parameter: replication */
 	snprintf(conninfo_repl, sizeof(conninfo_repl), "%s replication=true", conninfo);
 
 	streamConn = PQconnectdb(conninfo_repl);
