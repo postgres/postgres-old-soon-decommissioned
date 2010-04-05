bab3ed013d506bdac3be32d1d01bfb15247e3e98
Itagaki Takahiro <itagaki.takahiro@oss.ntt.co.jp>
2010-04-05 02:25:56 +0000

    Use a new API rather than a deprecated one in in cygwin. cygwin_conv_to_full_win32_path should be replaced with cygwin_conv_path.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -1080,7 +1080,7 @@ pgwin32_CommandLine(bool registration)
 
 #ifdef __CYGWIN__
 	/* need to convert to windows path */
-	cygwin_conv_to_full_win32_path(cmdLine, buf);
+	cygwin_conv_path(CCP_POSIX_TO_WIN_A, cmdLine, buf, sizeof(buf));
 	strcpy(cmdLine, buf);
 #endif
 
