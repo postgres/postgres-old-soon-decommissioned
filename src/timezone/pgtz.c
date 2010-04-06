5e545a5f898c6421a9914d0428ecd20ed7d8f226
Magnus Hagander <magnus@hagander.net>
2010-04-06 20:35:17 +0000

    Log the actual timezone name that we fail to look up the values for in case the registry data doesn't follow the format we expect, to facilitate debugging.

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -1087,7 +1087,8 @@ identify_system_timezone(void)
 		if ((r = RegQueryValueEx(key, "Std", NULL, NULL, zonename, &namesize)) != ERROR_SUCCESS)
 		{
 			ereport(WARNING,
-					(errmsg_internal("could not query value for 'std' to identify Windows timezone: %i", (int) r)));
+					(errmsg_internal("could not query value for 'std' to identify Windows timezone \"%s\": %i",
+									 keyname, (int) r)));
 			RegCloseKey(key);
 			break;
 		}
@@ -1103,7 +1104,8 @@ identify_system_timezone(void)
 		if ((r = RegQueryValueEx(key, "Dlt", NULL, NULL, zonename, &namesize)) != ERROR_SUCCESS)
 		{
 			ereport(WARNING,
-					(errmsg_internal("could not query value for 'dlt' to identify Windows timezone: %i", (int) r)));
+					(errmsg_internal("could not query value for 'dlt' to identify Windows timezone \"%s\": %i",
+									 keyname, (int) r)));
 			RegCloseKey(key);
 			break;
 		}
