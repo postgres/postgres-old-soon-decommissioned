dd01ff633cfb0b65d9ed1a2416048c763d67b70d
Magnus Hagander <magnus@hagander.net>
2010-04-08 11:26:00 +0000

    Proceed to look for the next timezone when matching a localized Windows timezone name where the information in the registry is incomplete, instead of aborting.
    This fixes cases when the registry information is incomplete for
    a timezone that is alphabetically before the one that is in use.
    
    Per report from Alexander Forschner

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -1076,7 +1076,7 @@ identify_system_timezone(void)
 					(errmsg_internal("could not query value for 'std' to identify Windows timezone \"%s\": %i",
 									 keyname, (int) r)));
 			RegCloseKey(key);
-			break;
+			continue; /* Proceed to look at the next timezone */
 		}
 		if (strcmp(tzname, zonename) == 0)
 		{
@@ -1093,7 +1093,7 @@ identify_system_timezone(void)
 					(errmsg_internal("could not query value for 'dlt' to identify Windows timezone \"%s\": %i",
 									 keyname, (int) r)));
 			RegCloseKey(key);
-			break;
+			continue; /* Proceed to look at the next timezone */
 		}
 		if (strcmp(tzname, zonename) == 0)
 		{
