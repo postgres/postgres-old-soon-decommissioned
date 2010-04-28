72c5406b5c9b6376ff4101efdb66214858722eb2
Tom Lane <tgl@sss.pgh.pa.us>
2010-04-28 17:35:35 +0000

    pg_controldata needs #define FRONTEND, same as pg_resetxlog. Per buildfarm results from dawn_bat.

--- src/bin/pg_controldata/pg_controldata.c
+++ src/bin/pg_controldata/pg_controldata.c
@@ -8,6 +8,14 @@
  *
  * $PostgreSQL$
  */
+
+/*
+ * We have to use postgres.h not postgres_fe.h here, because there's so much
+ * backend-only stuff in the XLOG include files we need.  But we need a
+ * frontend-ish environment otherwise.	Hence this ugly hack.
+ */
+#define FRONTEND 1
+
 #include "postgres.h"
 
 #include <unistd.h>
