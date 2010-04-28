61dc2a7e90695669b27aaea94b8d667b77f87b06
Magnus Hagander <magnus@hagander.net>
2010-04-28 13:46:23 +0000

    Fix typo that had the code check the same thing twice.
    Fujii Masao

--- src/interfaces/libpq/fe-protocol3.c
+++ src/interfaces/libpq/fe-protocol3.c
@@ -1909,7 +1909,7 @@ build_startup_packet(const PGconn *conn, char *packet,
 		ADD_STARTUP_OPTION("user", conn->pguser);
 	if (conn->dbName && conn->dbName[0])
 		ADD_STARTUP_OPTION("database", conn->dbName);
-	if (conn->replication && conn->replication)
+	if (conn->replication && conn->replication[0])
 		ADD_STARTUP_OPTION("replication", conn->replication);
 	if (conn->pgoptions && conn->pgoptions[0])
 		ADD_STARTUP_OPTION("options", conn->pgoptions);
