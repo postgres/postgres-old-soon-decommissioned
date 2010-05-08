d73306f902a526ac69b773dcfbbcbe08fcde5167
Tom Lane <tgl@sss.pgh.pa.us>
2010-05-08 16:40:14 +0000

    Work around a subtle portability problem in use of printf %s format. Depending on which spec you read, field widths and precisions in %s may be counted either in bytes or characters.  Our code was assuming bytes, which is wrong at least for glibc's implementation, and in any case libc might have a different idea of the prevailing encoding than we do.  Hence, for portable results we must avoid using anything more complex than just "%s" unless the string to be printed is known to be all-ASCII.
    This patch fixes the cases I could find, including the psql formatting
    failure reported by Hernan Gonzalez.  In HEAD only, I also added comments
    to some places where it appears safe to continue using "%.*s".

--- src/backend/utils/error/elog.c
+++ src/backend/utils/error/elog.c
@@ -1615,7 +1615,7 @@ log_line_prefix(StringInfo buf)
 					int			displen;
 
 					psdisp = get_ps_display(&displen);
-					appendStringInfo(buf, "%.*s", displen, psdisp);
+					appendBinaryStringInfo(buf, psdisp, displen);
 				}
 				break;
 			case 'r':
@@ -1794,7 +1794,7 @@ write_csvlog(ErrorData *edata)
 		initStringInfo(&msgbuf);
 
 		psdisp = get_ps_display(&displen);
-		appendStringInfo(&msgbuf, "%.*s", displen, psdisp);
+		appendBinaryStringInfo(&msgbuf, psdisp, displen);
 		appendCSVLiteral(&buf, msgbuf.data);
 	
 		pfree(msgbuf.data);
