76257622fcfa9fd13800d92c9b0f9bc3e2f62402
Simon Riggs <simon@2ndquadrant.com>
2010-05-13 11:39:30 +0000

    Ensure that top level aborts call XLogSetAsyncCommit(). Not doing so simply leads to data waiting in wal_buffers which then causes later commits to potentially do emergency writes and for all forms of replication to be potentially delayed without need or benefit. Issue pointed out exactly by Fujii Masao, following bug report by Robert Haas on a separate though related topic.

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -1348,6 +1348,18 @@ RecordTransactionAbort(bool isSubXact)
 	(void) XLogInsert(RM_XACT_ID, XLOG_XACT_ABORT, rdata);
 
 	/*
+	 * Report the latest async abort LSN, so that the WAL writer knows to
+	 * flush this abort. There's nothing to be gained by delaying this,
+	 * since WALWriter may as well do this when it can. This is important
+	 * with streaming replication because if we don't flush WAL regularly
+	 * we will find that large aborts leave us with a long backlog for
+	 * when commits occur after the abort, increasing our window of data
+	 * loss should problems occur at that point.
+	 */
+	if (!isSubXact)
+		XLogSetAsyncCommitLSN(XactLastRecEnd);
+
+	/*
 	 * Mark the transaction aborted in clog.  This is not absolutely necessary
 	 * but we may as well do it while we are here; also, in the subxact case
 	 * it is helpful because XactLockTableWait makes use of it to avoid
