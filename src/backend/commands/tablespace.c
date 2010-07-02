483b8c7c1b4ef39be3b976da3e231c2b960c5fa6
Bruce Momjian <bruce@momjian.us>
2010-07-02 02:44:32 +0000

    Issue 'mkdir' hint when replying CREATE TABLESPACE in recovery mode.
    Per idea from Fujii Masao

--- src/backend/commands/tablespace.c
+++ src/backend/commands/tablespace.c
@@ -552,8 +552,9 @@ create_tablespace_directories(const char *location, const Oid tablespaceoid)
 		if (errno == ENOENT)
 			ereport(ERROR,
 					(errcode(ERRCODE_UNDEFINED_FILE),
-					 errmsg("directory \"%s\" does not exist",
-							location)));
+					 errmsg("directory \"%s\" does not exist", location),
+					 InRecovery ? errhint("Create directory \"%s\" for this tablespace before "
+							 "restarting the server.", location) : 0));
 		else
 			ereport(ERROR,
 					(errcode_for_file_access(),
