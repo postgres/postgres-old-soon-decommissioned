7ca804bbc872f9c2fa7dd0efc51cd171729e5b4b
Andrew Dunstan <andrew@dunslane.net>
2010-07-02 23:25:27 +0000

    Unbreak MSVC builds by removing copydir.c from list of libpgport files

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -49,7 +49,7 @@ sub mkvcbuild
     our @pgportfiles = qw(
       chklocale.c crypt.c fseeko.c getrusage.c inet_aton.c random.c srandom.c
       getaddrinfo.c gettimeofday.c kill.c open.c erand48.c
-      snprintf.c strlcat.c strlcpy.c copydir.c dirmod.c exec.c noblock.c path.c pipe.c
+      snprintf.c strlcat.c strlcpy.c dirmod.c exec.c noblock.c path.c pipe.c
       pgsleep.c pgstrcasecmp.c qsort.c qsort_arg.c sprompt.c thread.c
       getopt.c getopt_long.c dirent.c rint.c win32env.c win32error.c);
 
