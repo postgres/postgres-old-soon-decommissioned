183ae5c5789908e644771cfe1a147375209e3a9a
Tom Lane <tgl@sss.pgh.pa.us>
2010-07-05 23:30:50 +0000

    Make sure LDFLAGS come before LIBS when linking contrib programs. Solaris, at least, seems to be sensitive to the relative order of -L and -l switches, so this is needed.  Per buildfarm results.

--- src/makefiles/pgxs.mk
+++ src/makefiles/pgxs.mk
@@ -286,5 +286,5 @@ endif
 
 ifdef PROGRAM
 $(PROGRAM): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) $(PG_LIBS) $(LIBS) $(LDFLAGS) $(LDFLAGS_EX) -o $@
+	$(CC) $(CFLAGS) $(OBJS) $(LDFLAGS) $(LDFLAGS_EX) $(PG_LIBS) $(LIBS) -o $@
 endif
