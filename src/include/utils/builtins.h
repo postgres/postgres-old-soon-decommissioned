8cd1e17ac10fbd2224296582346500e4d3c49443
Tom Lane <tgl@sss.pgh.pa.us>
2010-07-13 20:57:19 +0000

    Teach EXPLAIN to print PARAM_EXEC Params as the referenced expressions, rather than just $N.  This brings the display of nestloop-inner-indexscan plans back to where it's been, and incidentally improves the display of SubPlan parameters as well.  In passing, simplify the EXPLAIN code by having it deal primarily in the PlanState tree rather than separately searching Plan and PlanState trees.  This is noticeably cleaner for subplans, and about a wash elsewhere.
    One small difference from previous behavior is that EXPLAIN will no longer
    qualify local variable references in inner-indexscan plan nodes, since it
    no longer sees such nodes as possibly referencing multiple tables.  Vars
    referenced through PARAM_EXEC Params are still forcibly qualified, though,
    so I don't think the display is any more confusing than before.  Adjust a
    couple of examples in the documentation to match this behavior.

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -609,8 +609,8 @@ extern Datum pg_get_function_result(PG_FUNCTION_ARGS);
 extern char *deparse_expression(Node *expr, List *dpcontext,
 				   bool forceprefix, bool showimplicit);
 extern List *deparse_context_for(const char *aliasname, Oid relid);
-extern List *deparse_context_for_plan(Node *plan, Node *outer_plan,
-						 List *rtable, List *subplans);
+extern List *deparse_context_for_planstate(Node *planstate, List *ancestors,
+							  List *rtable);
 extern const char *quote_identifier(const char *ident);
 extern char *quote_qualified_identifier(const char *qualifier,
 						   const char *ident);
