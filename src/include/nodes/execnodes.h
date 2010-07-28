7ba720d11b7d8ecc3bfae773ed42d7d2be0ec158
Tom Lane <tgl@sss.pgh.pa.us>
2010-07-28 04:51:08 +0000

    Fix potential failure when hashing the output of a subplan that produces a pass-by-reference datatype with a nontrivial projection step. We were using the same memory context for the projection operation as for the temporary context used by the hashtable routines in execGrouping.c. However, the hashtable routines feel free to reset their temp context at any time, which'd lead to destroying input data that was still needed. Report and diagnosis by Tao Ma.
    Back-patch to 8.1, where the problem was introduced by the changes that
    allowed us to work with "virtual" tuples instead of materializing intermediate
    tuple values everywhere.  The earlier code looks quite similar, but it doesn't
    suffer the problem because the data gets copied into another context as a
    result of having to materialize ExecProject's output tuple.

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -670,8 +670,9 @@ typedef struct SubPlanState
 	TupleHashTable hashnulls;	/* hash table for rows with null(s) */
 	bool		havehashrows;	/* TRUE if hashtable is not empty */
 	bool		havenullrows;	/* TRUE if hashnulls is not empty */
-	MemoryContext tablecxt;		/* memory context containing tables */
-	ExprContext *innerecontext; /* working context for comparisons */
+	MemoryContext hashtablecxt;	/* memory context containing hash tables */
+	MemoryContext hashtempcxt;	/* temp memory context for hash tables */
+	ExprContext *innerecontext; /* econtext for computing inner tuples */
 	AttrNumber *keyColIdx;		/* control data for hash tables */
 	FmgrInfo   *tab_hash_funcs; /* hash functions for table datatype(s) */
 	FmgrInfo   *tab_eq_funcs;	/* equality functions for table datatype(s) */
