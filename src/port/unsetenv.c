1fe51d135c111ffaec72366d91fd4fde61324ea9
Bruce Momjian <bruce@momjian.us>
2010-09-07 14:10:39 +0000

    Modify pg_upgrade to set/restore all environment variables related to collation/encoding to match English when reading controldata.  This now matches the English variable setting used by pg_regress.c.
    Backpatch to 9.0.X.

--- src/port/unsetenv.c
+++ src/port/unsetenv.c
@@ -32,6 +32,7 @@ unsetenv(const char *name)
 	 * implementations (notably recent BSDs) that do not obey SUS but copy the
 	 * presented string.  This method fails on such platforms.	Hopefully all
 	 * such platforms have unsetenv() and thus won't be using this hack.
+	 * See:  http://www.greenend.org.uk/rjk/2008/putenv.html
 	 *
 	 * Note that repeatedly setting and unsetting a var using this code will
 	 * leak memory.
