a3a937d3098edd808fe00b655ae80574c4e50f79
Peter Eisentraut <peter_e@gmx.net>
2010-09-16 19:09:39 +0000

    Translation updates for 9.0.0

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR sv ta tr zh_CN
+AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ro sv ta tr zh_CN
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/pl/plperl/nls.mk
+++ src/pl/plperl/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL$
 CATALOG_NAME	:= plperl
-AVAIL_LANGUAGES	:= de es fr it ja pt_BR tr
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR ro tr
 GETTEXT_FILES	:= plperl.c SPI.c
 GETTEXT_TRIGGERS:= errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext
